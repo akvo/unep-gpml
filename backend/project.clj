d38e63434fc07d1e4ea7d813f5bd7f1529890d42
Iván Perdomo <ivan@perdomo.me>
2021-01-07 14:13:16 +0100

    [#1] Update dependencies and define exclusions
    Useful command:
    
        docker-compose exec backend lein with-profile uberjar deps :tree

--- backend/project.clj
+++ backend/project.clj
@@ -5,13 +5,15 @@
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [duct/core "0.7.0"]
-                 [duct/module.logging "0.4.0"]
-                 [duct/module.sql "0.5.0"]
-                 [duct/module.web "0.7.0"]
+                 [duct/core "0.8.0"]
+                 [integrant "0.8.0"]
+                 [duct/module.logging "0.5.0"]
+                 [duct/module.sql "0.6.1" :exclusions [medley]]
+                 [duct/module.web "0.7.1":exclusions [medley]]
                  [org.postgresql/postgresql "42.2.5"]
-                 [metosin/reitit-ring "0.5.11"]
-                 [metosin/reitit-middleware "0.5.11"]]
+                 [metosin/reitit-ring "0.5.11" :exclusions [ring/ring-core]]
+                 [metosin/reitit-middleware "0.5.11" :exclusions [ring/ring-core
+                                                                  org.clojure/spec.alpha]]]
   :plugins [[duct/lein-duct "0.12.1"]]
   :main ^:skip-aot gpml.main
   :resource-paths ["resources" "target/resources"]
@@ -23,9 +25,9 @@
    :profiles/dev {}
    :project/dev  {:source-paths   ["dev/src"]
                   :resource-paths ["dev/resources"]
-                  :dependencies   [[integrant/repl "0.3.1"]
-                                   [eftest "0.5.7"]
-                                   [kerodon "0.9.0"]]
+                  :dependencies   [[integrant/repl "0.3.2" :exclusions [integrant]]
+                                   [eftest "0.5.9"]
+                                   [kerodon "0.9.1"]]
                   :plugins [[jonase/eastwood "0.3.12"]]
                   :eastwood {:config-files ["eastwood_cfg.clj"]}
                   :repl-options {:init-ns dev
