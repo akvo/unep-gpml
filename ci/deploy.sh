b3498586d4da657383cb056d04bf192c32bca2b1
Iván Perdomo <ivan@perdomo.me>
2021-01-10 13:18:58 +0100

    [#1] First attempt to configure k8s Deployment + Service

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,19 +3,38 @@
 
 set -exuo pipefail
 
-
-image_prefix="eu.gcr.io/akvo-lumen/unep-gpml"
-
 [[ "${CI_BRANCH}" !=  "main" ]] && { echo "Branch different than main. Skip deploy"; exit 0; }
 [[ "${CI_PULL_REQUEST}" ==  "true" ]] && { echo "Pull request. Skip deploy"; exit 0; }
 
-gcloud auth activate-service-account --key-file=/home/semaphore/.secrets/gcp.json
-gcloud config set project akvo-lumen
-gcloud config set container/cluster europe-west1-d
-gcloud config set compute/zone europe-west1-d
-gcloud config set container/use_client_certificate False
+auth () {
+    gcloud auth activate-service-account --key-file=/home/semaphore/.secrets/gcp.json
+    gcloud config set project akvo-lumen
+    gcloud config set container/cluster europe-west1-d
+    gcloud config set compute/zone europe-west1-d
+    gcloud config set container/use_client_certificate False
+    gcloud auth configure-docker "eu.gcr.io"
+}
 
-gcloud auth configure-docker "eu.gcr.io"
+push_image () {
+    prefix="eu.gcr.io/akvo-lumen/unep-gpml"
+    docker push "${prefix}/${1}:${CI_COMMIT}"
+}
 
-docker push "${image_prefix}/backend:${CI_COMMIT}"
-docker push "${image_prefix}/frontend:${CI_COMMIT}"
+prepare_deployment () {
+    gcloud container clusters get-credentials test # TODO: Change when promoting
+
+    sed "s/\${CI_COMMIT}/${CI_COMMIT}/g" \
+	ci/k8s/deployment.yml.template \
+	> ci/k8s/deployment.yml
+}
+
+apply_deployment () {
+    kubectl apply -f ci/k8s/deployment.yml
+    kubectl apply -f ci/k8s/service.yml
+}
+
+auth
+push_image backend
+push_image frontend
+prepare_deployment
+apply_deployment
