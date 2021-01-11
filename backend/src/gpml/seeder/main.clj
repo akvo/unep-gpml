226920c9174acc7e8d2dfdaf0c40dba93db6a7c4
dedenbangkit <mail@dedenbangkit.com>
2021-01-11 21:39:16 +0700

    [#7] refine seed fn

--- backend/src/gpml/seeder/main.clj
+++ backend/src/gpml/seeder/main.clj
@@ -4,9 +4,10 @@
 
 (def db {:connection-uri "jdbc:postgresql://db/gpml?user=unep&password=password"})
 
-(def data (map (fn [x] {:name (:name x) :iso_code (:code x)})
-  (j/read-value (slurp "https://unep.tc.akvo.org/api/countries") j/keyword-keys-object-mapper)))
-
-(defn seed []
+(defn seed-country []
   (jdbc/delete! db :country [])
-  (jdbc/insert-multi! db :country data))
+  (jdbc/insert-multi! db :country
+    (map (fn [x] {:name (:name x) :iso_code (:code x)})
+      (j/read-value (slurp "https://unep.tc.akvo.org/api/countries") j/keyword-keys-object-mapper))))
+
+(seed-country)
