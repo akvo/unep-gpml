3db835c15601f5c527f2e29cee834aa4544bf2bd
dedenbangkit <mail@dedenbangkit.com>
2021-01-13 00:42:14 +0700

    [#7] Tag & it's category seeder

--- backend/src/gpml/seeder/main.clj
+++ backend/src/gpml/seeder/main.clj
@@ -11,34 +11,51 @@
          :password "password"
          :stringtype "unspecified"})
 
+(defn parse-data [x]
+  (j/read-value x j/keyword-keys-object-mapper))
+
+(defn get-data [x]
+  (parse-data (slurp (io/resource (str "files/" x ".json")))))
+
 (defn seed-countries []
   (jdbc/delete! db :country [])
   (jdbc/insert-multi! db :country
     (map (fn [x] {:name (:name x) :iso_code (:code x)})
-      (j/read-value (slurp (io/resource "files/countries.json")) j/keyword-keys-object-mapper))))
+      (get-data "countries"))))
 
 (defn seed-country-groups []
   (jdbc/delete! db :country_group [])
   (jdbc/insert-multi! db :country_group
-    (j/read-value (slurp (io/resource "files/country_group.json")) j/keyword-keys-object-mapper)))
+    (get-data "country_group")))
 
 (defn seed-organisations []
   (jdbc/delete! db :organisation [])
   (jdbc/insert-multi! db :organisation
-    (j/read-value (slurp (io/resource "files/organisations.json")) j/keyword-keys-object-mapper)))
+    (get-data "organisations")))
 
 (defn seed-currencies []
   (jdbc/delete! db :currency [])
   (jdbc/insert-multi! db :currency
-    (j/read-value (slurp (io/resource "files/currencies.json")) j/keyword-keys-object-mapper)))
+    (get-data "currencies")))
 
-(defn seed-languages[] 
+(defn seed-languages [] 
   (jdbc/delete! db :language [])
   (jdbc/insert-multi! db :language
   (reduce (fn [acc [k v]]
     (conj acc {:iso_code (str/trim (name k)) :english_name (:name v) :native_name (:nativeName v)}))
     []
-    (j/read-value (slurp (io/resource "files/languages.json")) j/keyword-keys-object-mapper))))
+    (get-data "languages"))))
+
+(defn seed-tags []
+  (jdbc/delete! db :tag [])
+  (jdbc/delete! db :tag_category [])
+  (doseq [data (j/read-value (slurp (io/resource "files/tags.json")))]
+    (let [category (jdbc/insert! db :tag_category {:category (first data)})
+          category-id (-> category first :id)
+          tags (map (fn [e] (assoc {} :tag_category category-id :tag e)) (second data))]
+      (print tags)
+      (jdbc/insert-multi! db :tag tags))
+    ))
 
 (defn seed []
   (println "-- Start Seeding")
@@ -47,5 +64,6 @@
   (seed-currencies)
   (seed-organisations)
   (seed-languages)
+  (seed-tags)
   (println "-- Done Seeding")
   )
