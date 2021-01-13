14aed59ac141fc4385738a13edae40648e5de49a
Martin Christov <martin.christov@gmail.com>
2021-01-13 20:35:03 +0300

    header nav + antd

--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Input, Button } from 'antd'
+import { SearchOutlined, PlusOutlined, DownOutlined, CaretDownOutlined } from '@ant-design/icons'
+import 'antd/dist/antd.css';
 import Landing from './modules/landing/view'
 import Browse from './modules/browse/view'
 import logo from './images/GPML-logo.svg'
@@ -20,10 +23,15 @@ const Root = () => {
         </div>
         <header>
           <div className="ui container">
-          <Link to="/"><img src={logo} className="logo" alt="GPML" /></Link>
-          <nav>
-            {/* <Link to="/browse">Browse</Link> */}
-          </nav>
+            <Link to="/"><img src={logo} className="logo" alt="GPML" /></Link>
+            <Input className="src" placeholder="Search for topics" suffix={<SearchOutlined />} />
+            <nav>
+              <a href="#">Who we are</a>
+              <a href="#">What we do</a>
+              <a href="#">News</a>
+              <Link to="/browse">Find and Connect</Link>
+              <Button type="primary" size="large">+ Add <CaretDownOutlined /></Button>
+            </nav>
           </div>
         </header>
         <Route path="/" exact component={Landing} />
