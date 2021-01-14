686a480f20853ba8a012230081692f9885e940be
Iván Perdomo <ivan@perdomo.me>
2021-01-14 13:46:21 +0100

    [#7] Handle for text[] values

--- backend/src/gpml/pg_util.clj
+++ backend/src/gpml/pg_util.clj
@@ -1,12 +1,25 @@
 (ns gpml.pg-util
-  (:require [clojure.java.jdbc :as jdbc])
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.string :as str])
   (:import org.postgresql.jdbc.PgArray))
 
 (defn pgarray->val
   [^PgArray pgarray]
   (into [] (.getArray pgarray)))
 
+(defn val->pgarray
+  [value]
+  (format "{%s}"
+          (str/join ","
+                    (map #(str "\"" % "\"")
+                         value))))
+
 (extend-protocol jdbc/IResultSetReadColumn
   PgArray
-  (result-set-read-column [pgarray _ _]
-    (pgarray->val pgarray)))
+  (result-set-read-column [value _ _]
+    (pgarray->val value)))
+
+(extend-protocol jdbc/ISQLValue
+  clojure.lang.PersistentVector
+  (sql-value [value]
+    (val->pgarray value)))
