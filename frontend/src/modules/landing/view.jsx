35b8c708ec3900a3aa858abeb02967f2311582db
Martin Christov <martin.christov@gmail.com>
2021-01-14 13:20:26 +0300

    landing map overlay

--- frontend/src/modules/landing/view.jsx
+++ frontend/src/modules/landing/view.jsx
@@ -1,5 +1,7 @@
+import { Select } from 'antd';
 import React ,{useState} from 'react'
 import Maps from '../../components/Maps'
+import './styles.scss'
 
 const Landing = () => {
     const [country, setCountry] = useState("nothing");
@@ -19,6 +21,29 @@ const Landing = () => {
 
     return (
       <div id="landing">
+        <div className="map-overlay">
+          <Select placeholder="Countries" />
+          <div className="summary">
+            <header>Global summary</header>
+            <ul>
+              <li>
+                <b>18</b>
+                <div className="label">projects</div>
+                <span>in 35 countries</span>
+              </li>
+              <li>
+                <b>18</b>
+                <div className="label">projects</div>
+                <span>in 35 countries</span>
+              </li>
+              <li>
+                <b>18</b>
+                <div className="label">projects</div>
+                <span>in 35 countries</span>
+              </li>
+            </ul>
+          </div>
+        </div>
         <Maps
           data={data}
           clickEvents={clickEvents}
