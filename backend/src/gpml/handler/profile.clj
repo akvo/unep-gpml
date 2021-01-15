539b24b973d061972f20136df736735340632cf9
Iván Perdomo <ivan@perdomo.me>
2021-01-15 17:17:35 +0100

    [#10] Adds example profile endpoint

---
+++ backend/src/gpml/handler/profile.clj
@@ -0,0 +1,8 @@
+(ns gpml.handler.profile
+  (:require [integrant.core :as ig]
+            [ring.util.response :as resp]))
+
+
+(defmethod ig/init-key :gpml.handler.profile/handler [_ _]
+  (fn [_]
+    (resp/response {})))
