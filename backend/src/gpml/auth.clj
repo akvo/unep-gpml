add72442c933c6e4dae61ee080b8bde2d15a3396
Iván Perdomo <ivan@perdomo.me>
2021-01-18 19:34:49 +0100

    [#15] Close over a RS256SignatureVerifier
    * While the IdTokenVerifier is not thread-safe this class is:
      https://github.com/auth0/auth0-java/blob/1.25.0/src/main/java/com/auth0/utils/tokens/RS256SignatureVerifier.java#L14
    
    * We shouldn't create a new instance of this class because it depends
      on the public key, and we're getting it via JWKS, meaning a request
      to get the public key from the IdP.
    
    * By default the public key will be cached via JWK and is what we want

--- backend/src/gpml/auth.clj
+++ backend/src/gpml/auth.clj
@@ -18,19 +18,20 @@
        (ex-info "Invalid IdToken verifier options"
                 (malli/explain verifier-opts opts)))))
 
-(defn token-verifier [opts]
-  (let [{:keys [issuer audience]} opts
-        jwk-provider (-> issuer
+(defn signature-verifier [issuer]
+  (let [jwk-provider (-> issuer
                          (JwkProviderBuilder.)
                          (.build))
         public-key-provider (reify PublicKeyProvider
                               (getPublicKeyById [this key-id]
-                                (.getPublicKey (.get ^JwkProvider jwk-provider key-id))))
-        signature-verifier (SignatureVerifier/forRS256 public-key-provider)]
-    (.build
-     (IdTokenVerifier/init issuer
-                           audience
-                           signature-verifier))))
+                                (.getPublicKey (.get ^JwkProvider jwk-provider key-id))))]
+    (SignatureVerifier/forRS256 public-key-provider)))
+
+(defn token-verifier [{:keys [issuer audience signature-verifier]}]
+  (.build
+   (IdTokenVerifier/init issuer
+                         audience
+                         signature-verifier)))
 
 (defn get-claims
   [token]
@@ -49,18 +50,19 @@
 
 (defmethod ig/init-key :gpml.auth/auth-middleware [_ opts]
   (fn [handler]
-    (fn [request]
-      (if-let [auth-header (get-in request [:headers "authorization"])]
-        (let [id-token-verifier (token-verifier opts)
-              [_ token] (re-find #"^Bearer (\S+)$" auth-header)
-              [valid? msg] (try
-                             (.verify ^IdTokenVerifier id-token-verifier token)
-                             [true "OK"]
-                             (catch Exception e
-                               [false (.getMessage e)]))]
-          (if valid?
-            (handler (assoc request :jwt-claims (get-claims token)))
-            {:status 403
-             :body {:message msg}}))
-        {:status 401
-         :body {:message "Authentication required"}}))))
+    (let [signature-verifier (signature-verifier (:issuer opts))]
+      (fn [request]
+        (if-let [auth-header (get-in request [:headers "authorization"])]
+          (let [id-token-verifier (token-verifier (assoc opts :signature-verifier signature-verifier))
+                [_ token] (re-find #"^Bearer (\S+)$" auth-header)
+                [valid? msg] (try
+                               (.verify ^IdTokenVerifier id-token-verifier token)
+                               [true "OK"]
+                               (catch Exception e
+                                 [false (.getMessage e)]))]
+            (if valid?
+              (handler (assoc request :jwt-claims (get-claims token)))
+              {:status 403
+               :body {:message msg}}))
+          {:status 401
+           :body {:message "Authentication required"}})))))
