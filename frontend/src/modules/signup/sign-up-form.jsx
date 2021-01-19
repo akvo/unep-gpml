9d9b6f47b329773178d0ed2b12ed22a3f44d6b1b
dedenbangkit <mail@dedenbangkit.com>
2021-01-19 13:48:25 +0700

    [#10] modal signup hooks, auth0 custom widget

--- frontend/src/modules/signup/sign-up-form.jsx
+++ frontend/src/modules/signup/sign-up-form.jsx
@@ -1,23 +1,24 @@
-import React ,{ useEffect, useState } from 'react'
-import { withTheme } from '@rjsf/core';
-import { Theme as AntDTheme } from '@rjsf/antd';
-import axios from 'axios';
-import { ExclamationCircleFilled, CheckCircleFilled } from '@ant-design/icons';
-import { useAuth0 } from '@auth0/auth0-react';
-import { cloneDeep } from 'lodash'
+import React, { useEffect, useState } from "react";
+import { withTheme } from "@rjsf/core";
+import { Theme as AntDTheme } from "@rjsf/antd";
+import axios from "axios";
+import { ExclamationCircleFilled, CheckCircleFilled } from "@ant-design/icons";
+import { useAuth0 } from "@auth0/auth0-react";
+import { cloneDeep } from "lodash";
 
 const Form = withTheme(AntDTheme);
+
 const defaultSchema = {
     type: "object",
     properties: {
         profile: {
             type: "object",
             title: "Personal Details",
-            required: ["email","firstName", "lastName"],
+            required: ["email", "firstName", "lastName"],
             properties: {
                 email: {
                     type: "string",
-                    title: "EMAIL",
+                    title: "EMAIL"
                 },
                 firstName: {
                     type: "string",
@@ -29,16 +30,16 @@ const defaultSchema = {
                 },
                 linkedin: {
                     type: "string",
-                    title: "LINKEDIN",
+                    title: "LINKEDIN"
                 },
                 twitter: {
                     type: "string",
-                    title: "TWITTER",
+                    title: "TWITTER"
                 },
                 avatar: {
                     type: "string",
                     format: "data-url",
-                    title: "AVATAR PHOTO",
+                    title: "AVATAR PHOTO"
                 }
             }
         },
@@ -48,21 +49,22 @@ const defaultSchema = {
             properties: {
                 name: {
                     type: "string",
-                    title: "ORGANISATION NAME",
+                    title: "ORGANISATION NAME"
                 },
                 website: {
                     type: "string",
-                    title: "WEBSITE URL",
+                    format: "url",
+                    title: "WEBSITE URL"
                 },
                 sector: {
                     type: "string",
                     title: "REPRESENTATIVE SECTOR",
-                    enum: ["Sector 1","Sector 2"],
+                    enum: ["Sector 1", "Sector 2"]
                 },
                 country: {
                     type: "string",
                     title: "COUNTRY",
-                    enum: ["Loading"],
+                    enum: ["Loading"]
                 }
             }
         },
@@ -77,48 +79,44 @@ const defaultSchema = {
             }
         }
     }
-}
+};
 
 const defaultUISchema = {
     profile: {
         email: {
-            "ui:disabled":"true",
-            extraProps: {
-                kind: "auth0verify"
-            }
+            "ui:disabled": "true",
+            "ui:widget": "Auth0Widget"
         },
         twitter: {
-            "ui:placeholder": "Twitter Username",
+            "ui:placeholder": "Twitter Username"
         },
         linkedin: {
-            "ui:placeholder": "Linkedin Username",
+            "ui:placeholder": "Linkedin Username"
         },
         avatar: {
-            "ui:options": {accept: [".jpg",".png", ".webp"]},
+            "ui:options": { accept: [".jpg", ".png", ".webp"] },
             "ui:widget": "file"
         },
         extraProps: {
             kind: "title"
-        },
+        }
     },
     organisation: {
         name: {
-            "ui:placeholder": "Organsation Name",
+            "ui:placeholder": "Organsation Name"
         },
         website: {
             "ui:placeholder": "www.org.com",
         },
         sector: {
-            "ui:placeholder": "Select your activity sector",
+            "ui:placeholder": "Select your activity sector"
         },
         country: {
             "ui:placeholder": "Select your country",
-            allowSearch: true,
-            "ui:allowSearch": true
         },
         extraProps: {
             kind: "title"
-        },
+        }
     },
     other: {
         details: {
@@ -127,86 +125,96 @@ const defaultUISchema = {
         },
         extraProps: {
             kind: "title"
-        },
+        }
     },
     extraProps: {
         kind: "title"
-    },
-}
+    }
+};
 
-const CustomVerification = ({email}) => {
-    const { logout } = useAuth0();
-    return (
-        <div style={{marginTop: "10px"}}>
-            <span style={{color: email !== "Verified" ? "red" : "green"}}>
-            {email !== "Verified" ? <ExclamationCircleFilled/> : <CheckCircleFilled/>} {email}
-            </span>
-            {email !== "Verified" ? (<span style={{fontWeight: "bold"}}> or </span>) : ""}
-            <button style={{marginLeft: "10px"}} onClick={() => logout()}>use diffrent email</button>
-        </div>
-    )
+const Auth0Widget = (props) => {
+  const { logout } = useAuth0();
+  const email = props.schema.description;
+  return (
+    <>
+    <input type="text"
+        className="ant-input"
+        value={props.value}
+        required={props.required}
+        disabled={props.disabled}
+    onChange={(event) => props.onChange(event.target.value)} />
+    <div style={{ marginTop: "10px" }}>
+        <span style={{ color: email !== "Verified" ? "red" : "green" }}>
+            {email !== "Verified" ? <ExclamationCircleFilled /> : <CheckCircleFilled />} {email}
+        </span>
+        {email !== "Verified" ? <span style={{ fontWeight: "bold" }}> or </span> : ""}
+        <button style={{ marginLeft: "10px" }} onClick={() => logout()}>
+            use diffrent email
+        </button>
+    </div>
+    </>
+  );
+};
 
-}
+const widgets = {
+  Auth0Widget: Auth0Widget
+};
 
-const CustomOptional = () => {
-    return <span style={{color: "#c2c2c2", fontStyle: "italic"}}> - Optional</span>
-}
-
-const CustomFieldTemplate = ({id, label, help, required, description, errors, children, uiSchema}) => {
+const CustomFieldTemplate = ({ id, label, help, required, description, errors, children, uiSchema }) => {
     const title = uiSchema?.extraProps?.kind === "title";
-    const warning = uiSchema?.extraProps?.kind === "auth0verify" ? <CustomVerification email={description?.props?.description}/> : "";
     return (
-        <div style={{marginBottom: "10px"}}>
-            {title ? "" : (
-                <label htmlFor={id} style={title ? {color: "#00AAF1", fontWeight: "bold"} : {}}>
-                    {label}{required ? "" : <CustomOptional/>}
+        <div style={{ marginBottom: "10px" }}>
+            {title ? (
+                ""
+            ) : (
+                <label htmlFor={id} style={title ? { color: "#00AAF1", fontWeight: "bold" } : {}}>
+                    {label}
+                    {required ? "" : <span style={{ color: "#c2c2c2", fontStyle: "italic" }}> - Optional</span>}
                 </label>
             )}
             {children}
-            {warning}
             {errors}
             {help}
         </div>
     );
-}
+};
 
-const SignUpForm = () => {
-    const [schema, setSchema] = useState({schema: defaultSchema, loading: true, data: {}});
+const SignUpForm = ({initialData, updateData}) => {
+    const [schema, setSchema] = useState({ schema: defaultSchema, loading: true });
     const { getIdTokenClaims } = useAuth0();
 
     useEffect(() => {
         if (schema.loading) {
             (async function fetchData() {
-                const response = await axios.get('/api/country');
+                const response = await axios.get("/api/country");
                 const claims = await getIdTokenClaims();
-                const newSchema = cloneDeep(defaultSchema)
-                newSchema.properties.profile.properties.email.description = claims?.email_verified ? "Verified" : "Please confirm your email address"
-                newSchema.properties.organisation.properties.country.enum = response.data.map(x => x.name)
-                
-              setSchema({
-                ...schema, schema: newSchema, loading: false, data: {
-                  profile: {
-                    email: claims?.email || ""
-                  }}});
-            }());
+                const newSchema = cloneDeep(schema);
+                newSchema.schema.properties.profile.properties.email.description = claims?.email_verified ? "Verified" : "Please confirm your email address";
+                newSchema.schema.properties.organisation.properties.country.enum = response.data.map(x => x.name);
+                newSchema.loading = false;
+                updateData({
+                    profile: {
+                        email: claims?.email || "",
+                        firstName: claims?.given_name || "",
+                        lastName: claims?.family_name || ""
+                    }
+                })
+                setSchema(newSchema);
+            })();
         }
     }, []);
 
-    const onSubmit = ({formData}) => {
-        console.log(formData);
-    };
+    const onChange = ({formData}) => {
+        updateData(formData);
+    }
 
     return (
         <div className={schema.loading ? "hidden" : ""}>
-            <Form
-              schema={schema.schema}
-              onSubmit={onSubmit}
-              FieldTemplate={CustomFieldTemplate}
-              formData={schema.data}
-              uiSchema={defaultUISchema}
-            />
+            <Form schema={schema.schema} FieldTemplate={CustomFieldTemplate} uiSchema={defaultUISchema} formData={initialData} onChange={onChange} widgets={widgets}>
+                <button type="submit" style={{display: "none"}}>Fire</button>
+            </Form>
         </div>
-    )
-}
+    );
+};
 
 export default SignUpForm;
--- frontend/src/modules/signup/signup-modal.jsx
+++ frontend/src/modules/signup/signup-modal.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { Modal, Upload, Button } from 'antd'
-import { UploadOutlined } from '@ant-design/icons'
-import SignUpForm from './sign-up-form'
+import React, {useState } from "react";
+import { Modal, Upload, Button } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import SignUpForm from "./sign-up-form";
 
 const SignupModal = ({ visible, onCancel }) => {
-  return [
-    <Modal
-      {...{ visible, onCancel }}
-      width={600} title="Complete your signup"
-      okText="Submit"
-      onOk={() => {
-        // TODO: call form submit
-      }}
-    >
-      <Upload>
-        <Button icon={<UploadOutlined />}>Click to Upload</Button>
-      </Upload>
-      <SignUpForm />
-    </Modal>
-  ]
-}
+    const [data, updateData] = useState({});
+    return (
+        <Modal
+            {...{ visible, onCancel }}
+            width={600}
+            title="Complete your signup"
+            okText="Submit"
+            onOk={() => {
+                // TODO: call form submit
+                console.log(data);
+            }}
+        >
+            <Upload>
+                <Button icon={<UploadOutlined />}>Click to Upload</Button>
+            </Upload>
+            <SignUpForm updateData={updateData} initialData={data}/>
+        </Modal>
+    );
+};
 
-export default SignupModal
+export default SignupModal;
