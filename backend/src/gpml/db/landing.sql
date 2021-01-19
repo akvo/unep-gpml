99ae36514b6396bc95a3246db648050e2f4b6cd7
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-01-19 19:18:51 +0530

    [#12] SQL to return real data for summary info

--- backend/src/gpml/db/landing.sql
+++ backend/src/gpml/db/landing.sql
@@ -54,3 +54,48 @@ WITH resource_count AS (
      LEFT JOIN policy_count_group pcg ON pcg.country = c.id
      LEFT JOIN technology_count tc ON tc.country = c.id
      LEFT JOIN technology_count_group tcg ON tcg.country = c.id;
+
+-- :name summary
+-- :doc Get summary of count of entities and number of countries
+WITH
+resource_countries AS (
+    SELECT c.id FROM resource_geo_coverage r, country_group_countries cgc
+    JOIN country c ON cgc.country = c.id
+    WHERE r.country_group = cgc.country_group
+    UNION
+    SELECT r.country AS id from resource_geo_coverage r
+    WHERE r.country IS NOT NULL
+),
+event_countries AS (
+    SELECT c.id FROM event_geo_coverage e, country_group_countries cgc
+    JOIN country c ON cgc.country = c.id
+    WHERE e.country_group = cgc.country_group
+    UNION
+    SELECT e.country AS id from event_geo_coverage e
+    WHERE e.country IS NOT NULL
+),
+policy_countries AS (
+    SELECT c.id FROM policy_geo_coverage p, country_group_countries cgc
+    JOIN country c ON cgc.country = c.id
+    WHERE p.country_group = cgc.country_group
+    UNION
+    SELECT p.country AS id from policy_geo_coverage p
+    WHERE p.country IS NOT NULL
+),
+technology_countries AS (
+    SELECT c.id FROM technology_geo_coverage t, country_group_countries cgc
+    JOIN country c ON cgc.country = c.id
+    WHERE t.country_group = cgc.country_group
+    UNION
+    SELECT t.country AS id from technology_geo_coverage t
+    WHERE t.country IS NOT NULL
+)
+SELECT
+    (SELECT COUNT(*) FROM resource_countries) AS resource_countries,
+    (SELECT COUNT(*) FROM resource) AS resource,
+    (SELECT COUNT(*) FROM event_countries) AS event_countries,
+    (SELECT COUNT(*) FROM event) AS event,
+    (SELECT COUNT(*) FROM policy_countries) AS policy_countries,
+    (SELECT COUNT(*) FROM policy) AS policy,
+    (SELECT COUNT(*) FROM technology_countries) AS technology_countries,
+    (SELECT COUNT(*) FROM technology) AS technology
