akvo/unep-gpml

commit c4fa22dbcfbe046dc593cd15afb745a182e32f39

[#18] Enforce correct geo_coverage_type data and fix form

frontend/src/modules/events/form.jsx
@@ -67,7 +67,7 @@ const defaultFormSchema = [
   {
     city: { label: 'City' },
     country: { label: 'Country', control: 'select', showSearch: true, options: Object.keys(countries).map(iso2 => ({ value: countries2to3[iso2], label: countries[iso2].name})) },
-    geoCoverageType: { label: 'Geo coverage type', required: true, control: 'select', options: geoCoverageTypeOptions.map(it => ({ value: it, label: it })) },
+    geoCoverageType: { label: 'Geo coverage type', required: true, control: 'select', options: geoCoverageTypeOptions.map(it => ({ value: it.toLowerCase(), label: it })) },
     geoCoverageValue: {
       label: 'Geo coverage',
       render: GeoCoverageInput

backend/src/gpml/handler/event.clj
@@ -30,7 +30,7 @@
    [:description {:optional true} string?]
    [:image {:optional true} string?]
    [:remarks {:optional true} string?]
-   [:geo_coverage_type string?]
+   [:geo_coverage_type [:enum "global", "regional", "national", "transnational", "sub-national", "global with elements in specific areas"]]
    [:country {:optional true} int?]
    [:city {:optional true} string?]])
 

commit 50ed473d13f0ba53c42bd3593dbece341e013550

[#18] Fix broken geo coverage value input field
The input field was broken in commit
c4fa22dbcfbe046dc593cd15afb745a182e32f39. This commit fixes it.

frontend/src/modules/events/form.jsx
@@ -21,22 +21,22 @@ const GeoCoverageInput = (props) => {
       ({ input: typeInput }) => {
         return <Field name="geoCoverageValue" render={
           ({ input }) => {
-            if(typeInput.value === 'Global') return <Input disabled />
-            if (typeInput.value === 'Sub-national') return <Input placeholder="Type regions here..." {...input} />
+            if(typeInput.value === 'global') return <Input disabled />
+            if (typeInput.value === 'sub-national') return <Input placeholder="Type regions here..." {...input} />
             if(typeInput.value === 'Other') return <Input placeholder="Type here..." {...input} />
             const selectProps = {...input}
-            if(typeInput.value === 'Regional'){
+            if(typeInput.value === 'regional'){
               selectProps.options = regionOptions.map(it => ({ value: it, label: it }))
             }
-            else if(typeInput.value === 'National' || typeInput.value === 'Transnational'){
+            else if(typeInput.value === 'national' || typeInput.value === 'transnational'){
               selectProps.options = Object.keys(countries).map(iso2 => ({ value: countries2to3[iso2], label: countries[iso2].name }))
               selectProps.showSearch = true
               selectProps.filterOption = (input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              if (typeInput.value === 'Transnational'){
+              if (typeInput.value === 'transnational'){
                 selectProps.mode = 'multiple'
               }
             }
-            else if (typeInput.value === 'Global with elements in specific areas'){
+            else if (typeInput.value === 'global with elements in specific areas'){
               selectProps.options = specificAreasOptions.map(it => ({ value: it, label: it }))
             }
             return <Select {...selectProps} />