4ee10d9e5ebdedf1cbfdc79061a14992ce6c8915
Martin Christov <martin.christov@gmail.com>
2021-01-26 16:41:27 +0200

    detach SignupForm from SignupModal

--- frontend/src/modules/signup/signup-form.jsx
+++ frontend/src/modules/signup/signup-form.jsx
@@ -1,154 +1,86 @@
-import React, { useEffect, useState } from "react";
-import { withTheme } from "@rjsf/core";
-import { Theme as AntDTheme } from "@rjsf/antd";
-import { cloneDeep } from "lodash";
-import widgets from "../../utils/forms";
-import ObjectFieldTemplate from "../../utils/forms/object-field-tpl";
-import api from "../../utils/api";
+import React, { useState } from "react";
+import { Form, Input, Select } from "antd";
+import { Form as FinalForm, Field } from 'react-final-form'
+import { createForm } from 'final-form'
+import './styles.scss'
+import Checkbox from "antd/lib/checkbox/Checkbox";
+import { LinkedinOutlined, TwitterOutlined } from "@ant-design/icons";
+import { FieldsFromSchema, validateSchema } from "../../utils/form-utils";
+import { countries } from 'countries-list'
+import countries2to3 from 'countries-list/dist/countries2to3.json'
 
-const Form = withTheme(AntDTheme);
+const sectorOptions = ['Governments', 'Private Sector', 'NGOs and MGS', 'Academia and Scientific Community', 'IGOs and multi - lateral processes', 'Other']
+const TitleNameGroup = (props) => {
+  return (
+    <Input.Group compact className="title-name-group">
+      <Field
+        name="title"
+        render={({ input }) => <Select {...input} placeholder="Title" options={['Mr', 'Mrs', 'Ms', 'Dr', 'Prof'].map(it => ({ value: it, label: it }))} />}
+      />
+      <Field
+        name="firstName"
+        render={({ input }) => <Input {...input} style={{ flex: 1 }} />}
+      />
+    </Input.Group>
+  )
+}
+const formSchema = [
+  {
+    firstName: { label: 'First name', required: true, render: TitleNameGroup },
+    lastName: { label: 'Last name', required: true },
+    linkedIn: { label: 'LinkedIn', prefix: <LinkedinOutlined /> },
+    twitter: { label: 'Twitter', prefix: <TwitterOutlined /> },
+    photo: { label: 'Photo', control: 'file' },
+    representation: { label: 'Representative sector', required: true, control: 'select', options: sectorOptions.map(it => ({ value: it, label: it })) },
+    country: { label: 'Country', control: 'select', showSearch: true, options: Object.keys(countries).map(iso2 => ({ value: countries2to3[iso2], label: countries[iso2].name })), autoComplete: 'off' }
+  },
+  {
+    'org.name': { label: 'Organisation name' },
+    'org.url': { label: 'Organisation URL', addonBefore: 'https://' },
+  },
+  {
+    about: { label: 'About yourself', control: 'textarea', required: true }
+  }
+]
 
-const defaultSchema = {
-    type: "object",
-    properties : {
-        profile: {
-            type: "object",
-            title: "Personal Details",
-            required: ["email", "firstName", "lastName"],
-            properties: {
-                firstName: {
-                    type: "string",
-                    title: "First name"
-                },
-                lastName: {
-                    type: "string",
-                    title: "Last name"
-                },
-                linkedin: {
-                    type: "string",
-                    title: "LinkedIn"
-                },
-                twitter: {
-                    type: "string",
-                    title: "Twitter"
-                },
-                avatar: {
-                    type: "string",
-                    format: "data-url",
-                    title: "Avatar photo"
-                }
-            }
-        },
-        organisation: {
-            type: "object",
-            title: "Organisation Details",
-            properties: {
-                name: {
-                    type: "string",
-                    title: "Organisation Name"
-                },
-                website: {
-                    type: "string",
-                    title: "Website URL"
-                },
-                sector: {
-                    type: "string",
-                    title: "Representative sector",
-                    enum: ["Sector 1", "Sector 2"]
-                },
-                country: {
-                    type: "string",
-                    title: "Country",
-                    enum: ["Loading"]
-                }
-            }
-        },
-        other: {
-            type: "object",
-            title: "Other",
-            required: ["details"],
-            properties: {
-                details: {
-                    type: "string",
-                    title: "Tell about yourself"
-                }
-            }
+const SignupForm = ({ onSubmit, formRef }) => {
+  const [noOrg, setNoOrg] = useState(false)
+
+  const form = createForm({
+    subscription: {},
+    initialValues: { title: null },
+    onSubmit, validate: validateSchema(formSchema.reduce((acc, val) => ({ ...acc, ...val }), {})) // combined formSchema sections
+  })
+
+  if(formRef) formRef(form)
+  return (
+    <Form layout="vertical">
+      <FinalForm
+        form={form}
+        render={
+          () => {
+            return (
+              <div>
+                <div className="section">
+                  <h2>Personal details</h2>
+                  <FieldsFromSchema schema={formSchema[0]} />
+                </div>
+                <div className="section">
+                  <h2>Organisation details</h2>
+                  <Checkbox className="org-check" checked={noOrg} onChange={({ target: { checked } }) => setNoOrg(checked)}>I don't belong to an organisation</Checkbox>
+                  {!noOrg && <FieldsFromSchema schema={formSchema[1]} />}
+                </div>
+                <div className="section">
+                  <h2>Other</h2>
+                  <FieldsFromSchema schema={formSchema[2]} />
+                </div>
+              </div>
+            )
+          }
         }
-    }
-};
+      />
+    </Form>
+  )
+}
 
-const defaultUISchema = {
-    profile: {
-        email: {
-            "ui:disabled": "true",
-            "ui:widget": "Auth0Widget"
-        },
-        twitter: {
-            "ui:placeholder": "Twitter Username"
-        },
-        linkedin: {
-            "ui:placeholder": "Linkedin Username"
-        },
-        avatar: {
-            "ui:options": { accept: [".jpg", ".png", ".webp"] },
-            "ui:widget": "file"
-        },
-    },
-    organisation: {
-        name: {
-            "ui:placeholder": "Organsation Name"
-        },
-        website: {
-            "ui:placeholder": "www.org.com",
-        },
-        sector: {
-            "ui:placeholder": "Select your activity sector"
-        },
-        country: {
-            "ui:placeholder": "Select your country",
-            "ui:showSearch": true
-        },
-    },
-    other: {
-        details: {
-            "ui:widget": "textarea",
-            "ui:placeholder": "Max 999 character"
-        },
-    },
-};
-
-const SignUpForm = ({initialData, dispatchData}) => {
-    const [schema, setSchema] = useState({ schema: defaultSchema, loading: true });
-
-    useEffect(() => {
-        if (schema.loading) {
-            (async function fetchData() {
-                const response = await api.get('/country')
-                const newSchema = cloneDeep(schema);
-                newSchema.schema.properties.organisation.properties.country.enum = response.data.map(x => x.name);
-                newSchema.loading = false;
-                setSchema(newSchema);
-            })();
-        }
-    }, [schema]);
-
-    const onChange = ({formData}) => {
-        dispatchData(formData);
-    }
-
-    return (
-        <div className={schema.loading ? "hidden" : ""}>
-            <Form
-              idPrefix="signup_"
-              schema={schema.schema}
-              ObjectFieldTemplate={ObjectFieldTemplate}
-              uiSchema={defaultUISchema}
-              formData={initialData}
-              onChange={onChange}
-              widgets={widgets}>
-            </Form>
-        </div>
-    );
-};
-
-export default SignUpForm;
+export default SignupForm
--- frontend/src/modules/signup/signup-modal.jsx
+++ frontend/src/modules/signup/signup-modal.jsx
@@ -1,64 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Modal, Form, Input, Select, Button } from "antd";
-import { Form as FinalForm, Field } from 'react-final-form'
-import { createForm } from 'final-form'
+import React, { useState } from "react";
+import { Modal, Button } from "antd";
 import api from "../../utils/api";
-import { cloneDeep } from "lodash";
 import './styles.scss'
-import Checkbox from "antd/lib/checkbox/Checkbox";
-import { LinkedinOutlined, TwitterOutlined } from "@ant-design/icons";
-import { FieldsFromSchema, validateSchema } from "../../utils/form-utils";
 import { useAuth0 } from '@auth0/auth0-react';
+import SignupForm from "./signup-form";
+import { useRef } from "react";
 
-const sectorOptions = ['Governments', 'Private Sector', 'NGOs and MGS', 'Academia and Scientific Community', 'IGOs and multi - lateral processes', 'Other']
-const TitleNameGroup = (props) => {
-  return (
-    <Input.Group compact className="title-name-group">
-      <Field
-        name="title"
-        render={({ input }) => <Select {...input} defaultValue="Mr" options={['Mr', 'Mrs', 'Ms', 'Dr', 'Prof'].map(it => ({ value: it, label: it }))} />}
-      />
-      <Field
-        name="firstName"
-        render={({ input }) => <Input {...input} style={{ flex: 1 }} />}
-      />
-    </Input.Group>
-  )
-}
-const defaultFormSchema = [
-  {
-    firstName: { label: 'First name', required: true, render: TitleNameGroup },
-    lastName: { label: 'Last name', required: true},
-    linkedIn: { label: 'LinkedIn', prefix: <LinkedinOutlined />},
-    twitter: { label: 'Twitter', prefix: <TwitterOutlined /> },
-    photo: { label: 'Photo', control: 'file' },
-    representation: { label: 'Representative sector', required: true, control: 'select', options: sectorOptions.map(it => ({ value: it, label: it })) },
-    country: { label: 'Country', control: 'select', showSearch: true, options: [{ value: 'loading', label: 'Loading' }], autoComplete: 'off' }
-  },
-  {
-    'org.name': { label: 'Organisation name'},
-    'org.url': { label: 'Organisation URL', addonBefore: 'https://' },
-  },
-  {
-    about: { label: 'About yourself', control: 'textarea', required: true }
-  }
-]
 
 const SignupModal = ({ visible, onCancel }) => {
-  const [formSchema, setFormSchema] = useState(defaultFormSchema)
-  const [noOrg, setNoOrg] = useState(false)
-  const [sending, setSending] = useState(false)
-  const [step, setStep] = useState(1)
-  const {user} = useAuth0();
-
-  useEffect(() => {
-    (async function fetchData() {
-      const response = await api.get('/country')
-      const newSchema = cloneDeep(defaultFormSchema);
-      newSchema[0].country.options = response.data.map(x => ({ value: x.isoCode, label: x.name}));
-      setFormSchema(newSchema);
-    })()
-  }, []);
+    const [sending, setSending] = useState(false)
+    const [step, setStep] = useState(1)
+    const {user} = useAuth0();
+    const formRef = useRef()
     const onSubmit = (vals) => {
       setSending(true)
       api.post('/profile', vals)
@@ -70,11 +23,6 @@ const SignupModal = ({ visible, onCancel }) => {
         setSending(false)
       })
     }
-    const form = createForm({
-      subscription: {},
-      initialValues: { title: 'Mr' },
-      onSubmit, validate: validateSchema(formSchema.reduce((acc, val) => ({...acc, ...val}), {})) // combined formSchema sections
-    })
     return (
         <Modal
           {...{ visible, onCancel }}
@@ -83,38 +31,13 @@ const SignupModal = ({ visible, onCancel }) => {
           okText="Submit"
           className="signup-modal"
           onOk={() => {
-            form.submit()
+            formRef.current.submit()
           }}
           confirmLoading={sending}
           footer={step === 2 ? (<Button onClick={onCancel}>Close</Button>) : undefined}
         >
           {step === 1 &&
-          <Form layout="vertical">
-            <FinalForm
-              form={form}
-              render={
-                () => {
-                  return (
-                    <div>
-                    <div className="section">
-                      <h2>Personal details</h2>
-                      <FieldsFromSchema schema={formSchema[0]} />
-                    </div>
-                    <div className="section">
-                      <h2>Organisation details</h2>
-                      <Checkbox className="org-check" checked={noOrg} onChange={({ target: { checked } }) => setNoOrg(checked)}>I don't belong to an organisation</Checkbox>
-                      {!noOrg && <FieldsFromSchema schema={formSchema[1]} />}
-                    </div>
-                    <div className="section">
-                      <h2>Other</h2>
-                      <FieldsFromSchema schema={formSchema[2]} />
-                    </div>
-                    </div>
-                  )
-                }
-              }
-            />
-          </Form>
+            <SignupForm onSubmit={onSubmit} formRef={ref => { formRef.current = ref }} />
           }
           {step === 2 &&
           <div className="submitted">
