akvo/unep-gpml

commit 876684db8a418fd9fb5e3871215e90d5224d9496

[#12] Remove country names from /api/landing

frontend/src/modules/landing/view.jsx
@@ -8,6 +8,7 @@ import api from '../../utils/api';
 
 const Landing = ({ history }) => {
     const [country, setCountry] = useState(null);
+    const [countries, setCountries] = useState(null);
     const [data, setData] = useState(null)
 
     const clickEvents = ({name, data}) => {
@@ -20,9 +21,11 @@ const Landing = ({ history }) => {
     const toolTip = (params) => {
         const summary = data?.map.find(it => it.isoCode === params.name)
         if(summary){
+          const countryInfo = countries?.find(it => it.isoCode === summary.isoCode)
+          const countryName = countryInfo?.name || summary.isoCode
           return `
             <div class="map-tooltip">
-              <h3>${summary.name}</h3>
+              <h3>${countryName}</h3>
               <ul>
               ${topicTypes.map(topic => `<li><span>${topic}</span><b>${summary[topic]}</b></li>`).join('')}
               </ul>
@@ -37,8 +40,14 @@ const Landing = ({ history }) => {
       .then((resp) => {
         setData(resp.data)
       })
+
+      api.get('/country')
+      .then((resp) => {
+        setCountries(resp.data)
+      })
+
     }, [])
-    
+
     const handleChangeCountry = (isoCode) => {
       setCountry(isoCode)
     }

backend/src/gpml/handler/landing.clj
@@ -2,16 +2,7 @@
   (:require [integrant.core :as ig]
             [ring.util.response :as resp]
             [gpml.db.browse :as db.browse]
-            [gpml.db.country :as db.country]
             [gpml.db.landing :as db.landing]))
-
-(defn map-counts-with-country-names [conn]
-  (let [countries (db.country/all-countries conn)
-        names (->> countries
-                   (map #(assoc {} (:iso_code %) (:name %)))
-                   (apply merge))
-        counts (db.landing/map-counts-grouped conn)]
-    (map #(merge {:name (names (:iso_code %))} %) counts)))
 
 (defn landing-response [conn]
   (let [topics '("project" "event" "policy" "resource" "technology")
@@ -30,17 +21,10 @@
                          flatten
                          (map #(merge (:json %) {:topic_type (:topic %)})))]
     (resp/response {:topics topics-data
-                    :map (map-counts-with-country-names conn)
+                    :map (db.landing/map-counts-grouped conn)
                     :summary summary-data})))
 
 (defmethod ig/init-key :gpml.handler.landing/get [_ {:keys [db]}]
   (fn [_]
     (let [conn (:spec db)]
       (landing-response conn))))
-
-(comment
-  (require 'dev)
-  (let [db (dev/db-conn)]
-    (map-counts-with-country-names db))
-  (rand-int 10)
-  )

commit 4094afb4ae59fe4b2cb2bd66a751dd3990d11008

[#12] Fix country filter on landing page
This was broken in commit 876684db8a418fd9fb5e3871215e90d5224d9496
which removed the country names from /api/landing end-point

frontend/src/modules/landing/view.jsx
@@ -52,8 +52,8 @@ const Landing = ({ history }) => {
     const handleChangeCountry = (isoCode) => {
       setCountry(isoCode)
     }
-    const countryOpts = data ? data.map.map(it => ({ value: it.isoCode, label: it.name })) : []
-    const countryObj = data && country && data.map.find(it => it.isoCode === country)
+    const countryOpts = countries ? countries.map(it => ({ value: it.isoCode, label: it.name })) : []
+    const countryObj = countries && country && countries.find(it => it.isoCode === country)
 
     const handleSummaryClick = (dataType) => {
         const selected = data.map.map(x => ({