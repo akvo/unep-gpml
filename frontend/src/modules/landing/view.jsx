630409730f871d35835c6c26cca2843223b2a306
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-01-28 18:03:32 +0530

    [#35] Fix broken checked toggle on summary click
    This was broken in 46dd62dd7f75ab440ca31a26e5ced49e8d711b9f

--- frontend/src/modules/landing/view.jsx
+++ frontend/src/modules/landing/view.jsx
@@ -110,7 +110,7 @@ const Summary = ({ clickEvents, summary, country, counts, selected }) => {
       <ul>
         {!country && summary.map((it, index) =>
           <li key={`li-${index}`} onClick={e => clickEvents(Object.keys(it)[0])}>
-            <Switch size="small" checked={counts === topicNames[Object.keys(it)[0].toUpperCase()]} />
+            <Switch size="small" checked={counts === Object.keys(it)[0].toUpperCase()} />
             <div className="text">
               <div className="label">{topicNames[Object.keys(it)[0]]}</div>
               <span>in {it.countries} countries</span>
