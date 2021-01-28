02c2f17008605461c702ebc4d639ad53d299a447
Martin Christov <martin.christov@gmail.com>
2021-01-28 12:33:10 +0200

    admin pending items use collapse

--- frontend/src/modules/profile/admin.jsx
+++ frontend/src/modules/profile/admin.jsx
@@ -1,16 +1,15 @@
-import { Button, Space, Table } from 'antd'
+import { Button, Collapse, Space } from 'antd'
 import React from 'react'
 import { useEffect, useState } from 'react'
 import api from '../../utils/api'
 
-const { Column, ColumnGroup } = Table;
 
 const AdminSection = () => {
   const [pendingItems, setPendingItems] = useState([])
   useEffect(() => {
     api.get('/profile/pending')
     .then((d) => {
-      setPendingItems(d.data.map(it => ({ type: 'stakeholder', id: it.id, title: `${it.firstName} ${it.lastName}` })))
+      setPendingItems(d.data.map(it => ({ type: 'stakeholder', ...it, title: `${it.firstName} ${it.lastName}` })))
     })
   }, [])
   const approve = (item) => () => {
@@ -22,22 +21,44 @@ const AdminSection = () => {
     }
   }
   return (
-    <div>
+    <div className="admin-view">
       <h2>New approval requests</h2>
-      <Table dataSource={pendingItems}>
-        <Column title="Type" dataIndex="type" key="type" />
-        <Column title="Title" dataIndex="title" key="title" />
-        <Column
-          title="Action"
-          key="action"
-          render={(text, record) => (
-            <Space size="middle">
-              <Button type="primary" onClick={approve(record)}>Approve</Button>
-              <Button type="link">Decline</Button>
-            </Space>
+      <div className="row head">
+        <div className="col">Type</div>
+        <div className="col">Title</div>
+        <div className="col">Action</div>
+      </div>
+      <Collapse>
+      {pendingItems.map(item =>
+      <Collapse.Panel
+        header={
+          <div className="row">
+            <div className="col">{item.type}</div>
+            <div className="col">{item.title}</div>
+            <div className="col" onClick={e => { e.stopPropagation() }}>
+              <Space size="middle">
+                <Button type="primary" onClick={approve(item)}>Approve</Button>
+                <Button type="link">Decline</Button>
+              </Space>
+            </div>
+          </div>
+        }
+      >
+        <div>
+          {item.type === 'stakeholder' && (
+            <div className="stakeholder-info">
+              {item.photo && <img src={item.photo} />}
+              <ul>
+                <li>{item.email}</li>
+                <li>{item.about}</li>
+                {item.org && <li><a href={item.org.url} target="_blank">{item.org.name}</a></li>}
+              </ul>
+            </div>
           )}
-        />
-      </Table>
+        </div>
+      </Collapse.Panel>
+      )}
+      </Collapse>
     </div>
   )
 }
