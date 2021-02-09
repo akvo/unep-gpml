1fcfb62dc1fd12e6be2fa71195136f5b17896a4b
Iván Perdomo <ivan@perdomo.me>
2021-02-09 15:45:15 +0100

    [#1] Check for duplicated migration prefix

--- backend/release.sh
+++ backend/release.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+#shellcheck disable=SC1010
 
 set -euo pipefail
 
+find ./resources/migrations/ -name '*.up.sql' | \
+    awk -F '/' '{print substr($4,1,3)}' | \
+    sort --numeric-sort | \
+    uniq --repeated > /tmp/duplicated
+
+if [[ -s /tmp/duplicated  ]]; then
+    echo "Error: Duplicated migration prefix"
+    cat /tmp/duplicated
+    exit 1
+fi
 
 # The only reason to build the uberjar is to get the duct_hierarhcy.edn file
 # More info: https://github.com/duct-framework/duct/issues/102#issuecomment-555545655
