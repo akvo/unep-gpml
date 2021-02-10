akvo/unep-gpml

commit 05cb890d85cec19e397cdf993bf6a72fdce61da1

[#53] Upload filesize limit validation

frontend/src/modules/events/form.jsx
@@ -81,6 +81,12 @@ const defaultFormSchema = [
   }
 ]
 
+const validation = (formSchema) => {
+    validateSchema(formSchema.reduce((acc, val) =>
+        ({...acc, ...val}), {}
+    ))
+}
+
 const AddEventForm = () => {
   const [formSchema, setFormSchema] = useState(defaultFormSchema)
   const [sending, setSending] = useState(false)
@@ -116,7 +122,7 @@ const AddEventForm = () => {
     mutators: {
     ...arrayMutators
     },
-    onSubmit, validate: validateSchema(formSchema.reduce((acc, val) => ({ ...acc, ...val }), {})) // combined formSchema sections
+    onSubmit, validate: validation(formSchema)
   })
   return (
     <div className="add-event-form">

frontend/src/modules/events/event-form.jsx
@@ -155,7 +155,6 @@ const EventForm = () => {
     }
 
     const sendData = () => {
-        console.log(data);
         (async function() {
             const payload = {
                 title: data.details?.title || null,
@@ -168,7 +167,6 @@ const EventForm = () => {
                 country: data.location?.country || null,
                 city: data.location?.city || null,
             };
-            console.log(payload);
             const response = await axios.post("/api/event", payload);
             console.log(response);
         })();

frontend/src/utils/final-field.jsx
@@ -50,7 +50,7 @@ const CONTROLS = {
       </Select>
     )
   },
-  file: ({ input, accept, maxFileSize}) => {
+  file: ({ input, accept, maxFileSize, ...props}) => {
     return (
       <FileWidget {...input} accept={accept} maxFileSize={maxFileSize}/>
     )

frontend/src/utils/form-utils.js
@@ -1,6 +1,12 @@
 import FinalField from "./final-field"
 import FinalFieldArray from "./final-field-array"
 
+const checkFileSize = (base64, maxFileSize) => {
+    const base64str = base64.split(',')[1];
+    const decoded = atob(base64str);
+    return decoded.length >= (maxFileSize * 1000000);
+}
+
 export const FieldsFromSchema = ({ schema, mutators }) => {
   return Object.keys(schema).map((name,i) => {
     const Comp = schema[name].type === 'array' ? FinalFieldArray : FinalField
@@ -10,8 +16,15 @@ export const FieldsFromSchema = ({ schema, mutators }) => {
 
 export const validateSchema = (schema) => (values) => {
   const errors = {}
-  Object.keys(schema).filter(it => schema[it].required).forEach(itemName => {
-    if (!values[itemName]) {
+  Object.keys(schema).forEach(itemName => {
+    const value = values[itemName]
+    if (value && schema[itemName]?.maxFileSize) {
+      const invalidSize = checkFileSize(value, schema[itemName].maxFileSize);
+      if (invalidSize) {
+        errors[itemName] = 'Allowed file size: ' + schema[itemName].maxFileSize + 'mb';
+      }
+    }
+    if (!value && schema[itemName]?.required) {
       errors[itemName] = 'Required'
     }
   })

frontend/src/utils/forms/form-file.js
@@ -6,9 +6,14 @@ import { Button } from "antd";
 const getBase64 = (file) => {
     return new Promise ((resolve, reject) => {
        var reader = new FileReader();
-       reader.readAsDataURL(file);
-       reader.onload = () => resolve(reader.result);
-       reader.onerror = () => reject(reader.result)
+       if (file) {
+           reader.readAsDataURL(file);
+           reader.onload = () => resolve(reader.result);
+           reader.onerror = () => reject(reader.result)
+       }
+       if (!file){
+           reject('discard')
+       }
     });
 }
 
@@ -31,7 +36,7 @@ const FileWidget = (props) => {
         base64.then(res => {
             setOutput(res);
             props.onChange(res);
-        })
+        }).catch(err => console.log(err))
     }
     return (
         <div className="photo-upload">

commit ecf840057788e943d58e2d5669d54e6f91a77c24

[#53] Fix broken validation for Event form
05cb890d85cec19e397cdf993bf6a72fdce61da1 broke the validation because
of the missing return.

frontend/src/modules/events/form.jsx
@@ -82,7 +82,7 @@ const defaultFormSchema = [
 ]
 
 const validation = (formSchema) => {
-    validateSchema(formSchema.reduce((acc, val) =>
+    return validateSchema(formSchema.reduce((acc, val) =>
         ({...acc, ...val}), {}
     ))
 }