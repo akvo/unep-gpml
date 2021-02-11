0b650a44ca4fd6a2f53495372439a8dcae6a7057
dedenbangkit <mail@dedenbangkit.com>
2021-02-11 18:00:59 +0700

    [#77] Add /cv endpoint to frontend

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    ['/api', '/image', '/env.js'],
+    ['/api', '/image', '/env.js','/cv'],
     createProxyMiddleware({
       target: 'http://backend:3000',
       changeOrigin: true,
