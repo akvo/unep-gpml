991a9047ea2f7d4e152c1ee3c42dc8038d7dcfc8
Daniel Lebrero Berna <dlebrero@gmail.com>
2021-02-23 09:07:30 +0100

    [#118] Properly quote the dc.sh params

--- dc.sh
+++ dc.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-COMPOSE_HTTP_TIMEOUT=180 docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose-docker-sync.yml $@
+COMPOSE_HTTP_TIMEOUT=180 docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose-docker-sync.yml "$@"
