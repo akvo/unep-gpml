2d4754b1b7d6de7027d8dd20e3e4149f765d7f0b
Daniel Lebrero Berna <dlebrero@gmail.com>
2021-02-24 19:39:33 +0100

    [#119] Add url and headquarters to tech

--- backend/src/gpml/db/technology.sql
+++ backend/src/gpml/db/technology.sql
@@ -1,4 +1,4 @@
 -- :name new-technology :<! :1
 -- :doc Insert a new policies
-insert into technology(name, year_founded, country, organisation_type, development_stage, specifications_provided, email, geo_coverage_type, attachments, remarks, review_status)
-values(:name, :year_founded, :country, :organisation_type, :development_stage, :v:specifications_provided::boolean, :email, :v:geo_coverage_type::geo_coverage_type, :v:attachments::text[], :remarks, :v:review_status::review_status) returning id;
+insert into technology(name, year_founded, country, organisation_type, development_stage, specifications_provided, email, geo_coverage_type, attachments, remarks, review_status, url)
+values(:name, :year_founded, :country, :organisation_type, :development_stage, :v:specifications_provided::boolean, :email, :v:geo_coverage_type::geo_coverage_type, :v:attachments::text[], :remarks, :v:review_status::review_status, :url) returning id;
