e39cd92ba3f0561e3c2cd78b7f94d2cd6de996f4
Iván Perdomo <ivan@perdomo.me>
2021-03-04 11:41:59 +0100

    [#1] Add Emacs/CIDER config

---
+++ backend/.dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((cider-ns-refresh-before-fn . "integrant.repl/suspend")
+         (cider-ns-refresh-after-fn  . "integrant.repl/resume")
+	 (eval . (customize-set-variable 'cider-path-translations
+					 (let ((m2 (concat (getenv "HOME") "/.m2")))
+					   (list
+					    (cons "/app" (clojure-project-dir))
+					    (cons "/home/akvo/.m2" m2)
+					    (cons "/root/.m2" m2))))))))
