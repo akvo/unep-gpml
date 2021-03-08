8555de8e43c16c4e363836f39e574fa84be65f19
dedenbangkit <mail@dedenbangkit.com>
2021-03-08 18:33:47 +0700

    [#115] Refactor stakeholder exporter query

--- backend/dev/src/gpml/exporter/main.sql
+++ backend/dev/src/gpml/exporter/main.sql
@@ -1,10 +1,12 @@
 --:name get-stakeholder-info :? :*
 SELECT
+    s.id,
     s.picture,
     s.title,
     s.first_name,
     s.last_name,
     s.email,
+    s.public_email,
     s.linked_in,
     s.twitter,
     s.url,
@@ -16,22 +18,41 @@ SELECT
     s.reviewed_at,
     s.role,
     s.cv,
-    r.email as reviewed_by,
+    sr.email as reviewed_by,
     s.review_status,
-    s.geo_coverage_values,
-    s.tags,
-    o.name as organisation,
-    c.name as country,
-    sa.associations
-FROM (v_stakeholder_data s
-    LEFT JOIN country c ON s.country = c.id
-    LEFT JOIN organisation o ON s.affiliation = o.id
-    LEFT JOIN stakeholder r ON s.id = r.id
-    LEFT JOIN (
-        SELECT sa.stakeholder, json_agg(sa.*) AS associations
-        FROM v_stakeholder_association sa
-        GROUP BY sa.stakeholder) sa ON (s.id = sa.stakeholder)
-)
+    c.iso_code AS country,
+    geo.geo_coverage_values,
+    row_to_json(o.*) AS affiliation,
+    tag.tags,
+    association.associations
+    FROM ((((
+        stakeholder s
+        LEFT JOIN (
+            SELECT vsa.stakeholder, json_agg(vsa.*) AS associations
+            FROM v_stakeholder_association vsa
+            GROUP BY vsa.stakeholder
+        ) association ON ((s.id = association.stakeholder))
+        LEFT JOIN (
+             SELECT st.stakeholder,
+                    json_agg(t.*) AS tags
+             FROM (stakeholder_tag st JOIN (
+                 SELECT tg.id,
+                        tg.tag,
+                        tc.category FROM tag tg
+                 LEFT JOIN tag_category tc
+                 ON tg.tag_category = tc.id) t ON ((st.tag = t.id)))
+             GROUP BY st.stakeholder) tag ON ((s.id = tag.stakeholder)))
+        LEFT JOIN (
+            SELECT sg.stakeholder,
+            json_agg(COALESCE(c_1.iso_code, (cg.name)::bpchar)) AS geo_coverage_values
+            FROM ((stakeholder_geo_coverage sg
+                   LEFT JOIN country c_1 ON ((sg.country = c_1.id)))
+                  LEFT JOIN country_group cg ON ((sg.country_group = cg.id)))
+            GROUP BY sg.stakeholder) geo ON ((s.id = geo.stakeholder)))
+    LEFT JOIN country c ON ((s.country = c.id)))
+    LEFT JOIN organisation o ON ((s.affiliation = o.id)))
+LEFT JOIN stakeholder sr ON s.reviewed_by = sr.id
+ORDER BY s.created;
 
 
 --:name get-topic-by-id :? :1
