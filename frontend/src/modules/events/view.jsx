60d92936ed4ff943a14eb232184b25d2d7ba524e
wayangalihpratama <wgprtm@gmail.com>
2021-04-02 12:00:28 +0800

    [#161] Refactor mobile view on event form

--- frontend/src/modules/events/view.jsx
+++ frontend/src/modules/events/view.jsx
@@ -12,10 +12,10 @@ const AddEvent = ({ ...props }) => {
     <div id="add-event">
       <div className="ui container">
         <Row>
-          <Col span={12} xs={24}>
+          <Col xs={24} md={12}>
             <h1>Add event</h1>
           </Col>
-          <Col span={12} xs={24}>
+          <Col xs={24} md={12}>
             <Card>
               <AddEventForm />
             </Card>
