38af12f229c9d50fdac9aca8a5c3472b00d9c12a
dedenbangkit <mail@dedenbangkit.com>
2021-04-07 14:29:11 +0700

    [#176] Reformat currency label

--- backend/src/gpml/db/currency.sql
+++ backend/src/gpml/db/currency.sql
@@ -4,4 +4,4 @@ values (:name, :iso_code)
 
 -- :name all-currencies :? :*
 -- :doc Get all currency
-select iso_code as value, name as label from currency;
+select iso_code as value, iso_code || ' '|| name as label from currency;
