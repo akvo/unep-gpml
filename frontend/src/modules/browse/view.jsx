akvo/unep-gpml

commit e87651f38db8973eda74079ff12a46ebdb81444b

[#159] Change browse dropdown behaviour based on country id

frontend/src/modules/browse/view.jsx
@@ -116,7 +116,7 @@ const Browse = ({
     clearTimeout(tmid);
     tmid = setTimeout(getResults, 1000);
     if (param === "country") {
-      setFilterCountries(value);
+      setFilterCountries([value]);
     }
   };
   const handleRelationChange = (relation) => {
@@ -189,20 +189,30 @@ const Browse = ({
             <div className="field">
               <div className="label">Country</div>
               <Select
-                value={query.country}
+                value={
+                  countries && query?.country
+                    ? countries.filter((x) => x.isoCode === query.country)?.id
+                    : []
+                }
                 placeholder="Find country"
                 mode="multiple"
                 options={
                   countries &&
                   countries
                     .map((it) => ({
-                      value: it.isoCode,
+                      value: it.id,
                       label: it.name,
                     }))
                     .sort((a, b) => a.label.localeCompare(b.label))
                 }
                 allowClear
-                onChange={(val) => updateQuery("country", val)}
+                onChange={(val) => {
+                  const selected = countries?.filter((x) => val.includes(x.id));
+                  updateQuery(
+                    "country",
+                    selected.map((x) => x.isoCode)
+                  );
+                }}
                 filterOption={(input, option) =>
                   option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0
                 }

commit 5da7945e2bf733d8395e1eb0f9bf4ed1e9d1559c

[#159] Fix bug in setting filterCountries in browse view
The country values were already a Array, and were incorrectly being
wrapped inside another array, breaking at least the counts code on the
browse page. The bug was introduced in e87651f38db8973eda74079ff12a46ebdb81444b

frontend/src/modules/browse/view.jsx
@@ -117,7 +117,7 @@ const Browse = ({
     clearTimeout(tmid);
     tmid = setTimeout(getResults, 1000);
     if (param === "country") {
-      setFilterCountries([value]);
+      setFilterCountries(value);
     }
   };
   const handleRelationChange = (relation) => {