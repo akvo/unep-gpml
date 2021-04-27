8e4b01ced52a3660778b465d84dec2c8c40167d3
Daniel Lebrero Berna <dlebrero@gmail.com>
2021-04-27 17:07:08 +0200

    [#203] s/TRAVIS_COMMIT/CI_COMMIT/

--- ci/k8s/wait-for-k8s-deployment-to-be-ready.sh
+++ ci/k8s/wait-for-k8s-deployment-to-be-ready.sh
@@ -4,9 +4,9 @@ starttime=`date +%s`
 
 while [ $(( $(date +%s) - 300 )) -lt ${starttime} ]; do
 
-    consumer_status=`kubectl get pods -l "unep-gpml-version=$TRAVIS_COMMIT,run=unep-gpml" -o jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
+    consumer_status=`kubectl get pods -l "unep-gpml-version=$CI_COMMIT,run=unep-gpml" -o jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
 
-    old_consumer_status=`kubectl get pods -l "unep-gpml-version!=$TRAVIS_COMMIT,run=unep-gpml" -o jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
+    old_consumer_status=`kubectl get pods -l "unep-gpml-version!=$CI_COMMIT,run=unep-gpml" -o jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
 
     if [[ ${consumer_status} =~ "ready=true" ]] && ! [[ ${old_consumer_status} =~ "ready" ]]; then
         exit 0
