ba2996abb386348526e2e8c75742a45ee0918d00
wayangalihpratama <wgprtm@gmail.com>
2021-05-25 11:57:21 +0800

    [#259] Add URL validation in event form

--- frontend/src/utils/form-utils.js
+++ frontend/src/utils/form-utils.js
@@ -1,5 +1,6 @@
 import FinalField from "./final-field";
 import FinalFieldArray from "./final-field-array";
+import { customFormats } from "./forms";
 
 export const FieldsFromSchema = ({ schema, mutators, ...props }) => {
   return Object.keys(schema)
@@ -19,6 +20,24 @@ export const validateSchema = (schema) => (values) => {
   const errors = {};
   Object.keys(schema).forEach((itemName) => {
     const value = values[itemName];
+    // validate url on array item field
+    if (value && Array.isArray(value) && value.length > 0) {
+      const urlErrors = [];
+      value.forEach((x, i) => {
+        if (x?.url && typeof x?.url !== "undefined") {
+          if (!x.url.match(customFormats.url)) {
+            urlErrors[i] = { url: 'should match format "url"' };
+          } else {
+            delete urlErrors[i];
+          }
+        }
+      });
+      if (urlErrors.length > 0) {
+        errors[itemName] = urlErrors;
+      }
+    }
+    // eol validate url on array item field
+
     if (!value && schema[itemName]?.required) {
       errors[itemName] = "Required";
     }
