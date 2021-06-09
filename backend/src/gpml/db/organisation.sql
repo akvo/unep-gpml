74ea42834dd71aa054e9e02fd328d1d872144e38
dedenbangkit <mail@dedenbangkit.com>
2021-06-09 19:50:40 +0700

    [#286] Fix entity geo coverage values
    Fix query for organisation geo coverage to use country IDs instead of
    iso_code

--- backend/src/gpml/db/organisation.sql
+++ backend/src/gpml/db/organisation.sql
@@ -4,10 +4,9 @@ select id, name from organisation order by id
 
 -- :name organisation-by-id :? :1
 -- :doc Get organisation by id
-select o.id, o.name, o.url, o.type, o.geo_coverage_type, c.iso_code as country
-from organisation o
-left join country c on o.country = c.id
-where o.id = :id;
+select id, name, url, type, geo_coverage_type, country
+from organisation
+where id = :id;
 
 -- :name organisation-by-name :? :1
 -- :doc Get organisation by name
@@ -56,10 +55,8 @@ where id = :id
 
 -- :name geo-coverage :? :*
 -- :doc Get geo coverage by organisation id
-select cg.id, c.iso_code, cg.name from organisation_geo_coverage o
-left join country c on c.id = o.country
-left join country_group cg on cg.id = o.country_group
-where o.organisation = :id
+select id, coalesce(country, country_group) as geo_coverage_values from organisation_geo_coverage
+where organisation = :id
 
 -- :name add-geo-coverage :<! :1
 -- :doc add organisation geo coverage
