2a934365bf191baee70f25214bf9f120d020609e
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-06-24 20:52:09 +0530

    [#205] Notify admins when a new initiative is created

--- backend/src/gpml/handler/initiative.clj
+++ backend/src/gpml/handler/initiative.clj
@@ -2,14 +2,22 @@
   (:require [integrant.core :as ig]
             [gpml.db.stakeholder :as db.stakeholder]
             [gpml.db.initiative :as db.initiative]
+            [gpml.email-util :as email]
             [ring.util.response :as resp]))
 
-(defmethod ig/init-key :gpml.handler.initiative/post [_ {:keys [db]}]
+(defmethod ig/init-key :gpml.handler.initiative/post [_ {:keys [db mailjet-config]}]
   (fn [{:keys [jwt-claims body-params] :as req}]
-      (let [user (db.stakeholder/stakeholder-by-email (:spec db) jwt-claims)
-            data (assoc body-params :created_by (:id user))
-            initiative-id (db.initiative/new-initiative (:spec db) data)]
-    (resp/created (:referrer req) (merge initiative-id {:message "New initiative created"})))))
+    (let [conn (:spec db)
+          user (db.stakeholder/stakeholder-by-email conn jwt-claims)
+          data (assoc body-params :created_by (:id user))
+          initiative-id (db.initiative/new-initiative conn data)]
+      (email/notify-admins-pending-approval
+       conn
+       mailjet-config
+       {:type "initiative" :title (:q2 data)})
+      (resp/created
+       (:referrer req)
+       (merge initiative-id {:message "New initiative created"})))))
 
 (defmethod ig/init-key :gpml.handler.initiative/post-params [_ _]
   [:map [:version integer?]])
