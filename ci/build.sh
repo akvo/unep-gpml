6a98b6a56d9f455a4399fbadb7d65b86380e8f38
Iván Perdomo <ivan@perdomo.me>
2021-06-30 08:54:19 +0200

    [#1] Update cljkondo

--- ci/build.sh
+++ ci/build.sh
@@ -34,7 +34,7 @@ backend_build () {
 	   --rm \
 	   --volume "$(pwd)/backend:/app" \
 	   --workdir /app \
-	   cljkondo/clj-kondo:2021.04.23-alpine \
+	   cljkondo/clj-kondo:2021.06.18-alpine \
 	   clj-kondo --lint src --lint test
 
     dc run \
