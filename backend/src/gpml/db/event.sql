akvo/unep-gpml

commit ec92d8c97a0ce147e05635c108c3a09f6fdb9769

[#262] Change event country and geo_covereage_value with id

backend/src/gpml/db/event.sql
@@ -75,10 +75,28 @@ update event
 
 -- :name event-by-id :? :1
 -- :doc Returns the data for a given event
-select e.id, e.title, e.start_date, e.end_date, e.description, e.image, e.geo_coverage_type,
-                e.remarks, e.created, e.modified, e.city, c.iso_code as country, e.languages, e.tags, e.review_status
-           from v_event_data e left join country c on e.country = c.id
-where e.id = :id
+select
+    id,
+    title,
+    start_date,
+    end_date,
+    description,
+    image,
+    geo_coverage_type,
+    remarks,
+    created,
+    modified,
+    city,
+    country,
+    languages,
+    tags,
+    review_status,
+    created_by,
+    (select json_agg(tag) from event_tag where technology = :id) as tags,
+    (select json_agg(coalesce(country, country_group))
+        from event_geo_coverage where event = :id) as geo_coverage_value
+from event
+where id = :id
 
 -- :name event-image-by-id :? :1
 -- :doc Get event image by id

backend/src/gpml/handler/event.clj
@@ -1,7 +1,6 @@
 (ns gpml.handler.event
   (:require [clojure.java.jdbc :as jdbc]
             [gpml.handler.geo :as handler.geo]
-            [gpml.handler.country :as handler.country]
             [gpml.handler.image :as handler.image]
             [gpml.db.event :as db.event]
             [gpml.db.language :as db.language]
@@ -23,7 +22,7 @@
               :geo_coverage_type geo_coverage_type
               :geo_coverage_value geo_coverage_value
               :city city
-              :country (handler.country/id-by-code conn country)
+              :country country
               :created_by created_by}
         event-id (->> data (db.event/new-event conn) :id)]
     (when (not-empty tags)
@@ -37,9 +36,8 @@
                                     (:url %)) urls)]
         (db.event/add-event-language-urls conn {:urls lang-urls})))
     (when (not-empty geo_coverage_value)
-      (let [geo-data (handler.geo/id-vec-geo conn event-id data)]
-        (when (some? geo-data)
-          (db.event/add-event-geo-coverage conn {:geo geo-data}))))
+      (let [geo-data (handler.geo/get-geo-vector event-id data)]
+          (db.event/add-event-geo-coverage conn {:geo geo-data})))
     (email/notify-admins-pending-approval
      conn
      mailjet-config
@@ -56,7 +54,7 @@
    [:geo_coverage_type
     [:enum "global", "regional", "national", "transnational",
      "sub-national", "global with elements in specific areas"]]
-   [:country {:optional true} string?]
+   [:country {:optional true} integer?]
    [:city {:optional true} string?]
    [:urls {:optional true}
     [:vector {:optional true}
@@ -64,9 +62,9 @@
       [:lang string?]
       [:url [:string {:min 1}]]]]]
    [:tags {:optional true}
-    [:vector {:optional true} int?]]
+    [:vector {:optional true} integer?]]
    [:geo_coverage_value {:optional true}
-    [:vector {:min 1 :error/message "Need at least one geo coverage value"} string?]]])
+    [:vector {:min 1 :error/message "Need at least one geo coverage value"} integer?]]])
 
 (defmethod ig/init-key :gpml.handler.event/post [_ {:keys [db mailjet-config]}]
   (fn [{:keys [jwt-claims body-params] :as req}]

commit 46d3702422dae48e4cabfd11868bb344603bf3d6

[#246] Fix broken event-by-id function
The commit ec92d8c97a0ce147e05635c108c3a09f6fdb9769 broke the
function, and this commit fixes it. The function was not being used
anywhere in the code, and hence the bug went unnoticed so far.

backend/src/gpml/db/event.sql
@@ -76,26 +76,25 @@ update event
 -- :name event-by-id :? :1
 -- :doc Returns the data for a given event
 select
-    id,
-    title,
-    start_date,
-    end_date,
-    description,
-    image,
-    geo_coverage_type,
-    remarks,
-    created,
-    modified,
-    city,
-    country,
-    languages,
-    tags,
-    review_status,
-    created_by,
-    (select json_agg(tag) from event_tag where technology = :id) as tags,
+    e.id,
+    e.title,
+    e.start_date,
+    e.end_date,
+    e.description,
+    e.image,
+    e.geo_coverage_type,
+    e.remarks,
+    e.created,
+    e.modified,
+    e.city,
+    e.country,
+    e.languages,
+    e.tags,
+    e.review_status,
+    (select json_agg(tag) from event_tag where event = :id) as tags,
     (select json_agg(coalesce(country, country_group))
         from event_geo_coverage where event = :id) as geo_coverage_value
-from event
+from v_event_data e
 where id = :id
 
 -- :name event-image-by-id :? :1