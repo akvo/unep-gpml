6349e79dad76ea2519b2a360d440b25784e46e55
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-08-10 12:51:18 +0530

    [#369] Add an end-point to get the list of reviewers

---
+++ backend/src/gpml/handler/review.clj
@@ -0,0 +1,13 @@
+(ns gpml.handler.review
+  (:require
+   [gpml.db.stakeholder :as db.stakeholder]
+   [integrant.core :as ig]
+   [ring.util.response :as resp]))
+
+(defn get-reviewers [db]
+  (let [conn (:spec db)]
+    (resp/response (db.stakeholder/get-reviewers conn))))
+
+(defmethod ig/init-key :gpml.handler.review/get-reviewers [_ {:keys [db]}]
+  (fn [_]
+    (get-reviewers db)))
