2378172ea0ab3db1ca2b85f883acaf51fb230bad
Juan A. Ruz <juanantonioruz@gmail.com>
2021-08-11 09:45:39 +0200

    [#367] Fix tests
    Create stakeholder and connect it to the new organisation
    
    Change visibility of profile-test/profile to public

--- backend/test/gpml/handler/organisation_test.clj
+++ backend/test/gpml/handler/organisation_test.clj
@@ -1,8 +1,9 @@
 (ns gpml.handler.organisation-test
   (:require [clojure.test :refer [deftest testing is use-fixtures]]
-            [gpml.handler.profile-test :as profile-test]
             [gpml.fixtures :as fixtures]
             [gpml.handler.organisation :as organisation]
+            [gpml.handler.profile :as profile]
+            [gpml.handler.profile-test :as profile-test]
             [integrant.core :as ig]
             [ring.mock.request :as mock]))
 
@@ -10,17 +11,30 @@
 
 (deftest handler-post-with-new-organisation-test
   (testing "New profile is created with new organisation"
-    (let [system (ig/init fixtures/*system* [::organisation/post])
-          handler (::organisation/post system)
-          db (-> system :duct.database.sql/hikaricp :spec)
-          data (profile-test/seed-important-database db)
-          body-params {:name "My nown company"
-                       :geo_coverage_type "regional"
-                       :country (-> (:countries data) second :id)
-                       :type "Company"
-                       :url "mycompany.org"}
-          resp (handler (-> (mock/request :post "/")
-                            (assoc :jwt-claims {:email "john@org" :picture "test.jpg"})
-                            (assoc :body-params body-params)))]
+    (let [system          (ig/init fixtures/*system* [::organisation/post ::profile/post])
+          profile-handler (::profile/post system)
+          org-handler     (::organisation/post system)
+          db              (-> system :duct.database.sql/hikaricp :spec)
+          data            (profile-test/seed-important-database db)
+          profile         (assoc (profile-test/new-profile 1)
+                         :org nil
+                         :organisation_role nil
+                         :geo_coverage_value (mapv :id (:country_groups data))
+                         :tags (mapv :id (:tags data))
+                         :country (-> (:countries data) first :id)
+                         :photo profile-test/picture)
+          body-params     {:name              "test10001"
+                           :geo_coverage_type "regional"
+                           :country           (-> (:countries data) second :id)
+                           :type              "Company"
+                           :url               "mycompany.org"
+                           :stakeholder       "juan@akvo.org"}
+          jwt-claims      {:email "john@org" :picture "test.jpg"}
+          _               (profile-handler (-> (mock/request :post "/")
+                                 (assoc :jwt-claims jwt-claims)
+                                 (assoc :body-params profile)))
+          resp            (org-handler (-> (mock/request :post "/")
+                                (assoc :jwt-claims jwt-claims)
+                                (assoc :body-params body-params)))]
       (is (= 201 (:status resp)))
-      (is (= (assoc body-params :id 10001 :created_by nil) (:body resp))))))
+      (is (= (assoc body-params :id 10001) (:body resp))))))
