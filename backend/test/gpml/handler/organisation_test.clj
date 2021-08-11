1ae89e0a7262c233396a96985e72e1c90dded683
Juan A. Ruz <juanantonioruz@gmail.com>
2021-08-11 12:50:42 +0200

    [#367] Improve mail texts and organisation-test checks

--- backend/test/gpml/handler/organisation_test.clj
+++ backend/test/gpml/handler/organisation_test.clj
@@ -1,40 +1,52 @@
 (ns gpml.handler.organisation-test
   (:require [clojure.test :refer [deftest testing is use-fixtures]]
+            [gpml.db.invitation :as db.invitation]
+            [gpml.db.stakeholder :as db.stakeholder]
             [gpml.fixtures :as fixtures]
             [gpml.handler.organisation :as organisation]
             [gpml.handler.profile :as profile]
-            [gpml.handler.profile-test :as profile-test]
             [integrant.core :as ig]
             [ring.mock.request :as mock]))
 
 (use-fixtures :each fixtures/with-test-system)
 
-(deftest handler-post-with-new-organisation-test
+(deftest handler-post-with-new-organisation-with-non-existent-second-contact-test
   (testing "New profile is created with new organisation"
     (let [system          (ig/init fixtures/*system* [::organisation/post ::profile/post])
           profile-handler (::profile/post system)
           org-handler     (::organisation/post system)
           db              (-> system :duct.database.sql/hikaricp :spec)
-          data            (profile-test/seed-important-database db)
-          profile         (assoc (profile-test/new-profile 1)
-                         :org nil
-                         :organisation_role nil
-                         :geo_coverage_value (mapv :id (:country_groups data))
-                         :tags (mapv :id (:tags data))
-                         :country (-> (:countries data) first :id)
-                         :photo profile-test/picture)
+          created-by      (format "created_by_%s@akvo.org" (fixtures/uuid))
+          profile         {:email created-by
+                           :first_name "John"
+                           :geo_coverage_type nil
+                           :affiliation nil
+                           :title "Mr"
+                           :representation ""
+                           :picture nil
+                           :last_name "Doe"
+                           :country nil}
+          _ (db.stakeholder/new-stakeholder db profile)
+          stakeholder     (format "stakeholder_%s@akvo.org" (fixtures/uuid))
           body-params     {:name              "test10001"
                            :geo_coverage_type "regional"
-                           :country           (-> (:countries data) second :id)
+                           :country           nil
                            :type              "Company"
                            :url               "mycompany.org"
-                           :stakeholder       "juan@akvo.org"}
+                           :stakeholder       stakeholder}
           jwt-claims      {:email "john@org" :picture "test.jpg"}
           _               (profile-handler (-> (mock/request :post "/")
-                                 (assoc :jwt-claims jwt-claims)
-                                 (assoc :body-params profile)))
+                                               (assoc :jwt-claims jwt-claims)
+                                               (assoc :body-params profile)))
+          _               (is (nil? (db.invitation/invitation-by-email db {:email stakeholder})))
           resp            (org-handler (-> (mock/request :post "/")
-                                (assoc :jwt-claims jwt-claims)
-                                (assoc :body-params body-params)))]
+                                           (assoc :jwt-claims jwt-claims)
+                                           (assoc :body-params body-params)))
+          mails           @fixtures/mails-sent]
       (is (= 201 (:status resp)))
-      (is (= (assoc body-params :id 10001) (:body resp))))))
+      (is (some? (db.invitation/invitation-by-email db {:email stakeholder})))
+      (is (= (assoc body-params :id 10001) (:body resp)))
+      (is (= 1 (count mails)))
+      (is (= (-> mails first :receivers) (list {:Name stakeholder, :Email stakeholder})))
+      (is (= (-> mails first :subject) "Mr. John Doe has invited you to join UNEP GPML Digital Platform"))
+      (is (= (-> mails first :texts first) "Dear user,\n\nMr. John Doe has invited you to join http://localhost as part of entity test10001. Please visit http://localhost/stakeholder-signup and follow instructions to signup.\n\n- UNEP GPML Digital Platform\n")))))
