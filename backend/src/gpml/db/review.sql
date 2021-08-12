3d5d48518b809b09884b7116ca85a7914fe8ce4e
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-08-12 14:06:37 +0530

    [#369] Add end-point to get review for a submission

---
+++ backend/src/gpml/db/review.sql
@@ -0,0 +1,17 @@
+-- :name all-reviews :? :*
+-- :doc Get all reviews
+SELECT * FROM review ORDER BY id
+
+-- :name review-by-id :? :1
+-- :doc Get review by id
+SELECT * FROM review WHERE id = :id;
+
+-- :name review-by-topic-item :? :1
+-- :doc Get review by topic_type, topic_id
+SELECT * FROM review WHERE
+  topic_type = :v:topic-type::topic_type AND
+  topic_id = :topic-id;
+
+-- :name new-review :<! :1
+INSERT INTO review (topic_type, topic_id, assigned_by, reviewer)
+VALUES (:topic-type::topic_type, :topic-id, :assigned-by, :reviewer) returning id;
