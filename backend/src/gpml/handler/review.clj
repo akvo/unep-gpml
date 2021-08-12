42f32eb4608bb23bb7c7f1471349049cca311530
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-08-12 14:06:37 +0530

    [#369] Add end-point to get review for a submission

--- backend/src/gpml/handler/review.clj
+++ backend/src/gpml/handler/review.clj
@@ -1,6 +1,8 @@
 (ns gpml.handler.review
   (:require
+   [gpml.constants :as constants]
    [gpml.db.stakeholder :as db.stakeholder]
+   [gpml.db.review :as db.review]
    [integrant.core :as ig]
    [ring.util.response :as resp]))
 
@@ -8,6 +10,21 @@
   (let [conn (:spec db)]
     (resp/response (db.stakeholder/get-reviewers conn))))
 
+(defn get-review [db topic-type topic-id]
+  (let [conn (:spec db)
+        topic-name (cond
+                     (contains? (set constants/resource-types) topic-type) "resource"
+                     (= topic-type "project") "initiative"
+                     :else topic-type)
+        review (db.review/review-by-topic-item
+                conn
+                {:topic-name topic-name :topic-id topic-id})]
+    (resp/response review)))
+
 (defmethod ig/init-key ::get-reviewers [_ {:keys [db]}]
   (fn [_]
     (get-reviewers db)))
+
+(defmethod ig/init-key ::get-review [_ {:keys [db]}]
+  (fn [{{{:keys [topic-type topic-id]} :path} :parameters}]
+    (get-review db topic-type topic-id)))
--- backend/test/gpml/handler/review_test.clj
+++ backend/test/gpml/handler/review_test.clj
@@ -2,12 +2,12 @@
   (:require [clojure.test :refer [deftest testing is use-fixtures]]
             [gpml.fixtures :as fixtures]
             [gpml.db.stakeholder :as db.stakeholder]
+            [gpml.db.review :as db.review]
             [gpml.handler.review :as review]
             [integrant.core :as ig]
             [ring.mock.request :as mock]))
 
 (use-fixtures :each fixtures/with-test-system)
-
 
 (defn- new-stakeholder [db email first_name last_name role review_status]
   (let [info {:picture "https://picsum.photos/200"
@@ -21,10 +21,11 @@
               :email email}
         sth (db.stakeholder/new-stakeholder db info)]
     (db.stakeholder/update-stakeholder-status db (assoc sth :review_status review_status))
-    (db.stakeholder/update-stakeholder-role db (assoc sth :role role))))
+    (db.stakeholder/update-stakeholder-role db (assoc sth :role role))
+    sth))
 
-(deftest handler-test
-  (testing "Landing endpoint returns non empty response"
+(deftest get-reviewers-test
+  (testing "Get reviewers endpoint returns non empty response"
     (let [system (ig/init fixtures/*system* [::review/get-reviewers])
           handler (::review/get-reviewers system)
           db (-> system :duct.database.sql/hikaricp :spec)
@@ -42,3 +43,37 @@
       (is (= 200 (:status resp)))
       (is (= 2 (count body)))
       (is (= #{"ADMIN" "REVIEWER"} (set (map :role body)))))))
+
+(deftest get-topic-review
+  (let [system (ig/init fixtures/*system* [::review/get-review])
+        handler (::review/get-review system)
+        db (-> system :duct.database.sql/hikaricp :spec)
+        admin (new-stakeholder db "admin-approved@org.com" "R" "A" "ADMIN" "APPROVED")
+        reviewer (new-stakeholder db "reviewer-approved@org.com" "R" "A" "REVIEWER" "APPROVED")
+        user (new-stakeholder db "user-submitted@org.com" "U" "S" "USER" "SUBMITTED")]
+
+    (testing "Fetching topic review when NO REVIEWER ASSIGNED"
+      (let [resp (handler (-> (mock/request :get "/")
+                              (assoc
+                               :parameters {:path {:topic-type "stakeholder"
+                                                   :topic-id (:id user)}})))
+            body (:body resp)]
+        (is (= 200 (:status resp)))
+        (is (empty? body))))
+
+    (testing "Fetching topic review when REVIEWER ASSIGNED"
+      (let [stakeholder-id (:id user)
+            _ (db.review/new-review db {:topic-name "stakeholder"
+                                        :topic-id stakeholder-id
+                                        :assigned-by (:id admin)
+                                        :reviewer (:id reviewer)})
+            resp (handler (-> (mock/request :get "/")
+                              (assoc
+                               :parameters {:path {:topic-type "stakeholder"
+                                                   :topic-id (:id user)}})))
+            body (:body resp)]
+        (is (= 200 (:status resp)))
+        (is (= (:id reviewer) (:reviewer body)))
+        (is (= (:id admin) (:assigned_by body)))
+        (is (= "stakeholder" (:topic_name body)))
+        (is (= (:id user) (:topic_id body)))))))
