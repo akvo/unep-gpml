3087f1341845159b58e4372510f5081cb7949378
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-08-17 20:59:13 +0530

    [#369] Change constants/resource-types to a set from a list

--- backend/src/gpml/constants.clj
+++ backend/src/gpml/constants.clj
@@ -1,6 +1,6 @@
 (ns gpml.constants)
 
-(def resource-types ["financing_resource" "technical_resource" "action_plan"])
+(def resource-types #{"financing_resource" "technical_resource" "action_plan"})
 (def approved-user-topics #{"organisation" "stakeholder"})
 (def topics
   (vec
