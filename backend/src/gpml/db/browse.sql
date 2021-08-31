71f86dd403d78925bc31a8fdf427ffa0aab97b97
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-08-31 18:21:53 +0530

    [#132] Use the more idiomatic some? instead of (not (nil?))

--- backend/src/gpml/db/browse.sql
+++ backend/src/gpml/db/browse.sql
@@ -8,7 +8,7 @@ SELECT DISTINCT ON (t.topic, (COALESCE(t.json->>'start_date', t.json->>'created'
 --~ (when (seq (:geo-coverage params)) "AND t.geo_coverage IN (:v*:geo-coverage)")
 -- NOTE: We deliberately use a check for nil as opposed to empty list, here. See commit message in
 -- 938110757e03f60e8c5a4396ccea8bd99bcef579
---~ (when (not (nil? (:topic params))) "AND t.topic = ANY(ARRAY[:v*:topic]::varchar[])")
+--~ (when (some? (:topic params)) "AND t.topic = ANY(ARRAY[:v*:topic]::varchar[])")
 ORDER BY (COALESCE(t.json->>'start_date', t.json->>'created'))::timestamptz DESC, (t.json->>'id')::int DESC
 --~ (format "LIMIT %s" (or (and (contains? params :limit) (:limit params)) 50))
 --~ (format "OFFSET %s" (or (and (contains? params :offset) (:offset params)) 0))
