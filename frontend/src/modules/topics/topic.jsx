fcdff3bd45a816df573f464311805ea60f052c4b
wayangalihpratama <wgprtm@gmail.com>
2021-09-02 16:08:14 +0800

    [#363] Link topic item to browse view with filtered tag

--- frontend/src/modules/topics/topic.jsx
+++ frontend/src/modules/topics/topic.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { Link } from "react-router-dom";
 import { Row, Col, Card, Image, Button } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
 
@@ -7,9 +8,36 @@ import imageNotFound from "../../images/image-not-found.png";
 
 import { UIStore } from "../../store";
 import isEmpty from "lodash/isEmpty";
+import sumBy from "lodash/sumBy";
+import api from "../../utils/api";
 
 const Topic = () => {
-  const { tags } = UIStore.useState((s) => s);
+  const tags = UIStore.useState((s) => s.tags);
+  const [topics, setTopics] = useState(null);
+
+  const isLoaded = useCallback(() => {
+    return Boolean(!isEmpty(tags));
+  }, [tags]);
+
+  // useEffect(() => {
+  //   if (isLoaded && tags?.topics) {
+  //     const topicData = tags.topics.map(async (t) => {
+  //       const res = await api.get(`/browse?tag=${t.tag}`);
+  //       const { counts } = res.data;
+  //       let countTmp = 0;
+  //       countTmp = counts
+  //         ? counts.length
+  //           ? sumBy(counts, "count")
+  //           : counts.length
+  //         : countTmp;
+  //       return {
+  //         ...t,
+  //         count: countTmp,
+  //       };
+  //     });
+  //   }
+  // }, [tags, isLoaded]);
+
   return (
     <div id="topics">
       {/* Featured topic */}
@@ -18,7 +46,7 @@ const Topic = () => {
           <div className="section-container">
             <h2>Featured Topics</h2>
             <Row className="topic-item-wrapper" gutter={[16, 16]}>
-              {isEmpty(tags) || !tags?.topics ? (
+              {!isLoaded || !tags?.topics ? (
                 <h2 className="loading">
                   <LoadingOutlined spin /> Loading...
                 </h2>
@@ -38,18 +66,29 @@ const Topic = () => {
 const renderTopics = (topics) => {
   return topics.map((x) => {
     const { id, tag } = x;
+    const tagName = tag.includes("-") ? tag.split("-").join(" ") : tag;
+
     return (
       <Col key={`${tag}-${id}`} sm={24} md={12} lg={8}>
-        <Card className="topic-item-card" onClick={() => console.log(tag)}>
-          <span className="featured-topic-label resource-label">Featured</span>
-          <Image size="90%" src={imageNotFound} preview={false} />
-          <div className="topic-item-title-wrapper">
-            <div className="topic-item-title">{tag}</div>
-            <Button className="topic-item-count" shape="circle" type="ghost">
-              50
-            </Button>
-          </div>
-        </Card>
+        <Link
+          to={{
+            pathname: "/browse",
+            search: `?tag=${tag}`,
+          }}
+        >
+          <Card className="topic-item-card">
+            <span className="featured-topic-label resource-label">
+              Featured
+            </span>
+            <Image size="90%" src={imageNotFound} preview={false} />
+            <div className="topic-item-title-wrapper">
+              <div className="topic-item-title">{tagName}</div>
+              <Button className="topic-item-count" shape="circle" type="ghost">
+                50
+              </Button>
+            </div>
+          </Card>
+        </Link>
       </Col>
     );
   });
