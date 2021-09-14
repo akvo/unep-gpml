ef2d925eb74e0980a1790a391ebc3e8d39763fc9
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-09-14 16:37:58 +0530

    [#361] Remove unused recent topics data from landing end-point

--- backend/src/gpml/handler/landing.clj
+++ backend/src/gpml/handler/landing.clj
@@ -1,22 +1,13 @@
 (ns gpml.handler.landing
   (:require [integrant.core :as ig]
             [ring.util.response :as resp]
-            [gpml.constants :refer [topics]]
-            [gpml.db.browse :as db.browse]
             [gpml.db.landing :as db.landing]))
 
 (defn landing-response [conn]
   (let [summary-data (->> (gpml.db.landing/summary conn)
                           (mapv (fn [{:keys [resource_type count country_count]}]
-                                  {(keyword resource_type) count :countries country_count})))
-        topics-data (->> topics
-                         (map #(assoc {} :topic [%]))
-                         (map #(db.browse/filter-topic conn %))
-                         (map #(take 1 %))
-                         flatten
-                         (map #(merge (:json %) {:topic_type (:topic %)})))]
-    (resp/response {:topics topics-data
-                    :map (db.landing/map-counts-include-all-countries conn)
+                                  {(keyword resource_type) count :countries country_count})))]
+    (resp/response {:map (db.landing/map-counts-include-all-countries conn)
                     :summary summary-data})))
 
 
