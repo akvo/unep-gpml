906fcd891b5b1cf8331da53deab323e1f0bacddf
Juan A. Ruz <juanantonioruz@gmail.com>
2021-09-17 13:03:36 +0200

    [#367] Add organisation representative-group db migration

---
+++ backend/resources/migrations/088-add-organisation-representative-group-details.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE organisation ADD COLUMN representative_group_government TEXT;
+ALTER TABLE organisation ADD COLUMN representative_group_private_sector TEXT;
+ALTER TABLE organisation ADD COLUMN representative_group_academia_research TEXT;
+ALTER TABLE organisation ADD COLUMN representative_group_civil_society TEXT;
+ALTER TABLE organisation ADD COLUMN representative_group_other TEXT;
