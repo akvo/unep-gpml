fcc9ea66bf46df84864376d5ef93376c3e0672de
Juan A. Ruz <juanantonioruz@gmail.com>
2021-09-28 10:56:26 +0200

    [#450] Use test db ids

--- backend/resources/migrations/093-seed-transnational-country-groups.down.sql
+++ backend/resources/migrations/093-seed-transnational-country-groups.down.sql
@@ -1,13 +1,13 @@
--- Deleting Country group APEC
-delete from country_group_country where country_group=159;
--- Deleting Country group ASEAN
-delete from country_group_country where country_group=157;
 -- Deleting Country group African States
 delete from country_group_country where country_group=121;
 -- Deleting Country group Antarctic
 delete from country_group_country where country_group=126;
+-- Deleting Country group APEC
+delete from country_group_country where country_group=159;
 -- Deleting Country group Arctic
 delete from country_group_country where country_group=127;
+-- Deleting Country group ASEAN
+delete from country_group_country where country_group=157;
 -- Deleting Country group Asia-Pacific States
 delete from country_group_country where country_group=122;
 -- Deleting Country group Baltic Sea
@@ -16,22 +16,22 @@ delete from country_group_country where country_group=128;
 delete from country_group_country where country_group=144;
 -- Deleting Country group Black Sea
 delete from country_group_country where country_group=129;
--- Deleting Country group CMS
-delete from country_group_country where country_group=149;
 -- Deleting Country group Caspian Sea
 delete from country_group_country where country_group=130;
+-- Deleting Country group CMS
+delete from country_group_country where country_group=149;
 -- Deleting Country group Common Wealth
 delete from country_group_country where country_group=156;
 -- Deleting Country group Convention on Biological
 delete from country_group_country where country_group=150;
 -- Deleting Country group Convention on Climate
 delete from country_group_country where country_group=147;
--- Deleting Country group EU
-delete from country_group_country where country_group=155;
 -- Deleting Country group East Asian Seas
 delete from country_group_country where country_group=132;
 -- Deleting Country group Eastern European States
 delete from country_group_country where country_group=124;
+-- Deleting Country group EU
+delete from country_group_country where country_group=155;
 -- Deleting Country group Europe
 delete from country_group_country where country_group=151;
 -- Deleting Country group G20
@@ -48,18 +48,18 @@ delete from country_group_country where country_group=148;
 delete from country_group_country where country_group=152;
 -- Deleting Country group Mediterranean
 delete from country_group_country where country_group=133;
+-- Deleting Country group North-East Atlantic
+delete from country_group_country where country_group=134;
 -- Deleting Country group North East Pacific
 delete from country_group_country where country_group=136;
 -- Deleting Country group North West Pacific
 delete from country_group_country where country_group=135;
--- Deleting Country group North-East Atlantic
-delete from country_group_country where country_group=134;
 -- Deleting Country group Pacific Region
 delete from country_group_country where country_group=137;
--- Deleting Country group ROPME Sea
-delete from country_group_country where country_group=139;
 -- Deleting Country group Red Sea and Gulf of Aden
 delete from country_group_country where country_group=138;
+-- Deleting Country group ROPME Sea
+delete from country_group_country where country_group=139;
 -- Deleting Country group Rotterdam Convention
 delete from country_group_country where country_group=145;
 -- Deleting Country group SIDS
