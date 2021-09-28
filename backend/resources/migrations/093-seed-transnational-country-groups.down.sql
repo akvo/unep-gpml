513c547380c6dc8244e0b0e0628a77532f227508
Juan A. Ruz <juanantonioruz@gmail.com>
2021-09-28 10:07:57 +0200

    [#450] Seed transnational country groups

---
+++ backend/resources/migrations/093-seed-transnational-country-groups.down.sql
@@ -0,0 +1,80 @@
+-- Deleting Country group APEC
+delete from country_group_country where country_group=159;
+-- Deleting Country group ASEAN
+delete from country_group_country where country_group=157;
+-- Deleting Country group African States
+delete from country_group_country where country_group=121;
+-- Deleting Country group Antarctic
+delete from country_group_country where country_group=126;
+-- Deleting Country group Arctic
+delete from country_group_country where country_group=127;
+-- Deleting Country group Asia-Pacific States
+delete from country_group_country where country_group=122;
+-- Deleting Country group Baltic Sea
+delete from country_group_country where country_group=128;
+-- Deleting Country group Basel Convention
+delete from country_group_country where country_group=144;
+-- Deleting Country group Black Sea
+delete from country_group_country where country_group=129;
+-- Deleting Country group CMS
+delete from country_group_country where country_group=149;
+-- Deleting Country group Caspian Sea
+delete from country_group_country where country_group=130;
+-- Deleting Country group Common Wealth
+delete from country_group_country where country_group=156;
+-- Deleting Country group Convention on Biological
+delete from country_group_country where country_group=150;
+-- Deleting Country group Convention on Climate
+delete from country_group_country where country_group=147;
+-- Deleting Country group EU
+delete from country_group_country where country_group=155;
+-- Deleting Country group East Asian Seas
+delete from country_group_country where country_group=132;
+-- Deleting Country group Eastern European States
+delete from country_group_country where country_group=124;
+-- Deleting Country group Europe
+delete from country_group_country where country_group=151;
+-- Deleting Country group G20
+delete from country_group_country where country_group=154;
+-- Deleting Country group G7
+delete from country_group_country where country_group=153;
+-- Deleting Country group Group of 77 and China
+delete from country_group_country where country_group=160;
+-- Deleting Country group Latin American and Caribbean
+delete from country_group_country where country_group=125;
+-- Deleting Country group  Law of the Sea
+delete from country_group_country where country_group=148;
+-- Deleting Country group Least Developed Countries
+delete from country_group_country where country_group=152;
+-- Deleting Country group Mediterranean
+delete from country_group_country where country_group=133;
+-- Deleting Country group North East Pacific
+delete from country_group_country where country_group=136;
+-- Deleting Country group North West Pacific
+delete from country_group_country where country_group=135;
+-- Deleting Country group North-East Atlantic
+delete from country_group_country where country_group=134;
+-- Deleting Country group Pacific Region
+delete from country_group_country where country_group=137;
+-- Deleting Country group ROPME Sea
+delete from country_group_country where country_group=139;
+-- Deleting Country group Red Sea and Gulf of Aden
+delete from country_group_country where country_group=138;
+-- Deleting Country group Rotterdam Convention
+delete from country_group_country where country_group=145;
+-- Deleting Country group SIDS
+delete from country_group_country where country_group=158;
+-- Deleting Country group South Asian Seas
+delete from country_group_country where country_group=140;
+-- Deleting Country group South-East Pacific
+delete from country_group_country where country_group=141;
+-- Deleting Country group Stockholm Convention
+delete from country_group_country where country_group=146;
+-- Deleting Country group WesternCentralSouthern Africa
+delete from country_group_country where country_group=142;
+-- Deleting Country group Western European
+delete from country_group_country where country_group=123;
+-- Deleting Country group Western Indian Ocean
+delete from country_group_country where country_group=131;
+-- Deleting Country group Wider Caribbean
+delete from country_group_country where country_group=143;
