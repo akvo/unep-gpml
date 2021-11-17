df44a1655d05f98d1156b43f1dfe6aa172af28d6
Juan A. Ruz <juanantonioruz@gmail.com>
2021-11-17 13:27:16 +0100

    [#567] Add db support
      (get-auth-by-stakeholder (dev/db-conn) { :stakeholder 10001} )
      ({:id 6, :stakeholder 10001, :topic_id 20, :topic_type "event", :roles ["owner"]})
      (update-auth (dev/db-conn) {:stakeholder 10001, :topic-id 20, :topic-type "event", :roles ["editor"]})
      (delete-auth (dev/db-conn) {:id 4})
      (new-auth (dev/db-conn)   {:stakeholder 10001, :topic-id 20, :topic-type "event", :roles ["owner"]})
      (get-auth-by-topic (dev/db-conn) {:topic-id 20 :topic-type "event"} )
      (get-auth-by-topic-and-stakeholder (dev/db-conn) {:topic-id 20 :topic-type "event" :stakeholder 10001})
      {:id 6, :stakeholder 10001, :topic_id 20, :topic_type "event", :roles ["editor"], :created #inst "2021-11-17T12:24:18.085909000-00:00", :modified #inst "2021-11-17T12:26:48.756618000-00:00"}

---
+++ backend/src/gpml/db/topic_stakeholder_auth.sql
@@ -0,0 +1,25 @@
+-- :name get-auth-by-topic :? :*
+-- :doc Get details about a particular topic
+select * from topic_stakeholder_auth where topic_id=:topic-id and topic_type=:topic-type::topic_type;
+
+-- :name get-auth-by-topic-and-stakeholder :? :1
+-- :doc Get details about a particular topic
+select * from topic_stakeholder_auth where topic_id=:topic-id and topic_type=:topic-type::topic_type and stakeholder=:stakeholder;
+
+-- :name get-auth-by-stakeholder :? :*
+-- :doc Get details about a particular topic
+select * from topic_stakeholder_auth where stakeholder=:stakeholder;
+
+
+-- :name new-auth :! :1
+INSERT INTO topic_stakeholder_auth(stakeholder, topic_id, topic_type, roles)
+values (:stakeholder, :topic-id, :topic-type::topic_type, :roles)
+
+-- :name delete-auth :! :n
+delete from topic_stakeholder_auth where id=:id
+
+-- :name update-auth :! :n
+update topic_stakeholder_auth set roles = :roles
+where stakeholder=:stakeholder and
+topic_id=:topic-id and
+topic_type=:topic-type::topic_type
