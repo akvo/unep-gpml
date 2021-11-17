04c2eb4523adc91316683a4b28fac745a02f9648
Juan A. Ruz <juanantonioruz@gmail.com>
2021-11-17 13:31:23 +0100

    [#567] Refine config.edn and fix lint problem

--- backend/src/gpml/handler/auth.clj
+++ backend/src/gpml/handler/auth.clj
@@ -4,30 +4,32 @@
    ;;   [clojure.string :as str]
    ;;   [gpml.constants :as constants]
    [gpml.handler.util :as util]
+   [gpml.db.topic-stakeholder-auth :as db.ts-auth]
    ;;   [gpml.model.topic :as model.topic]
    [integrant.core :as ig]
    [ring.util.response :as resp]))
 
 
-(defmethod ig/init-key ::get-topic-auth [_ {:keys [db]}]
-  (fn [{{:keys [path]} :parameters approved? :approved? user :user}]
-    (let [conn        (:spec db)
-  ;;        topic       (:topic-type path)
-          ]
-      (if-let [data {}]
-        (resp/response (merge path
+(defmethod ig/init-key ::get-topic-auth [_ {:keys [conn]}]
+  (fn [{{:keys [path]} :parameters user :user}]
+    (let [authorized? user]
+      (if authorized?
+        (if-let [data {}]
+         (resp/response (merge path
+                               data
+                               {:message "topic auth"
+                                :user user}))
+         util/not-found)
+        util/unauthorized))))
 
-                              {:message "topic auth"
-                               :user user}))
-        util/not-found))))
-
-(defmethod ig/init-key ::get-topic-stakeholder-auth [_ {:keys [db]}]
-  (fn [{{:keys [path]} :parameters approved? :approved? user :user}]
-    (let [conn        (:spec db)
-;;          topic       (:topic-type path)
-          ]
-      (if-let [data {}]
-        (resp/response (merge path
-                              {:message "topic auth"
-                               :user user}))
-        util/not-found))))
+(defmethod ig/init-key ::get-topic-stakeholder-auth [_ {:keys [conn]}]
+  (fn [{{:keys [path]} :parameters user :user}]
+    (let [authorized? user]
+      (if authorized?
+        (if-let [data {}]
+          (resp/response (merge path
+                                data
+                               {:message "topic auth"
+                                :user user}))
+         util/not-found)
+        util/unauthorized))))
