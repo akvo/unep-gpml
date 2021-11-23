77294d3948844f01eed7f159ce037ecf70e11c42
Navin <nikshingote@gmail.com>
2021-11-24 01:54:00 +0530

    [#589] Added sticky box

--- frontend/src/modules/flexible-forms/view.jsx
+++ frontend/src/modules/flexible-forms/view.jsx
@@ -2,9 +2,73 @@ import { UIStore } from "../../store";
 import React, { useEffect, useRef, useState } from "react";
 import { Row, Col, Card, Button, Switch } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
+import StickyBox from "react-sticky-box";
+import "./styles.scss";
 
 const FlexibleForms = ({ match: { params }, ...props }) => {
-  return <div>ss</div>;
+  const btnSubmit = useRef();
+  const [sending, setSending] = useState(false);
+  const [highlight, setHighlight] = useState(false);
+  const [disabledBtn, setDisabledBtn] = useState({
+    disabled: true,
+    type: "default",
+  });
+
+  useEffect(() => {
+    UIStore.update((e) => {
+      e.highlight = highlight;
+    });
+  }, [highlight]);
+
+  const handleOnClickBtnSubmit = (e) => {
+    setHighlight(true);
+    btnSubmit.current.click();
+  };
+
+  return (
+    <div id="flexible-forms">
+      <StickyBox style={{ zIndex: 10 }}>
+        <div className="form-info-wrapper">
+          <div className="ui container">
+            <Row>
+              <Col xs={24} lg={24}>
+                <div className={`form-meta `}>
+                  <div className="d-flex">
+                    <Button className="draft-button" size="large">
+                      Save as draft
+                    </Button>
+                    <Button
+                      className="custom-button"
+                      disabled={disabledBtn.disabled}
+                      loading={sending}
+                      type={disabledBtn.type}
+                      size="large"
+                      onClick={(e) => handleOnClickBtnSubmit(e)}
+                    >
+                      Submit
+                    </Button>
+                    <div className="form-title">
+                      <span className="title">Add Content</span>
+                    </div>
+                  </div>
+                  <div className="highlight">
+                    <Switch
+                      checked={highlight}
+                      size="small"
+                      onChange={(status) => setHighlight(status)}
+                    />{" "}
+                    {highlight
+                      ? "Required fields highlighted"
+                      : "Highlight required fields"}
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </div>
+      </StickyBox>
+    </div>
+  );
 };
 
 export default FlexibleForms;
