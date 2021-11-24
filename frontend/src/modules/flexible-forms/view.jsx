e0932e69ad6f935fd040f9bbeb663469d1b29206
Navin <nikshingote@gmail.com>
2021-11-24 18:43:53 +0530

    [#589] Added steps styling

--- frontend/src/modules/flexible-forms/view.jsx
+++ frontend/src/modules/flexible-forms/view.jsx
@@ -1,6 +1,6 @@
 import { UIStore } from "../../store";
 import React, { useEffect, useRef, useState } from "react";
-import { Row, Col, Card, Button, Switch, Radio } from "antd";
+import { Row, Col, Card, Button, Switch, Radio, Steps } from "antd";
 import { DownloadOutlined } from "@ant-design/icons";
 import StickyBox from "react-sticky-box";
 import "./styles.scss";
@@ -10,6 +10,8 @@ import ActionPlanImage from "../../images/action-plan.png";
 import FinancingResourceImage from "../../images/financing-resource.png";
 import TechnicalResourceImage from "../../images/technical-resource.png";
 import CapacityBuildingImage from "../../images/capacity-building.png";
+
+const { Step } = Steps;
 
 const FlexibleForms = ({ match: { params }, ...props }) => {
   const btnSubmit = useRef();
@@ -30,8 +32,6 @@ const FlexibleForms = ({ match: { params }, ...props }) => {
     setHighlight(true);
     btnSubmit.current.click();
   };
-
-  const style = { background: "#0092ff", padding: "8px 0" };
 
   return (
     <div id="flexible-forms">
@@ -75,139 +75,152 @@ const FlexibleForms = ({ match: { params }, ...props }) => {
           </div>
         </div>
       </StickyBox>
-
-      <StickyBox style={{ zIndex: 9 }} offsetTop={20} offsetBottom={20}>
-        <div className="ui container">
-          <div className="form-container">
-            <Row>
-              <Col
-                className="step-panel"
-                xs={24}
-                lg={6}
-                style={{
-                  minHeight: "100%",
-                  padding: "13px 0",
-                }}
-              >
-                Left Panel
-              </Col>
-              <Col
-                className="content-panel"
-                xs={24}
-                lg={18}
-                style={{
-                  minHeight: "100%",
-                  padding: "13px 10px",
-                }}
-              >
-                <Row>
-                  <div className="main-content">
-                    <div className="button-wrapper">
-                      <h5>Pick the main content type</h5>
-                      <Button
-                        icon={<img src={ExampleIcon} alt="Example button" />}
-                        size="large"
-                      >
-                        SHOW EXAMPLES
-                      </Button>
-                    </div>
-                    <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                      <Col className="gutter-row" xs={12} lg={6}>
-                        <div className="content-circle-wrapper">
-                          <div className="content-circle">
-                            <img src={InitiativeImage} alt="Initiative Image" />
-                          </div>
-                          <h2>initiative</h2>
-                        </div>
-                      </Col>
-                      <Col className="gutter-row" xs={12} lg={6}>
-                        <div className="content-circle-wrapper">
-                          <div className="content-circle">
-                            <img
-                              src={ActionPlanImage}
-                              alt="Action Plan Image"
-                            />
-                          </div>
-                          <h2>Action plan</h2>
-                        </div>
-                      </Col>
-                      <Col className="gutter-row" xs={12} lg={6}>
-                        <div className="content-circle-wrapper">
-                          <div className="content-circle">
-                            <img
-                              src={FinancingResourceImage}
-                              alt="Financing Resource Image"
-                            />
-                          </div>
-                          <h2>Policy</h2>
-                        </div>
-                      </Col>
-                      <Col className="gutter-row" xs={12} lg={6}>
-                        <div className="content-circle-wrapper">
-                          <div className="content-circle">
-                            <img
-                              src={FinancingResourceImage}
-                              alt="Financing Resource Image"
-                            />
-                          </div>
-                          <h2>Financing resource</h2>
-                        </div>
-                      </Col>
-                      <Col className="gutter-row" xs={12} lg={6}>
-                        <div className="content-circle-wrapper selected">
-                          <div className="content-circle">
-                            <img
-                              src={TechnicalResourceImage}
-                              alt="Technical Resource Image"
-                            />
-                          </div>
-                          <h2>Technical Resource</h2>
-                        </div>
-                      </Col>
-                      <Col className="gutter-row" xs={12} lg={6}>
-                        <div className="content-circle-wrapper">
-                          <div className="content-circle">
-                            <img
-                              src={CapacityBuildingImage}
-                              alt="Capacity Building Image"
-                            />
-                          </div>
-                          <h2>Event</h2>
-                        </div>
-                      </Col>
-                      <Col className="gutter-row" xs={12} lg={6}>
-                        <div className="content-circle-wrapper">
-                          <div className="content-circle">
-                            <img
-                              src={CapacityBuildingImage}
-                              alt="Capacity Building Image"
-                            />
-                          </div>
-                          <h2>Technology</h2>
-                        </div>
-                      </Col>
-                      <Col className="gutter-row" xs={12} lg={6}>
-                        <div className="content-circle-wrapper">
-                          <div className="content-circle">
-                            <img
-                              src={CapacityBuildingImage}
-                              alt="Capacity Building Image"
-                            />
-                          </div>
-                          <h2>Capacity Building</h2>
-                        </div>
-                      </Col>
-                    </Row>
+      {/* 
+      <StickyBox style={{ zIndex: 9 }} offsetTop={20} offsetBottom={20}> */}
+      <div className="ui container">
+        <div className="form-container">
+          <Row>
+            <Col
+              className="step-panel"
+              xs={24}
+              lg={6}
+              style={{
+                minHeight: "100%",
+              }}
+            >
+              <StickyBox style={{ zIndex: 9 }} offsetTop={100}>
+                <Steps direction="vertical">
+                  <Step title="Personal Details" />
+                  <Step title="Content type" />
+                  <Step title="Basic info" />
+                  <Step title="Detail info" />
+                </Steps>
+              </StickyBox>
+            </Col>
+            <Col
+              className="content-panel"
+              xs={24}
+              lg={18}
+              style={{
+                minHeight: "100%",
+              }}
+            >
+              <Row>
+                <div className="main-content">
+                  <div className="button-wrapper">
+                    <h5>Pick the main content type</h5>
+                    <Button
+                      icon={<img src={ExampleIcon} alt="Example button" />}
+                      size="large"
+                    >
+                      SHOW EXAMPLES
+                    </Button>
                   </div>
-                </Row>
-                <Row>
-                  <div className="sub-content">test</div>
-                </Row>
-              </Col>
-            </Row>
-          </div>
+                  <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                    <Col className="gutter-row" xs={12} lg={6}>
+                      <div className="content-circle-wrapper">
+                        <div className="content-circle">
+                          <img src={InitiativeImage} alt="Initiative Image" />
+                        </div>
+                        <h2>initiative</h2>
+                      </div>
+                    </Col>
+                    <Col className="gutter-row" xs={12} lg={6}>
+                      <div className="content-circle-wrapper">
+                        <div className="content-circle">
+                          <img src={ActionPlanImage} alt="Action Plan Image" />
+                        </div>
+                        <h2>Action plan</h2>
+                      </div>
+                    </Col>
+                    <Col className="gutter-row" xs={12} lg={6}>
+                      <div className="content-circle-wrapper">
+                        <div className="content-circle">
+                          <img
+                            src={FinancingResourceImage}
+                            alt="Financing Resource Image"
+                          />
+                        </div>
+                        <h2>Policy</h2>
+                      </div>
+                    </Col>
+                    <Col className="gutter-row" xs={12} lg={6}>
+                      <div className="content-circle-wrapper">
+                        <div className="content-circle">
+                          <img
+                            src={FinancingResourceImage}
+                            alt="Financing Resource Image"
+                          />
+                        </div>
+                        <h2>Financing resource</h2>
+                      </div>
+                    </Col>
+                    <Col className="gutter-row" xs={12} lg={6}>
+                      <div className="content-circle-wrapper selected">
+                        <div className="content-circle">
+                          <img
+                            src={TechnicalResourceImage}
+                            alt="Technical Resource Image"
+                          />
+                        </div>
+                        <h2>Technical Resource</h2>
+                      </div>
+                    </Col>
+                    <Col className="gutter-row" xs={12} lg={6}>
+                      <div className="content-circle-wrapper">
+                        <div className="content-circle">
+                          <img
+                            src={CapacityBuildingImage}
+                            alt="Capacity Building Image"
+                          />
+                        </div>
+                        <h2>Event</h2>
+                      </div>
+                    </Col>
+                    <Col className="gutter-row" xs={12} lg={6}>
+                      <div className="content-circle-wrapper">
+                        <div className="content-circle">
+                          <img
+                            src={CapacityBuildingImage}
+                            alt="Capacity Building Image"
+                          />
+                        </div>
+                        <h2>Technology</h2>
+                      </div>
+                    </Col>
+                    <Col className="gutter-row" xs={12} lg={6}>
+                      <div className="content-circle-wrapper">
+                        <div className="content-circle">
+                          <img
+                            src={CapacityBuildingImage}
+                            alt="Capacity Building Image"
+                          />
+                        </div>
+                        <h2>Capacity Building</h2>
+                      </div>
+                    </Col>
+                  </Row>
+                </div>
+              </Row>
+              <Row>
+                <div className="sub-content">
+                  <div className="sub-content-top">
+                    <h5>Pick the sub-content type</h5>
+                    <span>Optional</span>
+                  </div>
+                  <div className="before-selection">
+                    <p>
+                      Select a Main Content Type above to see sub-content type
+                      options
+                    </p>
+                  </div>
+                </div>
+              </Row>
+            </Col>
+          </Row>
         </div>
-      </StickyBox>
+      </div>
+      {/* </StickyBox> */}
     </div>
   );
 };
