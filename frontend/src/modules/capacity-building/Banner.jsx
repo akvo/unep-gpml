41a48532874670ab3cce884de3f24e404653d04e
ifirmawan <firmawaneiwan@gmail.com>
2021-12-20 19:27:41 +0700

    [#610] Fix prettier at capacity building

--- frontend/src/modules/capacity-building/Banner.jsx
+++ frontend/src/modules/capacity-building/Banner.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import moment from 'moment'
+import React from "react";
+import moment from "moment";
 
 const Banner = ({ title, category, date }) => (
   <div className="banner_info">
     <div className="banner_img">
       <div className="photo_credit">
-        <div><span>Photo by Izdhaan Nizar</span></div>
+        <div>
+          <span>Photo by Izdhaan Nizar</span>
+        </div>
         21 Jun 2021
       </div>
     </div>
     <div className="event_details">
-      <span className="title">{category || 'Webinar'}</span>
+      <span className="title">{category || "Webinar"}</span>
       <div className="site_title">
-        <h1>{title || ''}</h1>
+        <h1>{title || ""}</h1>
       </div>
-      <div className="event_date">{date || moment().format('DD MMMM YYYY')}</div>
+      <div className="event_date">
+        {date || moment().format("DD MMMM YYYY")}
+      </div>
       <div className="location"></div>
     </div>
   </div>
-)
+);
 
-export default Banner
+export default Banner;
