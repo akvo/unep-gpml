52f74cd4a7ffdc5b10f3956aedb9068278624dcb
Lucas Sousa <lucas.sousa@magnet.coop>
2022-01-12 11:41:04 +0100

    [#664] Create activity when creating / bookmarking resources

---
+++ backend/src/gpml/util.clj
@@ -0,0 +1,20 @@
+(ns gpml.util
+  (:import [java.util UUID]))
+
+(defn uuid
+  "If no argument is passed, creates a random UUID. If the passed
+  paramenter is a UUID, returns it verbatim. If it is a string
+  representing a UUID value return the corresponding UUID. Any other
+  value or invalid string returns nil. "
+  ([]
+   (UUID/randomUUID))
+  ([uuid]
+   (try
+     (cond
+       (uuid? uuid)
+       uuid
+
+       (string? uuid)
+       (UUID/fromString uuid))
+     (catch Exception _
+       nil))))
