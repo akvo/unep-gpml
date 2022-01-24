71ba063df50687b4ee9e35ec4675859735969151
Dipti Gautam <gautamdipti69@gmail.com>
2022-01-25 00:18:30 +0545

    [#611] [#607] Add v_stakeholder_association
    Even though we're migrating from views, and v_organisation_association
    is no longer necessary, the bookmark/favorite endpoint still uses
    the v_stakeholder_association, hence needs to be added back until
    we refactor that part.

--- backend/resources/migrations/127-add-enum-initiative-association.down.sql
+++ backend/resources/migrations/127-add-enum-initiative-association.down.sql
@@ -11,3 +11,39 @@ ALTER TABLE stakeholder_initiative
 ADD COLUMN association project_association;
 --;;
 DROP TYPE initiative_association;
+--;;
+CREATE VIEW v_stakeholder_association AS
+ SELECT 'resource'::text AS topic,
+    stakeholder_resource.stakeholder,
+    stakeholder_resource.resource AS id,
+    (stakeholder_resource.association)::text AS association,
+    stakeholder_resource.remarks
+   FROM stakeholder_resource
+UNION ALL
+ SELECT 'event'::text AS topic,
+    stakeholder_event.stakeholder,
+    stakeholder_event.event AS id,
+    (stakeholder_event.association)::text AS association,
+    stakeholder_event.remarks
+   FROM stakeholder_event
+UNION ALL
+ SELECT 'technology'::text AS topic,
+    stakeholder_technology.stakeholder,
+    stakeholder_technology.technology AS id,
+    (stakeholder_technology.association)::text AS association,
+    stakeholder_technology.remarks
+   FROM stakeholder_technology
+UNION ALL
+ SELECT 'policy'::text AS topic,
+    stakeholder_policy.stakeholder,
+    stakeholder_policy.policy AS id,
+    (stakeholder_policy.association)::text AS association,
+    stakeholder_policy.remarks
+   FROM stakeholder_policy
+UNION ALL
+ SELECT 'initiative'::text AS topic,
+    stakeholder_initiative.stakeholder,
+    stakeholder_initiative.initiative AS id,
+    (stakeholder_initiative.association)::text AS association,
+    stakeholder_initiative.remarks
+   FROM stakeholder_initiative;
