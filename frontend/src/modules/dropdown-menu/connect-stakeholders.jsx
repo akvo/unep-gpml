1ce9a78f3dc39f222edc20f95f2f00709de8cfb5
voromahery <daniel.her@gmail.org>
2022-02-07 16:56:48 +0300

    [#748] Removes dropdown from the connect stakeholders

--- frontend/src/modules/dropdown-menu/connect-stakeholders.jsx
+++ frontend/src/modules/dropdown-menu/connect-stakeholders.jsx
@@ -1,89 +1,16 @@
 import React from "react";
-import { withRouter } from "react-router";
-import { Button, Menu, Dropdown } from "antd";
-import { DownOutlined } from "@ant-design/icons";
-import humps from "humps";
-import { topicNames } from "../../utils/misc";
 
-const ConnectStakeholdersDropdownMenu = withRouter(
-  ({
-    history,
-    profile,
-    setWarningModalVisible,
-    isAuthenticated,
-    setStakeholderSignupModalVisible,
-    loginWithPopup,
-    stakeholderCounts,
-    setFilterMenu,
-  }) => {
-    const redirectPage = (topic) => {
-      setFilterMenu([topic]);
-      return history.push(`/stakeholders?topic=${topic}`);
-    };
+import { Link } from "react-router-dom";
+import { Button } from "antd";
 
-    const handleOnClickNeedAuth = (topic) => {
-      {
-        redirectPage(topic);
-      }
-    };
-
-    const handleComunityClick = () => {
-      window.open("https://communities.gpmarinelitter.org/", "_blank");
-    };
-
-    const loading = !stakeholderCounts;
-
-    return (
-      <Dropdown
-        overlayClassName="menu-dropdown-wrapper"
-        overlay={
-          <Menu className="menu-dropdown">
-            {!loading ? (
-              stakeholderCounts.map((x, i) => {
-                const { name, count } = x;
-                return (
-                  <Menu.Item
-                    key={`${name}-${i}`}
-                    className="nav-link"
-                    disabled={loading}
-                    onClick={() =>
-                      handleOnClickNeedAuth(humps.decamelize(name))
-                    }
-                  >
-                    {topicNames(name)}
-                  </Menu.Item>
-                );
-              })
-            ) : (
-              <Menu.Item className="nav-link">
-                Loading
-                <Button
-                  className="badge-count"
-                  size="small"
-                  type="ghost"
-                  shape="circle"
-                  loading={loading}
-                />
-              </Menu.Item>
-            )}
-            <Menu.Item
-              className="nav-link"
-              disabled={loading}
-              onClick={() => handleComunityClick()}
-            >
-              Community
-            </Menu.Item>
-          </Menu>
-        }
-        trigger={["click"]}
-        placement="bottomRight"
-      >
-        <Button type="link" className="menu-btn nav-link">
-          Connect Stakeholders <DownOutlined />
-        </Button>
-      </Dropdown>
-    );
-  }
-);
+const ConnectStakeholdersDropdownMenu = () => {
+  return (
+    <Link to="/events">
+      <Button type="link" className="menu-btn nav-link">
+        Connect Stakeholders
+      </Button>
+    </Link>
+  );
+};
 
 export default ConnectStakeholdersDropdownMenu;
