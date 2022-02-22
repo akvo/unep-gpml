db27d84cc488e4be0ead50314c4011fcc4676b54
voromahery <daniel.her@gmail.org>
2022-02-22 13:03:27 +0300

    Using only one component for the sidebar
    Refactor code
    
    Add active state for the anchor

--- frontend/src/modules/event-page/view.jsx
+++ frontend/src/modules/event-page/view.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import { Row, Col } from "antd";
 import "./styles.scss";
 import EventCalendar from "../event-calendar/view";
-import LeftSidebar from "./leftSidebar";
+import LeftSidebar from "../left-sidebar/LeftSidebar";
+// Icons
+import IconEvent from "../../images/events/event-icon.svg";
+import IconForum from "../../images/events/forum-icon.svg";
+import IconCommunity from "../../images/events/community-icon.svg";
 
 const EventPage = () => {
+  const sidebar = [
+    { id: 1, title: "Events", url: "", icon: IconEvent },
+    {
+      id: 2,
+      title: "Community",
+      url: "/stakeholder-overview",
+      icon: IconCommunity,
+    },
+    { id: 3, title: "Forums", url: null, icon: IconForum },
+  ];
+
   return (
     <div id="events">
       <Row type="flex" className="body-wrapper">
-        <LeftSidebar />
-        <Col lg={22} xs={24} order={2}>
-          <EventCalendar />
-        </Col>
+        <LeftSidebar active={1} sidebar={sidebar}>
+          <Col lg={24} xs={24} order={2}>
+            <EventCalendar />
+          </Col>
+        </LeftSidebar>
       </Row>
     </div>
   );
--- frontend/src/modules/left-sidebar/LeftSidebar.jsx
+++ frontend/src/modules/left-sidebar/LeftSidebar.jsx
@@ -2,58 +2,44 @@ import React, { useState } from "react";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
 
-import IconLibrary from "../../images/capacity-building/ic-knowledge-library.svg";
-import IconLearning from "../../images/capacity-building/ic-capacity-building.svg";
-import IconExchange from "../../images/capacity-building/ic-exchange.svg";
-import IconCaseStudies from "../../images/capacity-building/ic-case-studies.svg";
 import "./styles.scss";
 
-const LeftSidebar = ({ children, active = 1 }) => {
+const LeftSidebar = ({ children, active = 1, sidebar }) => {
   const [activeMenu, setActiveMenu] = useState(active);
-  const sidebar = [
-    {
-      id: 1,
-      title: "LIBRARY",
-      url: "/knowledge-library",
-      icon: IconLibrary,
-    },
-    {
-      id: 2,
-      title: "LEARNING",
-      url: "/capacity-building",
-      icon: IconLearning,
-    },
-    {
-      id: 4,
-      title: "Case studies",
-      url: "/case-studies",
-      icon: IconCaseStudies,
-    },
-  ];
+
   return (
     <div id="siteWrapper">
       <aside id="mainNavigation">
         <ul className="sidebar">
-          {sidebar.map((s, sx) => (
+          {sidebar?.map((s, sx) => (
             <li
               className={classNames("item-sidebar", {
-                active: activeMenu === s.id,
+                active: activeMenu === s?.id,
               })}
               key={sx}
             >
-              {s.url ? (
+              {s?.url ? (
                 <Link
                   to={s.url}
                   className="item-menu"
                   onClick={() => setActiveMenu(s.id)}
                 >
-                  <img src={s.icon} alt={s.title} />
-                  <p>{s.title}</p>
+                  <img src={s?.icon} alt={s?.title} />
+                  <p>{s?.title}</p>
                 </Link>
+              ) : s.title.toLowerCase() === "forums" ? (
+                <a
+                  href="https://communities.gpmarinelitter.org/"
+                  className="item-menu"
+                  onClick={() => setActiveMenu(s.id)}
+                >
+                  <img src={s?.icon} alt={s?.title} />
+                  <p>{s?.title}</p>
+                </a>
               ) : (
                 <div className="item-menu disabled">
-                  <img src={s.icon} alt={s.title} />
-                  <p>{s.title}</p>
+                  <img src={s?.icon} alt={s?.title} />
+                  <p>{s?.title}</p>
                 </div>
               )}
             </li>
@@ -64,26 +50,26 @@ const LeftSidebar = ({ children, active = 1 }) => {
         <div id="appInnerWrapper">
           <div id="bodyContent">
             <ul className="ant-row ant-row-center sidebar-mobile">
-              {sidebar.map((s, sx) => (
+              {sidebar?.map((s, sx) => (
                 <li
                   className={classNames("ant-col ant-col-8 item-sidebar", {
-                    active: activeMenu === s.id,
+                    active: activeMenu === s?.id,
                   })}
                   key={sx}
                 >
-                  {s.url ? (
+                  {s?.url ? (
                     <Link
                       to={s.url}
                       className="item-menu"
                       onClick={() => setActiveMenu(s.id)}
                     >
-                      <img src={s.icon} alt={s.title} />
-                      <p>{s.title}</p>
+                      <img src={s?.icon} alt={s?.title} />
+                      <p>{s?.title}</p>
                     </Link>
                   ) : (
                     <div className="ant-col ant-col-8 item-menu disabled">
-                      <img src={s.icon} alt={s.title} />
-                      <p>{s.title}</p>
+                      <img src={s?.icon} alt={s?.title} />
+                      <p>{s?.title}</p>
                     </div>
                   )}
                 </li>
