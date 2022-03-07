4439b4d06e15be1ae7af58aecedbb5f935c2e793
voromahery <daniel.her@gmail.org>
2022-03-07 16:57:52 +0300

    Change phase 2 to phase 3

--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -141,7 +141,7 @@ const disclaimerContent = {
   home: (
     <>
       <span>
-        The GPML Digital Platform Phase 2 is now live and currently a Beta
+        The GPML Digital Platform Phase 3 is now live and currently a Beta
         Version. Help us test the platform and let us know what you think at{" "}
         <a style={{ color: "white" }} href="mailto:unep-gpmarinelitter@un.org">
           unep-gpmarinelitter@un.org
