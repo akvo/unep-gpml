92026853d34a81179dbbcbb76b63a66352c323d9
Navin <nikshingote@gmail.com>
2022-03-26 03:14:08 +0530

    #764 add entity signup

---
+++ frontend/src/modules/entity-edit-signup/entity.js
@@ -0,0 +1,211 @@
+import { Store } from "pullstate";
+import { schema } from "./entitySchema";
+import cloneDeep from "lodash/cloneDeep";
+
+const initialSignUpData = {
+  tabs: ["S3"],
+  required: {
+    S1: [],
+    S2: [],
+    S3: [],
+    S4: [],
+    S5: [],
+  },
+  S1: {
+    steps: 0,
+    required: {},
+    authorizeSubmission: true,
+  },
+  S2: {
+    steps: 0,
+    required: {},
+  },
+  S3: {
+    steps: 0,
+    required: {},
+  },
+  S4: {
+    steps: 0,
+    required: {},
+  },
+  S5: {
+    steps: 0,
+    required: {},
+    publicDatabase: true,
+  },
+};
+const signUpData = new Store({
+  data: initialSignUpData,
+  editId: null,
+});
+const getSchema = (
+  {
+    stakeholders,
+    countries,
+    tags,
+    regionOptions,
+    transnationalOptions,
+    meaOptions,
+    sectorOptions,
+    organisationType,
+    representativeGroup,
+    profile,
+  },
+  hideEntityPersonalDetail = false
+) => {
+  const prop = cloneDeep(schema.properties);
+  if (!hideEntityPersonalDetail) {
+    prop.S2.properties.email.default = profile.email;
+    prop.S2.properties.S1_ExpertisesAndActivities.properties[
+      "seeking"
+    ].enum = tags?.seeking?.map((it) => String(it.id));
+    prop.S2.properties.S1_ExpertisesAndActivities.properties[
+      "seeking"
+    ].enumNames = tags?.seeking?.map((it) => it.tag);
+    prop.S2.properties.S1_ExpertisesAndActivities.properties[
+      "offering"
+    ].enum = tags?.offering?.map((it) => String(it.id));
+    prop.S2.properties.S1_ExpertisesAndActivities.properties[
+      "offering"
+    ].enumNames = tags?.offering?.map((it) => it.tag);
+
+    // // country options
+    prop.S2.properties["country"].enum = countries?.map((x) => x.id);
+    prop.S2.properties["country"].enumNames = countries?.map((x) => x.name);
+  }
+
+  const representative = representativeGroup?.map((x) => x.name);
+  prop.S3.properties["org.representativeGroup"].enum = [...representative, -1];
+  prop.S3.properties["org.representativeGroup"].enumNames = [
+    ...representative,
+    "Other",
+  ];
+  prop.S3.properties[
+    "org.representativeGroupGovernment"
+  ].enum = representativeGroup.find((x) => x.code === "government")?.childs;
+  prop.S3.properties[
+    "org.representativeGroupPrivateSector"
+  ].enum = tags?.sector?.map((it) => String(it.id));
+  prop.S3.properties[
+    "org.representativeGroupPrivateSector"
+  ].enumNames = tags?.sector?.map((it) => it.tag);
+  prop.S3.properties[
+    "org.representativeGroupAcademiaResearch"
+  ].enum = representativeGroup.find(
+    (x) => x.code === "academia-research"
+  )?.childs;
+  prop.S3.properties[
+    "org.representativeGroupCivilSociety"
+  ].enum = representativeGroup.find((x) => x.code === "civil-society")?.childs;
+
+  prop.S4.properties["orgExpertise"].enum = tags?.offering?.map((it) =>
+    String(it.id)
+  );
+  prop.S4.properties["orgExpertise"].enumNames = tags?.offering?.map(
+    (it) => it.tag
+  );
+  prop.S5.properties["orgHeadquarter"].enum = countries?.map((x) => x.id);
+
+  prop.S5.properties["orgHeadquarter"].enumNames = countries?.map(
+    (x) => x.name
+  );
+  // prop.S1.properties["registeredStakeholders"].enum = stakeholders?.map((it) =>
+  //   String(it.id)
+  // );
+  // prop.S1.properties["registeredStakeholders"].enumNames = stakeholders?.map(
+  //   (it) =>
+  //     `${it.firstName} ${it.lastName} ${it.email ? "<" + it.email + ">" : ""}`
+  // );
+
+  // geocoverage regional options
+  // prop.S5.properties["geoCoverageValueRegional"].enum = regionOptions.map((x) =>
+  //   String(x.id)
+  // );
+  // prop.S5.properties["geoCoverageValueRegional"].enumNames = regionOptions.map(
+  //   (x) => x.name
+  // );
+  // geocoverage national options
+  prop.S5.properties["geoCoverageValueNational"].enum = countries?.map((x) =>
+    String(x.id)
+  );
+  prop.S5.properties["geoCoverageValueNational"].enumNames = countries?.map(
+    (x) => x.name
+  );
+  // geocoverage sub-national options
+  // prop.S5.properties["geoCoverageValueSubNational"].enum = countries?.map(
+  //   (x) => x.id
+  // );
+  // prop.S5.properties["geoCoverageValueSubNational"].enumNames = countries?.map(
+  //   (x) => x.name
+  // );
+  // geocoverage transnational options
+  prop.S5.properties[
+    "geoCoverageValueTransnational"
+  ].enum = transnationalOptions?.map((x) => String(x.id));
+  prop.S5.properties[
+    "geoCoverageValueTransnational"
+  ].enumNames = transnationalOptions?.map((x) => x.name);
+
+  prop.S5.properties["geoCoverageCountries"].enum = countries?.map((x) =>
+    String(x.id)
+  );
+  prop.S5.properties["geoCoverageCountries"].enumNames = countries?.map(
+    (x) => x.name
+  );
+  // geocoverage global with elements in specific areas options
+  // prop.S5.properties[
+  //   "geoCoverageValueGlobalSpesific"
+  // ].enum = meaOptions?.map((x) => String(x.id));
+  // prop.S5.properties[
+  //   "geoCoverageValueGlobalSpesific"
+  // ].enumNames = meaOptions?.map((x) => x.name);
+
+  return {
+    schema: {
+      ...schema,
+      properties: prop,
+    },
+  };
+};
+
+const tabs = [
+  {
+    key: "S1",
+    title: "Getting Started",
+    desc: "",
+    steps: [],
+  },
+  {
+    key: "S2",
+    title: "Personal Details",
+    desc: "",
+    steps: [],
+  },
+  {
+    key: "S3",
+    title: "Entity Details",
+    desc: "",
+    steps: [],
+  },
+  {
+    key: "S4",
+    title: "Area of Expertise",
+    desc: "",
+    steps: [],
+  },
+  {
+    key: "S5",
+    title: "Geo Coverage",
+    desc: "",
+    steps: [],
+  },
+];
+
+export default {
+  me: "entity",
+  initialSignUpData,
+  signUpData,
+  getSchema,
+  tabs,
+  schema,
+};
---
+++ frontend/src/modules/entity-edit-signup/entitySchema.js
@@ -0,0 +1,252 @@
+import { UIStore } from "../../store";
+import { geoCoverage, newGeoCoverageFormat } from "../../utils/geo";
+
+const {
+  languages,
+  geoCoverageTypeOptions,
+  organisationType,
+} = UIStore.currentState;
+
+const titleField = {
+  title: "Title",
+  type: "string",
+  dependency: [],
+  enum: ["Mr", "Mrs", "Ms", "Dr", "Prof"], //["1-0", "1-1"],
+  enumNames: ["Mr", "Mrs", "Ms", "Dr", "Prof"],
+};
+
+export const schema = {
+  type: "object",
+  version: "2",
+  properties: {
+    S1: {
+      title: "",
+      type: "object",
+      depend: {
+        id: "tabs",
+        value: ["S1"],
+      },
+      value: ["S1"],
+      required: ["authorizeSubmission"],
+      properties: {
+        authorizeSubmission: {
+          title:
+            "By completing this form I confirm that I have the authorization to submit an application on behalf of this Entity to become a member of the Global Partnership on Marine Litter (GPML)​.",
+          type: "boolean",
+        },
+      },
+    },
+    S2: {
+      title: "",
+      type: "object",
+      depend: {
+        id: "tabs",
+        value: ["S2"],
+      },
+      value: ["S2"],
+      required: ["title", "lastName", "firstName", "email"],
+      properties: {
+        title: titleField,
+        lastName: {
+          title: "Last name",
+          type: "string",
+        },
+        firstName: {
+          title: "First name",
+          type: "string",
+        },
+        email: {
+          title: "Email",
+          type: "string",
+        },
+        publicEmail: {
+          title: "Show my email address on public listing",
+          type: "boolean",
+        },
+        linkedIn: {
+          title: "LinkedIn",
+          type: "string",
+        },
+        twitter: {
+          title: "Twitter",
+          type: "string",
+        },
+        photo: {
+          title: "Photo",
+          type: "string",
+          format: "data-url",
+        },
+        country: {
+          title: "Country",
+          type: "integer",
+        },
+
+        S1_ExpertisesAndActivities: {
+          title: "Expertises and activities",
+          type: "object",
+          properties: {
+            seeking: {
+              title: "Seeking",
+              enum: [],
+            },
+            offering: {
+              title: "Offering",
+              enum: [],
+            },
+            about: {
+              title: "About yourself (Bio) (max 100 words)",
+              type: "string",
+            },
+            cv: {
+              title: "CV / Portfolio",
+              type: "string",
+              format: "data-url",
+            },
+          },
+          required: ["seeking", "offering", "about"],
+        },
+      },
+    },
+    S3: {
+      title: "",
+      type: "object",
+      depend: {
+        id: "tabs",
+        value: ["S3"],
+      },
+      required: [
+        "org.name",
+        "org.representativeGroup",
+        "org.representativeGroupGovernment",
+        "org.representativeGroupPrivateSector",
+        "org.representativeGroupAcademiaResearch",
+        "org.representativeGroupCivilSociety",
+        "org.representativeGroupOther",
+        "org.program",
+        "org.url",
+      ],
+      properties: {
+        "org.name": {
+          title: "What is the name of your Entity?",
+          type: "string",
+        },
+        "org.representativeGroup": {
+          title: "Which representative group fits your Entity?",
+          type: "string",
+          enum: [],
+        },
+        "org.representativeGroupGovernment": {
+          depend: {
+            id: "org.representativeGroup",
+            value: ["government"],
+          },
+          title: "Select representative group",
+          type: "string",
+          enum: [],
+        },
+        "org.representativeGroupPrivateSector": {
+          depend: {
+            id: "org.representativeGroup",
+            value: ["private sector (for-profit)"],
+          },
+          title: "Select representative group",
+          type: "string",
+          enum: [],
+        },
+        "org.representativeGroupAcademiaResearch": {
+          depend: {
+            id: "org.representativeGroup",
+            value: ["academia and research"],
+          },
+          title: "Select representative group",
+          type: "string",
+          enum: [],
+        },
+        "org.representativeGroupCivilSociety": {
+          depend: {
+            id: "org.representativeGroup",
+            value: ["civil society (not-for-profit)"],
+          },
+          title: "Select representative group",
+          type: "string",
+          enum: [],
+        },
+        "org.representativeGroupOther": {
+          depend: {
+            id: "org.representativeGroup",
+            value: ["-1"],
+          },
+          title: "Please specify your representative group",
+          type: "string",
+        },
+        "org.program": {
+          title: "Briefly describe your Entity (200 words max)",
+          type: "string",
+        },
+        "org.url": {
+          title: "Your Entity’s website",
+          type: "string",
+          format: "url",
+        },
+        "org.logo": {
+          title: "Upload your Entity’s logo",
+          type: "string",
+          format: "data-url",
+        },
+      },
+    },
+    S4: {
+      title: "",
+      type: "object",
+      depend: {
+        id: "tabs",
+        value: ["S4"],
+      },
+      required: ["orgExpertise"],
+      properties: {
+        orgExpertise: {
+          title:
+            "What areas of interest or expertise does your entity have or offer?",
+          enum: [],
+        },
+      },
+    },
+    S5: {
+      title: "",
+      type: "object",
+      depend: {
+        id: "tabs",
+        value: ["S5"],
+      },
+      required: [
+        "orgHeadquarter",
+        "geoCoverageType",
+        "geoCoverageValueTransnational",
+        "geoCoverageCountries",
+        "geoCoverageValueNational",
+        "publicDatabase",
+      ],
+      properties: {
+        orgHeadquarter: {
+          title: "In which country are you headquarters?",
+          enum: [],
+        },
+        ...newGeoCoverageFormat,
+        geoCoverageType: {
+          ...newGeoCoverageFormat.geoCoverageType,
+          title: "What is the geographical coverage of your Entity?",
+        },
+        orgSubnationalArea: {
+          title:
+            "Please indicate if your Entity operates in a Subnational area only",
+          type: "string",
+        },
+        publicDatabase: {
+          title:
+            "By submitting this form, I will be included in the public database of GPML Digital Platform members and acknowledge that the provided information will be made public and used to find and connect via smart-matchmaking functionalities with other stakeholders and resources.",
+          type: "boolean",
+        },
+      },
+    },
+  },
+};
