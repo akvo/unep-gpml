4cabfef115c8098523c642402eb0e33a1a055e3f
voromahery <daniel.her@gmail.org>
2022-03-25 14:54:01 +0300

    Adding a table

--- frontend/src/modules/map/Map.jsx
+++ frontend/src/modules/map/Map.jsx
@@ -138,23 +138,45 @@ const ToolTipContent = ({ data, geo, path }) => {
           };
 
           return (
-            dataToDisplayPerPath() && (
-              <li key={topic}>
-                <span>{topicNames(topic)}</span>
-                <b className="tooltip-counts">
-                  {dataToDisplay()?.[topic] ? dataToDisplay()?.[topic] : 0}
-                  {transnationalData()?.[topic] > 0 && (
-                    <div
-                      className="transnational-count"
-                      style={{ right: "-5%" }}
-                    >
-                      {" "}
-                      ({transnationalData()?.[topic]})
-                    </div>
-                  )}
-                </b>
-              </li>
-            )
+            <>
+              {/* {dataToDisplayPerPath() && (
+                <li key={topic}>
+                  <span>{topicNames(topic)}</span>
+                  <b className="tooltip-counts">
+                    {dataToDisplay()?.[topic] ? dataToDisplay()?.[topic] : 0}
+                    {transnationalData()?.[topic] > 0 && (
+                      <div
+                        className="transnational-count"
+                        style={{ right: "-5%" }}
+                      >
+                        {" "}
+                        ({transnationalData()?.[topic]})
+                      </div>
+                    )}
+                  </b>
+                </li> */}
+              {/* )} */}
+
+              <table class="styled-table">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Points</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Dom</td>
+                    <td>6000</td>
+                    <td>6000</td>
+                  </tr>
+                  <tr class="active-row">
+                    <td>Melissa</td>
+                    <td>5150</td>
+                  </tr>
+                </tbody>
+              </table>
+            </>
           );
         })}
       </ul>
