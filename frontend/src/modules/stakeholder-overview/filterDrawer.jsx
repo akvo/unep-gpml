a06156ca625387f3469e4e26e5241e869303a17a
voromahery <daniel.her@gmail.org>
2022-03-30 18:39:09 +0300

    [#699] Refactor transnational filter

--- frontend/src/modules/stakeholder-overview/filterDrawer.jsx
+++ frontend/src/modules/stakeholder-overview/filterDrawer.jsx
@@ -121,44 +121,6 @@ const FilterDrawer = ({
       }))
     : [];
 
-  const handleChangeLocationTab = (key) => {
-    const param = key === "country" ? "transnational" : "country";
-  };
-
-  const handleChangeCountry = (val) => {
-    updateQuery("country", query?.country && val);
-  };
-
-  const handleDeselectCountry = (val) => {
-    updateQuery(
-      "country",
-      query?.country ? query?.country.filter((x) => x != val) : []
-    );
-  };
-
-  const handleChangeMultiCountry = (val) => {
-    updateQuery("transnational", [val]);
-
-    // Fetch transnational countries
-    val.forEach((id) => {
-      const check = multiCountryCountries.find((x) => x.id === id);
-      !check &&
-        api.get(`/country-group/${id}`).then((resp) => {
-          setMultiCountryCountries([
-            ...multiCountryCountries,
-            { id: id, countries: resp.data?.[0]?.countries },
-          ]);
-        });
-    });
-  };
-
-  const handleDeselectMultiCountry = (val) => {
-    updateQuery(
-      "transnational",
-      query?.transnational ? query?.transnational.filter((x) => x != val) : []
-    );
-  };
-
   return (
     <div className="site-drawer-render-in-current-wrapper">
       <Drawer
@@ -327,13 +289,11 @@ const FilterDrawer = ({
             <div className="country-filter-tab-wrapper">
               <CountryTransnationalFilter
                 {...{
+                  query,
+                  updateQuery,
                   multiCountryCountries,
-                  handleChangeCountry,
-                  handleDeselectCountry,
-                  handleChangeMultiCountry,
-                  handleDeselectMultiCountry,
+                  setMultiCountryCountries,
                 }}
-                handleChangeTab={handleChangeLocationTab}
                 country={query?.country?.map((x) => parseInt(x)) || []}
                 multiCountry={
                   query?.transnational?.map((x) => parseInt(x)) || []
