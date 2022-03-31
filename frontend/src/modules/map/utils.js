4e0c3dff6b4666108498a1f586909f051144b163
voromahery <daniel.her@gmail.org>
2022-03-31 10:22:48 +0300

    [#651] [#699] Refactor utilities

--- frontend/src/modules/map/utils.js
+++ frontend/src/modules/map/utils.js
@@ -1,62 +1,24 @@
 const sumValues = (obj) => Object.values(obj).reduce((a, b) => a + b);
 
-export const curr = (topic, findData, path) => {
-  let property = [];
-  for (let i = 0; topic?.length > i; i++) {
-    property.push(topic[i]);
-  }
+const snakeToCamel = (str) =>
+  str
+    .toLowerCase()
+    .replace(/([-_][a-z])/g, (group) =>
+      group.toUpperCase().replace("-", "").replace("_", "")
+    );
 
-  property = property.map((text) => {
-    if (text === "action_plan") {
-      return "actionPlan";
-    } else if (text === "financing_resource") {
-      return "financingResource";
-    } else if (text === "technology") {
-      return "technology";
-    } else if (text === "technical_resource") {
-      return "technicalResource";
-    } else {
-      return text;
-    }
-  });
+export const curr = (topics, findData, path) => {
+  const properties = topics.map(snakeToCamel);
 
-  if (topic?.length === 1) {
-    return sumValues({ [property[0]]: findData?.[property[0]] });
-  } else if (topic?.length === 2) {
-    return sumValues({
-      [property[0]]: findData?.[property[0]],
-      [property[1]]: findData?.[property[1]],
-    });
-  } else if (topic?.length === 3) {
-    return sumValues({
-      [property[0]]: findData?.[property[0]],
-      [property[1]]: findData?.[property[1]],
-      [property[2]]: findData?.[property[2]],
-    });
-  } else if (topic?.length === 4) {
-    return sumValues({
-      [property[0]]: findData?.[property[0]],
-      [property[1]]: findData?.[property[1]],
-      [property[2]]: findData?.[property[2]],
-      [property[3]]: findData?.[property[3]],
-    });
-  } else if (topic?.length === 5) {
-    return sumValues({
-      [property[0]]: findData?.[property[0]],
-      [property[1]]: findData?.[property[1]],
-      [property[2]]: findData?.[property[2]],
-      [property[3]]: findData?.[property[3]],
-      [property[4]]: findData?.[property[4]],
-    });
-  } else if (topic?.length === 6) {
-    return sumValues({
-      [property[0]]: findData?.[property[0]],
-      [property[1]]: findData?.[property[1]],
-      [property[2]]: findData?.[property[2]],
-      [property[3]]: findData?.[property[3]],
-      [property[4]]: findData?.[property[4]],
-      [property[5]]: findData?.[property[5]],
-    });
+  const propsToSum = properties.reduce((acc, curr, index) => {
+    const currProp = properties[index];
+
+    acc[currProp] = findData?.[currProp];
+    return acc;
+  }, {});
+
+  if (properties.length) {
+    return sumValues(propsToSum);
   } else {
     if (path === "/knowledge-library") {
       return sumValues({
