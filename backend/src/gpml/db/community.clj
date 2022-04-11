c91b8ab318c26150e55f43449eb7b7e6d5b79c7b
Lucas Sousa <lucas.sousa@magnet.coop>
2022-04-11 12:48:54 +0200

    [#997] Add order by functionality to community API

--- backend/src/gpml/db/community.clj
+++ backend/src/gpml/db/community.clj
@@ -7,11 +7,16 @@
 
 (defn get-community-members-query-and-filters
   [params]
-  (let [{:keys [count-only? limit page filters]} params
-        {:keys [search-text network-type affiliation geo-coverage-type transnational country tag representative-group]} filters
+  (let [{:keys [count-only? limit page filters order-by descending]} params
+        {:keys [search-text network-type affiliation geo-coverage-type transnational
+                country tag representative-group]} filters
         pagination (when (and (not count-only?)
                               limit page)
                      (format "LIMIT %d OFFSET %d" limit (* limit page)))
+        order-by (when order-by
+                   (format "ORDER BY %s %s" order-by (if descending
+                                                       "DESC"
+                                                       "ASC")))
         tags-join (when (seq tag)
                     "JOIN json_array_elements(tags) ts ON TRUE
                      JOIN json_each_text(ts) t ON LOWER(t.value) = ANY (ARRAY[:v*:filters.tag]::VARCHAR[]) AND LOWER(t.value) IS NOT NULL")
@@ -51,4 +56,5 @@
                FROM community_members cm
                %s
                %s
-               %s;" (or tags-join "") where-cond (or pagination "")))))
+               %s
+               %s;" (or tags-join "") where-cond order-by (or pagination "")))))
