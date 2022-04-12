aaa8dc64da799b70de7945b6f66729e011671677
voromahery <daniel.her@gmail.org>
2022-04-12 12:10:54 +0300

    [#651] Using a separate component for the header

---
+++ frontend/src/modules/knowledge-library/header.jsx
@@ -0,0 +1,180 @@
+import React, { useState } from "react";
+import "./header.scss";
+import { Row, Col, Button, Input, Space, Select } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import FilterIcon from "../../images/knowledge-library/filter-icon.svg";
+import { ReactComponent as SortIcon } from "../../images/knowledge-library/sort-icon.svg";
+import { withRouter, useHistory } from "react-router-dom";
+import { KNOWLEDGE_LIBRARY } from "../map/Map";
+
+const KnowledgeLibrarySearch = withRouter(({ history, updateQuery }) => {
+  const [search, setSearch] = useState("");
+  const handleSearch = (src) => {
+    if (src) {
+      history.push(`?q=${src.trim()}`);
+      updateQuery("q", src.trim());
+    } else {
+      updateQuery("q", "");
+    }
+    setSearch("");
+  };
+
+  return (
+    <div className="src">
+      <Input
+        className="input-src"
+        placeholder="Search resources"
+        value={search}
+        suffix={
+          <Button
+            onClick={() => handleSearch(search)}
+            type="primary"
+            shape="circle"
+            size="small"
+            icon={<SearchOutlined />}
+          />
+        }
+        onPressEnter={(e) => handleSearch(e.target.value)}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </div>
+  );
+});
+
+const StakeholderOverviewSearch = withRouter(
+  ({ history, updateQuery, setView }) => {
+    const [search, setSearch] = useState("");
+    const handleSearch = (src) => {
+      if (src) {
+        history.push(`?q=${src.trim()}`);
+        updateQuery("q", src.trim());
+      } else {
+        updateQuery("q", "");
+      }
+      setSearch("");
+    };
+
+    return (
+      <div className="src">
+        <Input
+          className="input-src"
+          placeholder="Search the community"
+          value={search}
+          suffix={
+            <Button
+              onClick={() => handleSearch(search)}
+              type="primary"
+              shape="circle"
+              size="small"
+              icon={<SearchOutlined />}
+            />
+          }
+          onPressEnter={(e) => handleSearch(e.target.value)}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            if (e.target.value.length >= 3) {
+              history.push(`?q=${e.target.value.trim()}`);
+              updateQuery("q", e.target.value.trim());
+            }
+            if (e.target.value.length === 0) {
+              updateQuery("q", "");
+            }
+          }}
+        />
+      </div>
+    );
+  }
+);
+
+const Header = ({
+  setView,
+  sortResults,
+  isAscending,
+  filterVisible,
+  setFilterVisible,
+  selectionValue,
+  filterTagValue,
+  renderFilterTag,
+  updateQuery,
+}) => {
+  const history = useHistory();
+  const path = history?.location?.pathname;
+  return (
+    <Col span={24} className="ui-header">
+      <div className="ui-container">
+        <Row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          gutter={[10, 10]}
+          className="header-filter-option"
+        >
+          {/* Search input & filtered by list */}
+          <Col lg={22} md={20} sm={18}>
+            <Row type="flex" justify="space-between" align="middle">
+              <Col lg={5} md={7} sm={9} className="search-box">
+                <Space>
+                  {/* <Search updateQuery={updateQuery} /> */}
+                  {path === KNOWLEDGE_LIBRARY ? (
+                    <KnowledgeLibrarySearch {...{ updateQuery }} />
+                  ) : (
+                    <StakeholderOverviewSearch {...{ updateQuery }} />
+                  )}
+                  <Button
+                    onClick={() => setFilterVisible(!filterVisible)}
+                    type="ghost"
+                    shape="circle"
+                    className="filter-icon-button"
+                    icon={
+                      <img
+                        src={FilterIcon}
+                        className="filter-icon"
+                        alt="config-icon"
+                      />
+                    }
+                  />
+                  <Button
+                    className="sort-btn"
+                    onClick={() => sortResults(!isAscending)}
+                  >
+                    <SortIcon
+                      style={{
+                        transform:
+                          isAscending || isAscending === null
+                            ? "initial"
+                            : "rotate(180deg)",
+                      }}
+                    />
+                  </Button>
+                </Space>
+              </Col>
+              {filterTagValue.length > 0 && (
+                <Col lg={19} md={17} sm={15} className="filter-tag">
+                  <Space direction="horizontal">{renderFilterTag()}</Space>
+                </Col>
+              )}
+            </Row>
+          </Col>
+          {/* Map/Topic view dropdown */}
+          <Col lg={2} md={4} sm={6} className="select-wrapper">
+            <Select
+              dropdownClassName="overlay-zoom"
+              className="view-selection"
+              value={selectionValue}
+              onChange={(val) => setView(val)}
+            >
+              <Select.Option value="map">Map View</Select.Option>
+              {path === KNOWLEDGE_LIBRARY ? (
+                <Select.Option value="topic">Topic View </Select.Option>
+              ) : (
+                <Select.Option value="card">Card View </Select.Option>
+              )}
+            </Select>
+          </Col>
+        </Row>
+      </div>
+    </Col>
+  );
+};
+
+export default Header;
