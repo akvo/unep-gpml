5d09a9d7471785d4f343d1fe6df3a18053d4ceba
voromahery <daniel.her@gmail.org>
2022-04-12 12:10:54 +0300

    [#651] Using a separate component for the header

--- frontend/src/modules/knowledge-library/view.jsx
+++ frontend/src/modules/knowledge-library/view.jsx
@@ -2,7 +2,6 @@ import React, { useState, useLayoutEffect, useEffect } from "react";
 import { Row, Col, Button, Input, Space, Tag, Select } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 
-import FilterIcon from "../../images/knowledge-library/filter-icon.svg";
 import GlobeOutlined from "../../images/knowledge-library/globe-outline.svg";
 import TooltipOutlined from "../../images/knowledge-library/tooltip-outlined.svg";
 import DownArrow from "../../images/knowledge-library/chevron-down.svg";
@@ -12,8 +11,6 @@ import { UIStore } from "../../store";
 import LeftSidebar from "../left-sidebar/LeftSidebar";
 import ResourceList from "./ResourceList";
 import FilterDrawer from "./FilterDrawer";
-
-import { withRouter } from "react-router-dom";
 
 import api from "../../utils/api";
 
@@ -28,9 +25,10 @@ import { ReactComponent as IconLibrary } from "../../images/capacity-building/ic
 import { ReactComponent as IconLearning } from "../../images/capacity-building/ic-capacity-building.svg";
 import { ReactComponent as IconExchange } from "../../images/capacity-building/ic-exchange.svg";
 import { ReactComponent as IconCaseStudies } from "../../images/capacity-building/ic-case-studies.svg";
-import { ReactComponent as SortIcon } from "../../images/knowledge-library/sort-icon.svg";
+
 import { titleCase } from "../../utils/string";
 import { multicountryGroups } from "./multicountry";
+import Header from "./header";
 
 const { Option } = Select;
 
@@ -312,71 +310,19 @@ const KnowledgeLibrary = ({
   return (
     <Row id="knowledge-library">
       {/* Header */}
-      <Col span={24} className="ui-header">
-        <div className="ui-container">
-          <Row
-            type="flex"
-            justify="space-between"
-            align="middle"
-            gutter={[10, 10]}
-            className="header-filter-option"
-          >
-            {/* Search input & filtered by list */}
-            <Col lg={22} md={20} sm={18}>
-              <Row type="flex" justify="space-between" align="middle">
-                <Col lg={5} md={7} sm={9} className="search-box">
-                  <Space>
-                    <Search updateQuery={updateQuery} />
-                    <Button
-                      onClick={() => setFilterVisible(!filterVisible)}
-                      type="ghost"
-                      shape="circle"
-                      className="filter-icon-button"
-                      icon={
-                        <img
-                          src={FilterIcon}
-                          className="filter-icon"
-                          alt="config-icon"
-                        />
-                      }
-                    />
-                    <Button
-                      className="sort-btn"
-                      onClick={() => sortResults(!isAscending)}
-                    >
-                      <SortIcon
-                        style={{
-                          transform:
-                            isAscending || isAscending === null
-                              ? "initial"
-                              : "rotate(180deg)",
-                        }}
-                      />
-                    </Button>
-                  </Space>
-                </Col>
-                {filterTagValue.length > 0 && (
-                  <Col lg={19} md={17} sm={15} className="filter-tag">
-                    <Space direction="horizontal">{renderFilterTag()}</Space>
-                  </Col>
-                )}
-              </Row>
-            </Col>
-            {/* Map/Topic view dropdown */}
-            <Col lg={2} md={4} sm={6} className="select-wrapper">
-              <Select
-                dropdownClassName="overlay-zoom"
-                className="view-selection"
-                value={selectionValue}
-                onChange={(val) => setView(val)}
-              >
-                <Option value="map">Map View</Option>
-                <Option value="topic">Topic View </Option>
-              </Select>
-            </Col>
-          </Row>
-        </div>
-      </Col>
+      <Header
+        {...{
+          isAscending,
+          updateQuery,
+          filterVisible,
+          setFilterVisible,
+          sortResults,
+          filterTagValue,
+          renderFilterTag,
+          selectionValue,
+          setView,
+        }}
+      />
 
       {/* Content */}
       <Col span={24}>
@@ -498,39 +444,5 @@ const KnowledgeLibrary = ({
     </Row>
   );
 };
-
-const Search = withRouter(({ history, updateQuery }) => {
-  const [search, setSearch] = useState("");
-  const handleSearch = (src) => {
-    if (src) {
-      history.push(`?q=${src.trim()}`);
-      updateQuery("q", src.trim());
-    } else {
-      updateQuery("q", "");
-    }
-    setSearch("");
-  };
-
-  return (
-    <div className="src">
-      <Input
-        className="input-src"
-        placeholder="Search resources"
-        value={search}
-        suffix={
-          <Button
-            onClick={() => handleSearch(search)}
-            type="primary"
-            shape="circle"
-            size="small"
-            icon={<SearchOutlined />}
-          />
-        }
-        onPressEnter={(e) => handleSearch(e.target.value)}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-    </div>
-  );
-});
 
 export default KnowledgeLibrary;
