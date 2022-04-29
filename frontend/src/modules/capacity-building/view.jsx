f6979266febc0fa05379dd005409a0d98fad07ce
voromahery <daniel.her@gmail.org>
2022-04-29 14:03:00 +0300

    [#1088] Add arrow after see more text

--- frontend/src/modules/capacity-building/view.jsx
+++ frontend/src/modules/capacity-building/view.jsx
@@ -121,7 +121,7 @@ const CapacityBuilding = () => {
                           gutter: 16,
                           xs: 1,
                           sm: 1,
-                          md: 1,
+                          md: 2,
                           lg: 2,
                           xl: 3,
                           xxl: 3,
