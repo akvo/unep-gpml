aa334c6a61b7a1906f7ddb87402f56fa4e53f518
Lucas Sousa <lucas.sousa@magnet.coop>
2022-04-29 09:58:44 +0200

    [#1073] Add tests for put and delete comments handlers

--- backend/test/gpml/handler/comment_test.clj
+++ backend/test/gpml/handler/comment_test.clj
@@ -72,11 +72,11 @@
         handler (::sut/get system)
         profile (make-profile "John" "Doe" "mail@org.com")
         _ (seeder/seed db {:country? true :event? true})
-        sth-id (:id (db.stakeholder/new-stakeholder db profile))]
-    (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
-                                             :id (util/uuid)
-                                             :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
-                                      (util/replace-in-keys #"_" "-")))
+        sth-id (:id (db.stakeholder/new-stakeholder db profile))
+        comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
+                                                         :id (util/uuid)
+                                                         :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                  (util/replace-in-keys #"_" "-")))]
     (testing "Happy path"
       (let [request (-> (mock/request :gett "/")
                         (assoc :parameters {:query {:resource_id "1" :resource_type "event"}}))
@@ -89,4 +89,89 @@
         (is (some #{::m/missing-key} (map :type (:errors (m/explain sut/get-resource-comments-params parameters)))))))
     (testing "Validation fails due to invalid parameter schema"
       (let [parameters {:resource_type "event" :resource_id :a}]
-        (is (not (m/validate sut/get-resource-comments-params parameters)))))))
+        (is (not (m/validate sut/get-resource-comments-params parameters)))))
+    (testing "Returns a tree like datastructure when there are comment replies"
+      (let [reply-comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
+                                                                   :id (util/uuid)
+                                                                   :parent-id (:id comment)
+                                                                   :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                            (util/replace-in-keys #"_" "-")))
+            request (-> (mock/request :get "/")
+                        (assoc :parameters {:query {:resource_id "1" :resource_type "event"}}))
+            {:keys [status body]} (handler request)]
+        (is (= 200 status))
+        (is (= (:id reply-comment) (-> body :comments first :children first :id)))))))
+
+(deftest update-comment-test
+  (let [db (test-util/db-test-conn)
+        system (-> fixtures/*system*
+                   (ig/init [::sut/put]))
+        handler (::sut/put system)
+        profile (make-profile "John" "Doe" "mail@org.com")
+        _ (seeder/seed db {:country? true :event? true})
+        sth-id (:id (db.stakeholder/new-stakeholder db profile))
+        comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
+                                                         :id (util/uuid)
+                                                         :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                  (util/replace-in-keys #"_" "-")))]
+    (testing "Happy path"
+      (let [request (-> (mock/request :put "/")
+                        (assoc :parameters {:body {:id (str (:id comment))
+                                                   :title "test comment"
+                                                   :content "test content"}}))
+            {:keys [status body]} (handler request)
+            updated-comment (first (db.comment/get-resource-comments db {:id (:id comment)}))]
+        (is (= 200 status))
+        (is (= 1 (:updated-comments body)))
+        (is (= "test comment" (:title updated-comment)))
+        (is (= "test content" (:content updated-comment)))))
+    (testing "Fails validation due to missing required parameters"
+      (let [parameters {}]
+        (is (not (m/validate sut/update-comment-params parameters)))
+        (is (some #{::m/missing-key} (map :type (:errors (m/explain sut/update-comment-params parameters)))))))
+    (testing "Fails validation due to invalid parameter schema"
+      (let [parameters {:id 1
+                        :title :a
+                        :content true}]
+        (is (not (m/validate sut/update-comment-params parameters)))
+        (is (= 3 (count (:errors (m/explain sut/update-comment-params parameters)))))))))
+
+(deftest delete-comment-test
+  (let [db (test-util/db-test-conn)
+        system (-> fixtures/*system*
+                   (ig/init [::sut/delete]))
+        handler (::sut/delete system)
+        profile (make-profile "John" "Doe" "mail@org.com")
+        _ (seeder/seed db {:country? true :event? true})
+        sth-id (:id (db.stakeholder/new-stakeholder db profile))]
+    (testing "Happy path"
+      (let [comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
+                                                             :id (util/uuid)
+                                                             :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                      (util/replace-in-keys #"_" "-")))
+            request (-> (mock/request :delete "/")
+                        (assoc :parameters {:path {:id (str (:id comment))}}))
+            {:keys [status body]} (handler request)]
+        (is (= 200 status))
+        (is (= 1 (:deleted-comments body)))))
+    (testing "Deleting a root comment also delete its replies"
+      (let [comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
+                                                             :id (util/uuid)
+                                                             :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                      (util/replace-in-keys #"_" "-")))
+            _ (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
+                                                       :id (util/uuid)
+                                                       :parent-id (:id comment)
+                                                       :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                (util/replace-in-keys #"_" "-")))
+            request (-> (mock/request :delete "/")
+                        (assoc :parameters {:path {:id (str (:id comment))}}))
+            {:keys [status body]} (handler request)
+            comments (db.comment/get-resource-comments db {})]
+        (is (= 200 status))
+        ;; NOTE: since the parent-id foreign key has the ON DELETE
+        ;; CASCADE the deletion of child comments doesn't account to
+        ;; number of rows affected. Therefore we just get the number
+        ;; of affected rows by the DELETE statement.
+        (is (= 1 (:deleted-comments body)))
+        (is (not (seq comments)))))))
