8b2bbce0ae7a57eb7c9ca0450b6c33ddccd1da3d
Navin <nikshingote@gmail.com>
2022-05-02 15:39:04 +0530

    #1103 add export option

--- frontend/src/modules/profile/admin.jsx
+++ frontend/src/modules/profile/admin.jsx
@@ -286,6 +286,7 @@ const AdminSection = ({
   const [approveLoading, setApproveLoading] = useState({});
   const [loadingAssignReviewer, setLoadingAssignReviewer] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [exportLoading, setExportLoading] = useState(false);
 
   const [tab, setTab] = useState("stakeholders");
   const [stakeholdersListOpts, setStakeholdersListOpts] = useState({
@@ -432,6 +433,39 @@ const AdminSection = ({
     }
   };
 
+  const downloadCSV = async (data, name) => {
+    const blob = new Blob([data], { type: "data:text/csv;charset=utf-8," });
+    const blobURL = window.URL.createObjectURL(blob);
+
+    const anchor = document.createElement("a");
+    anchor.download = name;
+    anchor.href = blobURL;
+    anchor.dataset.downloadurl = [
+      "text/csv",
+      anchor.download,
+      anchor.href,
+    ].join(":");
+    anchor.click();
+
+    setTimeout(() => {
+      URL.revokeObjectURL(blobURL);
+    }, 100);
+  };
+
+  const exportList = (type) => {
+    setExportLoading(true);
+    api
+      .get(`/export/${type}`)
+      .then((res) => {
+        downloadCSV(res.data, `${type}.csv`);
+        setExportLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setExportLoading(false);
+      });
+  };
+
   const assignReviewer = (item, reviewers, listOpts, setListOpts) => {
     setLoadingAssignReviewer(item);
     const apiCall = isEmpty(item?.reviewers) ? api.post : api.patch;
@@ -544,6 +578,33 @@ const AdminSection = ({
       }
     >
       {publishStatusUIText[uiTitle]}
+    </Button>
+  );
+
+  const ExportButton = ({
+    item,
+    type,
+    className = "",
+    disabled = false,
+    listOpts,
+    setListOpts,
+  }) => (
+    <Button
+      type={type}
+      className={className}
+      disabled={disabled}
+      loading={exportLoading}
+      onClick={() =>
+        exportList(
+          listOpts.type === "stakeholders"
+            ? "users"
+            : listOpts.type === "resources"
+            ? "topics"
+            : listOpts.type
+        )
+      }
+    >
+      Export
     </Button>
   );
 
@@ -695,17 +756,24 @@ const AdminSection = ({
     };
 
     return (
-      <div key="new-approval" className="approval">
+      <div key={`new-approval-${title ? title : ""}`} className="approval">
         {title && <Title className="tab-label" level={4}>{`${title}`}</Title>}
         <div>
           <b>Total:</b> {itemList.count || 0}
         </div>
         {(listOpts.reviewStatus || listOpts.title) && (
           <div>
-            <div>
+            <div className="export-wrapper">
               <b>Filtering by:</b>
-              <hr />
+              <div>
+                <ExportButton
+                  type="ghost"
+                  className="black"
+                  listOpts={listOpts}
+                />
+              </div>
             </div>
+            <hr />
             {listOpts.reviewStatus && (
               <div>
                 <b>Review status:</b> {statusDictToHuman[listOpts.reviewStatus]}
