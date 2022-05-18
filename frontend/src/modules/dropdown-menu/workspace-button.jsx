5ebc10cff98db034d7e699662600ee5dfa5f99a9
Navin <nikshingote@gmail.com>
2022-05-18 22:37:53 +0530

    run prettier

--- frontend/src/modules/dropdown-menu/workspace-button.jsx
+++ frontend/src/modules/dropdown-menu/workspace-button.jsx
@@ -3,10 +3,14 @@ import { HomeOutlined } from "@ant-design/icons";
 import { NavLink } from "react-router-dom";
 
 const WorkspaceButton = () => (
-  <NavLink to="/workspace" className="btn-workspace menu-btn" activeClassName="selected">
+  <NavLink
+    to="/workspace"
+    className="btn-workspace menu-btn"
+    activeClassName="selected"
+  >
     <HomeOutlined />
     Workspace
   </NavLink>
-)
+);
 
 export default WorkspaceButton;
