390a7caeb5f62e187b3cf18f494d2d3ba69ffcb7
lucassousaf <lucas.sousa@magnet.coop>
2022-05-23 14:45:48 +0200

    [#1209] Fix down file update statement: IS NULL -> = NULL

--- backend/resources/migrations/150-fix-populate-stakeholder-tag-relation-category.down.sql
+++ backend/resources/migrations/150-fix-populate-stakeholder-tag-relation-category.down.sql
@@ -1,11 +1,11 @@
 BEGIN;
 --;;
-UPDATE stakeholder_tag SET tag_relation_category IS NULL
+UPDATE stakeholder_tag SET tag_relation_category = NULL
 FROM tag
 WHERE stakeholder_tag.tag = tag.id
 AND tag.tag_category = 1;
 --;;
-UPDATE stakeholder_tag SET tag_relation_category IS NULL
+UPDATE stakeholder_tag SET tag_relation_category = NULL
 FROM tag
 WHERE stakeholder_tag.tag = tag.id
 AND tag.tag_category = 10;
