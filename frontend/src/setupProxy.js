999a6a096e904a1a4ebfbc4eaf5b11037e7e2cc6
Martin Christov <martin.christov@gmail.com>
2022-06-03 09:47:00 +0300

    standalone FE env starting with proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     ["/api", "/image", "/env.js", "/cv"],
     createProxyMiddleware({
-      target: "http://backend:3000",
+      target: process.env.REACT_APP_FEENV ? "https://unep-gpml.akvotest.org/" : 'http://backend:3000',
       changeOrigin: true,
     })
   );
