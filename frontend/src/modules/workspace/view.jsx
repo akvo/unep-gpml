35a58f7fd15d8fba58ace9542341487abe516503
Navin <nikshingote@gmail.com>
2022-07-04 21:57:20 +0530

    update focal point

--- frontend/src/modules/workspace/view.jsx
+++ frontend/src/modules/workspace/view.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col, Carousel, Avatar, Typography, Button, List } from "antd";
 const { Title, Link } = Typography;
-
 import "./styles.scss";
 import Header from "./header";
 import LeftSidebar from "../../components/left-sidebar/left-sidebar";
@@ -17,6 +16,8 @@ import Video from "../../images/workspace/video.png";
 import FAQ from "../../images/workspace/faq.png";
 
 const Workspace = ({ profile }) => {
+  const [isFocal, setIsFocal] = useState(false);
+
   const userName =
     profile?.firstName !== undefined &&
     profile?.lastName !== undefined &&
@@ -32,6 +33,15 @@ const Workspace = ({ profile }) => {
     },
   ];
 
+  const handleFocalPoint = () => {
+    setIsFocal(true);
+    localStorage.setItem("is_focal", true);
+  };
+
+  useEffect(() => {
+    setIsFocal(localStorage.getItem("is_focal"));
+  }, []);
+
   return (
     <div id="workspace">
       <Row type="flex">
@@ -39,84 +49,87 @@ const Workspace = ({ profile }) => {
           {/* <Header userName={userName} /> */}
           <div className="workspace-content-wrapper">
             <div className="workspace-container">
-              {profile.org && (
-                <Row className="bg-white">
-                  <Col lg={12} sm={24}>
-                    <div className="content-container">
-                      <p className="recommend-text">RECOMMENDED</p>
-                      <Title level={2}>GPML Partnership​</Title>
-                      <p className="registration-text">
-                        Hello, It looks like your entity: <b>Entity Name,</b> is
-                        not yet part <br /> of the GPML partnership.
-                        <br /> If you are the focal point, submit your
-                        application below
-                      </p>
-                      <div className="join-box">
-                        <div>
-                          <p>
-                            By completing this form I confirm that I have the
-                            authorization to submit an application on behalf of
-                            this Entity to become a member of the Global
-                            Partnership on Marine Litter (GPML)​.
-                          </p>
-                        </div>
-                        <div className="button-container">
+              <Row className="bg-white">
+                <Col lg={12} sm={24}>
+                  <div className="content-container">
+                    <p className="recommend-text">RECOMMENDED</p>
+                    <Title level={2}>GPML Partnership​</Title>
+                    <p className="registration-text">
+                      Hello, It looks like your entity: <b>Entity Name,</b> is
+                      not yet part <br /> of the GPML partnership.
+                      <br /> If you are the focal point, submit your application
+                      below
+                    </p>
+                    <div className="join-box">
+                      <div>
+                        <p>
+                          By completing this form I confirm that I have the
+                          authorization to submit an application on behalf of
+                          this Entity to become a member of the Global
+                          Partnership on Marine Litter (GPML)​.
+                        </p>
+                      </div>
+                      <div className="button-container">
+                        <Button
+                          className="join-button"
+                          type="primary"
+                          shape="round"
+                        >
+                          JOIN GPML
+                        </Button>
+                        {!isFocal && (
                           <Button
-                            className="join-button"
-                            type="primary"
-                            shape="round"
+                            className="focal-point"
+                            onClick={() => handleFocalPoint()}
                           >
-                            JOIN GPML
-                          </Button>
-                          <Button className="focal-point">
                             I AM NOT THE FOCAL POINT
                           </Button>
-                        </div>
+                        )}
                       </div>
                     </div>
-                  </Col>
-                  <Col lg={12} sm={24}>
-                    <div className="slider-container">
-                      <Carousel effect="fade">
-                        <div>
-                          <div className="slider-wrapper">
-                            <Avatar
-                              src={NetworkIcon}
-                              style={{
-                                borderRadius: "initial",
-                                margin: "0 auto 40px auto",
-                                display: "block",
-                                width: 160,
-                                height: 140,
-                              }}
-                            />
-                            <Title level={2}>
-                              Tap into a global network of like-minded members​
-                            </Title>
-                          </div>
+                  </div>
+                </Col>
+                <Col lg={12} sm={24}>
+                  <div className="slider-container">
+                    <Carousel effect="fade">
+                      <div>
+                        <div className="slider-wrapper">
+                          <Avatar
+                            src={NetworkIcon}
+                            style={{
+                              borderRadius: "initial",
+                              margin: "0 auto 40px auto",
+                              display: "block",
+                              width: 160,
+                              height: 140,
+                            }}
+                          />
+                          <Title level={2}>
+                            Tap into a global network of like-minded members​
+                          </Title>
                         </div>
-                        <div>
-                          <div className="slider-wrapper">
-                            <Avatar
-                              src={NetworkIcon}
-                              style={{
-                                borderRadius: "initial",
-                                margin: "0 auto 40px auto",
-                                display: "block",
-                                width: 160,
-                                height: 140,
-                              }}
-                            />
-                            <Title level={2}>
-                              Network with other stakeholders
-                            </Title>
-                          </div>
+                      </div>
+                      <div>
+                        <div className="slider-wrapper">
+                          <Avatar
+                            src={NetworkIcon}
+                            style={{
+                              borderRadius: "initial",
+                              margin: "0 auto 40px auto",
+                              display: "block",
+                              width: 160,
+                              height: 140,
+                            }}
+                          />
+                          <Title level={2}>
+                            Network with other stakeholders
+                          </Title>
                         </div>
-                      </Carousel>
-                    </div>
-                  </Col>
-                </Row>
-              )}
+                      </div>
+                    </Carousel>
+                  </div>
+                </Col>
+              </Row>
               <Row gutter={[8, 16]} className="action-plan-container">
                 <Col lg={12} sm={24}>
                   <div className="content-container">
