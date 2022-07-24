a8407f52806ab27b851934efe67cc14cc07488ad
Navin <nikshingote@gmail.com>
2022-07-25 01:06:22 +0530

    add nested route

--- frontend/src/components/left-sidebar/left-sidebar.jsx
+++ frontend/src/components/left-sidebar/left-sidebar.jsx
@@ -1,21 +1,52 @@
 import React, { useState } from "react";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+import { ReactComponent as IconEvent } from "../../images/events/event-icon.svg";
+import { ReactComponent as IconForum } from "../../images/events/forum-icon.svg";
+import { ReactComponent as IconCommunity } from "../../images/events/community-icon.svg";
+import { ReactComponent as IconPartner } from "../../images/stakeholder-overview/partner-icon.svg";
+import { ReactComponent as ExpertIcon } from "../../images/stakeholder-overview/expert-icon.svg";
 
 import "./styles.scss";
 
-const LeftSidebar = ({ children, active = 1, sidebar }) => {
+const sidebar = [
+  { id: 1, title: "Events", url: "/connect/events", icon: <IconEvent /> },
+  {
+    id: 2,
+    title: "Community",
+    url: "/connect/community",
+    icon: <IconCommunity />,
+  },
+  { id: 3, title: "Forums", url: null, icon: <IconForum /> },
+  {
+    id: 4,
+    title: "Partners",
+    url: "/connect/partners",
+    icon: <IconPartner />,
+  },
+  {
+    id: 5,
+    title: "Experts",
+    url: "/connect/experts",
+    icon: <ExpertIcon />,
+  },
+];
+
+const LeftSidebar = ({ children, active = 1 }) => {
   const [activeMenu, setActiveMenu] = useState(active);
+  const location = useLocation();
 
   return (
-    <div id="siteWrapper">
+    <>
       {sidebar != null && (
         <aside id="mainNavigation">
-          <ul className="sidebar">
+          <ul className="sidebar sidebar-desktop">
             {sidebar?.map((s, sx) => (
               <li
                 className={classNames("item-sidebar", {
-                  active: activeMenu === s?.id,
+                  active: s?.url === location.pathname,
                 })}
                 key={sx}
               >
@@ -46,42 +77,35 @@ const LeftSidebar = ({ children, active = 1, sidebar }) => {
               </li>
             ))}
           </ul>
+          <ul className="ant-row ant-row-center sidebar-mobile">
+            {sidebar?.map((s, sx) => (
+              <li
+                className={classNames("ant-col ant-col-8 item-sidebar", {
+                  active: activeMenu === s?.id,
+                })}
+                key={sx}
+              >
+                {s?.url ? (
+                  <Link
+                    to={s.url}
+                    className="item-menu"
+                    onClick={() => setActiveMenu(s?.id)}
+                  >
+                    {s?.icon}
+                    <p>{s?.title}</p>
+                  </Link>
+                ) : (
+                  <div className="ant-col ant-col-8 item-menu disabled">
+                    {s?.icon}
+                    <p>{s?.title}</p>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
         </aside>
       )}
-      <div id="appWrapper">
-        <div id="appInnerWrapper">
-          <div id="bodyContent">
-            <ul className="ant-row ant-row-center sidebar-mobile">
-              {sidebar?.map((s, sx) => (
-                <li
-                  className={classNames("ant-col ant-col-8 item-sidebar", {
-                    active: activeMenu === s?.id,
-                  })}
-                  key={sx}
-                >
-                  {s?.url ? (
-                    <Link
-                      to={s.url}
-                      className="item-menu"
-                      onClick={() => setActiveMenu(s?.id)}
-                    >
-                      {s?.icon}
-                      <p>{s?.title}</p>
-                    </Link>
-                  ) : (
-                    <div className="ant-col ant-col-8 item-menu disabled">
-                      {s?.icon}
-                      <p>{s?.title}</p>
-                    </div>
-                  )}
-                </li>
-              ))}
-            </ul>
-            {children}
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
