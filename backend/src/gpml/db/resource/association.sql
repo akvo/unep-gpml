79d7e4954da6c5e6e1038ddb2f9d7455ec45e521
Lucas Sousa <46933642+lucassousaf@users.noreply.github.com>
2022-07-27 11:15:56 +0200

    [#1368] Implement focal-points auth role validation (#1380)
    * [#1368] Implement focal-points auth role validation
    
    * Refactored gpml.auth namespace to better handle errors (exceptions).
    * Added the data model in domain for topic_stakeholder_auth.
    * Added the new focal-point role logic to edit organisations auth
    middleware. So we also check for that now.
    * Handle maximum number of focal points per organisation.
    * Added logic to check for organisation's resources that the focal points can edit.
    * Closes #1368

---
+++ backend/src/gpml/db/resource/association.sql
@@ -0,0 +1,7 @@
+-- :name get-resource-associations :query :many
+-- :doc Get the organisation association details for a specific resource.
+SELECT *
+FROM :i:table
+WHERE :i:resource-col = :filters.resource-id
+--~ (when (get-in params [:filters :entity-id]) " AND :i:entity-col = :filters.entity-id")
+--~ (when (get-in params [:filters :associations]) " AND association = ANY(CAST(ARRAY[:v*:filters.associations] AS :i:resource-assoc-type[])) ")
