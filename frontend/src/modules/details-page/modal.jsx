453748ea1f8c799147cd5d6a403e6d28b5c43784
voromahery <daniel.her@gmail.org>
2022-08-09 16:07:23 +0300

    Remove body scroll when the modal is opened

--- frontend/src/modules/details-page/modal.jsx
+++ frontend/src/modules/details-page/modal.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Modal } from "antd";
 import "./modal.scss";
 import DetailsView from "./view";
+import bodyScrollLock from "./scroll-utils";
 
 const DetailModal = ({
   match,
@@ -12,17 +13,13 @@ const DetailModal = ({
   visible,
   setVisible,
 }) => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    setScrollPosition(window.pageYOffset);
-    window.scrollTo(0, scrollPosition);
-  }, [visible]);
-
   return (
     <Modal
       visible={visible}
-      onCancel={() => setVisible(false)}
+      onCancel={() => {
+        setVisible(false);
+        bodyScrollLock.disable();
+      }}
       className="detail-modal"
       destroyOnClose={true}
       centered={window.innerWidth > 600 ? false : true}
