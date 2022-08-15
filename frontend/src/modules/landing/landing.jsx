3b497ca0aa901ad4fbc50958f4fbee71be1b6573
Martin Christov <martin.christov@gmail.com>
2022-08-15 16:39:54 +0300

    refactor & polish

--- frontend/src/modules/landing/landing.jsx
+++ frontend/src/modules/landing/landing.jsx
@@ -1,44 +1,21 @@
-import React, { useState } from 'react'
-import { Input, Button, Menu, Dropdown, Layout } from "antd";
-import logo from "../../images/gpml.svg";
+import React from 'react'
+import { Button } from "antd";
 import './styles.scss'
-import { Link, withRouter } from 'react-router-dom';
-import { ReactComponent as Dots3x3 } from "../../images/3x3.svg";
 import { ReactComponent as Down } from "../../images/down.svg";
 import { ReactComponent as PlasticLitter } from "../../images/plastic-litter.svg";
-import { ArrowDownOutlined, DownOutlined, SearchOutlined } from '@ant-design/icons';
+import MenuBar from './menu-bar';
 
-const Landing = ({ isAuthenticated, updateQuery, profile, isRegistered, logout}) => {
+const Landing = (props) => {
   return (
     <div id="landing">
-      <Layout.Header className="nav-header-container">
-        <div className="ui container">
-          <Link to="/">
-            <img src={logo} className="logo" alt="GPML" />
-          </Link>
-          {/* <div className="all-tools-btn"></div> */}
-          <div className="all-tools-btn">
-            <Dots3x3 />
-            <span>All Tools</span>
-          </div>
-          <div className="rightside">
-            <Search updateQuery={updateQuery} />
-            {!isAuthenticated ? (
-            <Button type="ghost">Login</Button>
-            ) : [
-            <AddButton />, 
-            <UserButton {...{ logout, isRegistered, profile }} />]
-          }
-          </div>
-        </div>
-      </Layout.Header>
+      <MenuBar {...props} />
       <div className="hero">
         <div className="litter">
           <PlasticLitter />
         </div>
         <div className="content">
           <h1>The Digital Platform on Plastic Pollution & Marine Litter</h1>
-          <h4>Informing and connecting all actors working to prevent marine litter and plastic pollution.</h4>
+          <h4>Informing and connecting all actors working to prevent plastic pollution and marine litter across the lifecycle and from source to sea.</h4>
           <Button type="primary" size='large'>Join the Partnership</Button>
         </div>
         <div className="next-btn">
@@ -48,32 +25,5 @@ const Landing = ({ isAuthenticated, updateQuery, profile, isRegistered, logout})
     </div>
   )
 }
-
-const Search = withRouter(({ history, updateQuery }) => {
-  const [search, setSearch] = useState('');
-
-  const handleSearch = (src) => {
-    const path = history.location.pathname;
-    if (src) {
-      history.push(`/knowledge/library?q=${src.trim()}`);
-      updateQuery("q", src.trim());
-    } else {
-      updateQuery("q", src.trim());
-    }
-  };
-
-  return (
-    <div className="src">
-      <Input
-        value={search}
-        className="input-src"
-        placeholder="Search"
-        suffix={<SearchOutlined />}
-        onPressEnter={(e) => handleSearch(e.target.value)}
-        onSubmit={(e) => setSearch(e.target.value)}
-      />
-    </div>
-  );
-});
 
 export default Landing
---
+++ frontend/src/modules/landing/menu-bar.jsx
@@ -0,0 +1,75 @@
+import { Link, withRouter } from 'react-router-dom';
+import { Input, Button, Layout } from "antd";
+import classNames from 'classnames'
+import { ReactComponent as Dots3x3 } from "../../images/3x3.svg";
+
+import logo from "../../images/gpml.svg";
+import { useEffect, useRef, useState } from 'react';
+import { SearchOutlined } from '@ant-design/icons';
+
+const MenuBar = ({ updateQuery, isAuthenticated, logout, isRegistered, profile }) => {
+  const domRef = useRef()
+  useEffect(() => {
+    document.addEventListener('scroll', (e) => {
+      console.log()
+      if(window.scrollY > 100 && domRef.current.classList.contains('scrolled') === false){
+        domRef.current.classList.add('scrolled')
+      } else if(window.scrollY < 100 && domRef.current.classList.contains('scrolled')){
+        domRef.current.classList.remove('scrolled')
+      }
+    })
+  }, [])
+  return (
+    <Layout.Header className="nav-header-container" ref={domRef}>
+      <div className="ui container">
+        <Link to="/">
+          <img src={logo} className="logo" alt="GPML" />
+        </Link>
+        {/* <div className="all-tools-btn"></div> */}
+        <div className="all-tools-btn">
+          <Dots3x3 />
+          <span>All Tools</span>
+        </div>
+        <div className="rightside">
+          <Search updateQuery={updateQuery} />
+          {!isAuthenticated ? (
+          <Button type="ghost">Login</Button>
+          ) : [
+          <AddButton />, 
+          <UserButton {...{ logout, isRegistered, profile }} />]
+        }
+        </div>
+      </div>
+    </Layout.Header>
+  )
+}
+
+
+const Search = withRouter(({ history, updateQuery }) => {
+  const [search, setSearch] = useState('');
+
+  const handleSearch = (src) => {
+    const path = history.location.pathname;
+    if (src) {
+      history.push(`/knowledge/library?q=${src.trim()}`);
+      updateQuery("q", src.trim());
+    } else {
+      updateQuery("q", src.trim());
+    }
+  };
+
+  return (
+    <div className="src">
+      <Input
+        value={search}
+        className="input-src"
+        placeholder="Search"
+        suffix={<SearchOutlined />}
+        onPressEnter={(e) => handleSearch(e.target.value)}
+        onSubmit={(e) => setSearch(e.target.value)}
+      />
+    </div>
+  );
+});
+
+export default MenuBar
