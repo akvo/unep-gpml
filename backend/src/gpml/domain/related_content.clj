9e2bc7dc7ca926dec93f4b8b4a162d52c9bf6605
Lucas Sousa <46933642+lucassousaf@users.noreply.github.com>
2022-08-30 14:35:03 +0200

    [#1451] Implement new policy domain model changes (#1453)
    * [#1451] Implement new policy domain model changes
    
    Main details:
    * Updated policy domain model to align with LEAP API model.
    * Updated related code
      * Related content creation refactor to add logging and a more
      generic way of creating the DB entries.
      * Updated LEAP API importer to use domain model definition for
      it's constants (types_of_laws, sub_content_types and statuses)
    * Drop deprecated columns from the database model (check migration
    file for details).
    
    Secondary details:
    * Updated reitit dependencies. It fixes CVE-2020-36518.
    
    * See #1451 
    * Closes #1451

---
+++ backend/src/gpml/domain/related_content.clj
@@ -0,0 +1,33 @@
+(ns gpml.domain.related-content)
+
+(def ^:const resource-types
+  "Allowed resource types for related content. Note that `resource`
+  entity is treated differently and it's subtypes are used instead of
+  the table name to define domain level resource types. This is not
+  true at persistence level where we MUST convert the names to the
+  correct table names."
+  #{"policy"
+    "event"
+    "initiative"
+    "technology"
+    "financing_resource"
+    "technical_resource"
+    "action_plan"})
+
+(def ^:const relation-types
+  "Possible resource relation types.
+
+  NOTE: Currently the types refer to a relationship between two
+  policies."
+  #{"implements"
+    "amends"
+    "repeals"})
+
+(def RelatedContent
+  "The Related Content entity."
+  [:map
+   [:resource_id pos-int?]
+   [:resource_table_name (apply conj [:enum] resource-types)]
+   [:related_resource_id pos-int?]
+   [:related_resource_table_name (apply conj [:enum] resource-types)]
+   [:related_content_relation_type]])
