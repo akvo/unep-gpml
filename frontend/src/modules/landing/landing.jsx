9b7a8aca72d728fbd2fdf9865970f8d74ad35160
Martin Christov <martin.christov@gmail.com>
2022-08-31 09:46:11 +0300

    journey mobile

--- frontend/src/modules/landing/landing.jsx
+++ frontend/src/modules/landing/landing.jsx
@@ -88,65 +88,76 @@ const TheJourney = () => {
   }, [])
   return (
     <div className="journey" ref={topRef}>
-        <img ref={svgRef} src="/plastic-journey.svg" />
-        <div className="contents">
-          <div className="screen-view">
-            <h1>THE PLASTICS JOURNEY</h1>
+      <img ref={svgRef} src="/plastic-journey.svg" />
+      <div className="contents">
+        <div className="screen-view">
+          <h1>THE PLASTICS JOURNEY</h1>
+          <div className="mobile-only img">
+            <img src="/plastic-journey.svg" />
           </div>
-          <div className="screen-view">
-            <div className="pane">
-              <h3>FROM SOURCE TO SEA ACCROSS THE PLASTICS life cycle</h3>
-              <p>Plastics can have environmental, economic, health and social impacts and are leaked into the environment across the plastics life cycle and from source to sea. Once the plastics are in the environment, they can be transported through various pathways.</p>
-              <Button type="ghost" size="large">Learn More</Button>
-            </div>
+        </div>
+        <div className="screen-view">
+          <div className="pane">
+            <h3>FROM SOURCE TO SEA ACCROSS THE PLASTICS life cycle</h3>
+            <p>Plastics can have environmental, economic, health and social impacts and are leaked into the environment across the plastics life cycle and from source to sea. Once the plastics are in the environment, they can be transported through various pathways.</p>
+            <Button type="ghost" size="large">Learn More</Button>
           </div>
-          <div className="screen-view">
-            <div className="pane">
-              <h3>PLASTIC POLLUTION PATHWAYS INCLUDE</h3>
-              <h4>Airborne</h4>
-              <p>Plastic particles such as vehicle tyres and synthetic textiles accumulate in the atmosphere, where they can be transported over long distances. Rain, snow and wind can deposit plastics in different places. Microplastics are found in snow, ice and sea-ice, from the poles to remote mountain tops.
-              </p>
-              <h4>rivers</h4>
-              <p>Waste can be transported by rivers through the environment including into lakes and oceans.</p>
-            </div>
+          <div className="mobile-only img">
+            <img src="/plastic-journey.svg" />
           </div>
-          <div className="screen-view">
-            <div className="pane">
-              <h3>PLASTIC POLLUTION SOURCES INCLUDE</h3>
-              <h4>Agriculture</h4>
-              <p>Use of plastic films, irrigation tubes and fibre textiles in farming practices. Sewage sludge with plastic residue used as fertilizer. Irrigation with plastic contaminated water. The use of polymer coatings on fertilizers, pesticides, and seeds.</p>
-              <h4>Cities and Waste Management</h4>
-              <p>Use of consumer goods and products such as clothes, building materials and take-away food containers and lack of or inadequate waste management of materials and products.</p>
-              <h4>Industry</h4>
-              <p>Use and leakage of products, particles, and fibres during industrial activities as well as transportation of consumer and industrial goods</p>
-            </div>
+        </div>
+        <div className="screen-view">
+          <div className="pane">
+            <h3>PLASTIC POLLUTION PATHWAYS INCLUDE</h3>
+            <h4>Airborne</h4>
+            <p>Plastic particles such as vehicle tyres and synthetic textiles accumulate in the atmosphere, where they can be transported over long distances. Rain, snow and wind can deposit plastics in different places. Microplastics are found in snow, ice and sea-ice, from the poles to remote mountain tops.
+            </p>
+            <h4>rivers</h4>
+            <p>Waste can be transported by rivers through the environment including into lakes and oceans.</p>
           </div>
-          <div className="screen-view">
-            <div className="pane">
-              <h3>PLASTIC POLLUTION SOURCES, PATHWAYS AND SINKS INCLUDE</h3>
-              <h4>Wastewater including sewage – source and pathway</h4>
-              <p>Capture of pollutants including microplastics by wastewater treatment systems prevents releases to the environment via outfalls; however, many are still lost and leaked into the environment.</p>
-              <h4>Lakes and reservoirs – source and sink</h4>
-              <p>A source under specific weather patterns and hydrodynamic regimes such as winds and water currents as well as due to anthropogenic activities such as tourism and industrial lake activities including aquaculture. Lakes can also be sinks meaning areas of temporary and long-term storage of pollution, and possible infiltration into groundwater.</p>
-               {/* <h4>Reservoirs – sources and sinks</h4>
-               <p>Plastics from atmospheric fallout, streams,
-and rivers. Temporary and long-term storage,
-and possible infiltration into groundwater.</p> */}
-            </div>
+          <div className="mobile-only img">
+            <img src="/plastic-journey.svg" />
           </div>
-          <div className="screen-view">
-            <div className="pane">
-              <h3>FURTHER PLASTIC POLLUTION SOURCES INCLUDE</h3>
-              <h4>Tourism</h4>
-              <p>Intentional or accidental littering in the environment due to tourism such as terrestrial, including mountain, coastal and sea-based tourism.</p>
-              <h4>Fishing Activities and Aquaculture</h4>
-              <p>Waste thrown overboard, abandoned, lost, or otherwise discarded fishing gear and aquaculture equipment, and marine coatings.</p>
-              <h4>Shipping and Recreational activities</h4>
-              <p>Waste thrown overboard, loss of shipped goods such as industrial and consumer products and materials, and marine coatings.</p>
-            </div>
+        </div>
+        <div className="screen-view">
+          <div className="pane">
+            <h3>PLASTIC POLLUTION SOURCES INCLUDE</h3>
+            <h4>Agriculture</h4>
+            <p>Use of plastic films, irrigation tubes and fibre textiles in farming practices. Sewage sludge with plastic residue used as fertilizer. Irrigation with plastic contaminated water. The use of polymer coatings on fertilizers, pesticides, and seeds.</p>
+            <h4>Cities and Waste Management</h4>
+            <p>Use of consumer goods and products such as clothes, building materials and take-away food containers and lack of or inadequate waste management of materials and products.</p>
+            <h4>Industry</h4>
+            <p>Use and leakage of products, particles, and fibres during industrial activities as well as transportation of consumer and industrial goods</p>
+          </div>
+          <div className="mobile-only img">
+            <img src="/plastic-journey.svg" />
+          </div>
+        </div>
+        <div className="screen-view">
+          <div className="pane">
+            <h3>PLASTIC POLLUTION SOURCES, PATHWAYS AND SINKS INCLUDE</h3>
+            <h4>Wastewater including sewage – source and pathway</h4>
+            <p>Capture of pollutants including microplastics by wastewater treatment systems prevents releases to the environment via outfalls; however, many are still lost and leaked into the environment.</p>
+            <h4>Lakes and reservoirs – source and sink</h4>
+            <p>A source under specific weather patterns and hydrodynamic regimes such as winds and water currents as well as due to anthropogenic activities such as tourism and industrial lake activities including aquaculture. Lakes can also be sinks meaning areas of temporary and long-term storage of pollution, and possible infiltration into groundwater.</p>
+          </div>
+          <div className="mobile-only img">
+            <img src="/plastic-journey.svg" />
+          </div>
+        </div>
+        <div className="screen-view">
+          <div className="pane">
+            <h3>FURTHER PLASTIC POLLUTION SOURCES INCLUDE</h3>
+            <h4>Tourism</h4>
+            <p>Intentional or accidental littering in the environment due to tourism such as terrestrial, including mountain, coastal and sea-based tourism.</p>
+            <h4>Fishing Activities and Aquaculture</h4>
+            <p>Waste thrown overboard, abandoned, lost, or otherwise discarded fishing gear and aquaculture equipment, and marine coatings.</p>
+            <h4>Shipping and Recreational activities</h4>
+            <p>Waste thrown overboard, loss of shipped goods such as industrial and consumer products and materials, and marine coatings.</p>
           </div>
         </div>
       </div>
+    </div>
   )
 }
 
