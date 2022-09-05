2708b25ef859dd141998ee37205c952ad16ef318
lucassousaf <lucas.sousa@magnet.coop>
2022-09-05 13:03:41 +0200

    HotFix migration 174

--- backend/resources/migrations/174-alter-table-project-add-stage-column.up.sql
+++ backend/resources/migrations/174-alter-table-project-add-stage-column.up.sql
@@ -3,6 +3,11 @@ BEGIN;
 CREATE TYPE PROJECT_STAGE AS ENUM('create','implement','report','update');
 --;;
 ALTER TABLE project
-ADD COLUMN stage PROJECT_STAGE NOT NULL;
+ADD COLUMN stage PROJECT_STAGE;
+--;;
+UPDATE project SET stage = 'create';
+--;;
+ALTER TABLE project
+ALTER COLUMN stage SET NOT NULL;
 --;;
 COMMIT;
