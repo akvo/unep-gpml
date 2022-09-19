ece6c0164c73f857f76c73da4b8d6846abda6c3e
Lucas Sousa <lucas.sousa@magnet.coop>
2022-09-19 11:53:23 +0200

    [#1447] Remove unused ediscript dependency

--- backend/project.clj
+++ backend/project.clj
@@ -48,8 +48,7 @@
                  [ns-tracker "0.4.0"]
                  [twarc "0.1.15"]
                  [diehard "0.10.3"]
-                 [org.jsoup/jsoup "1.15.3"]
-                 [juji/editscript "0.6.2"]]
+                 [org.jsoup/jsoup "1.15.3"]]
   :plugins [[duct/lein-duct "0.12.1"]]
   :main ^:skip-aot gpml.main
   :resource-paths ["resources" "target/resources"]
