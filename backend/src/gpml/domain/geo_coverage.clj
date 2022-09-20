dec6997a9d7fda147016ed88073724d41eaeecb5
Lucas Sousa <lucas.sousa@magnet.coop>
2022-09-20 11:15:39 +0200

    [#1447] Fix GeoCoverage schema

--- backend/src/gpml/domain/geo_coverage.clj
+++ backend/src/gpml/domain/geo_coverage.clj
@@ -12,7 +12,7 @@
      [:event {:optional true} pos-int?]
      [:policy {:optional true} pos-int?]
      [:technology {:optional true} pos-int?]
-     [:initiative {:optional true} post-int?]
+     [:initiative {:optional true} pos-int?]
      [:organisation {:optional true} pos-int?]
      [:country {:optional true} pos-int?]
      [:country_group {:optional true} pos-int?]]
