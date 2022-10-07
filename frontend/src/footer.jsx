437d0e7e54bfb05b76378e761290beb0a49b8b57
Navin <nikshingote@gmail.com>
2022-10-07 14:20:48 +0530

    remove nav, timer

--- frontend/src/footer.jsx
+++ frontend/src/footer.jsx
@@ -42,157 +42,136 @@ const Footer = ({ setShowMenu }) => {
         setLoading(false);
         console.log(err?.response);
         notification.error({
-          message: err?.response?.data['errorDetails']
-            ? err?.response?.data['errorDetails']?.email[0]
+          message: err?.response?.data["errorDetails"]
+            ? err?.response?.data["errorDetails"]?.email[0]
             : "Oops, something went wrong",
         });
       });
   };
 
-  useEffect(() => {
-    setTimeout(function () {
-      setShowMessage(false);
-      setTimeoutState(0);
-    }, 3000);
-  }, [showMessage]);
-
   return (
     <footer>
       <div className="ui container">
         <div className="col">
-          <nav>
-            <ul>
-              <li>
-                <h4>About Us</h4>
-              </li>
-              <li>
-                <a
-                  href="https://www.gpmarinelitter.org/who-we-are"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Who we are
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.gpmarinelitter.org/what-we-do"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  What we do
-                </a>
-              </li>
-              <li>
-                <Link to="/about-us">About the GPML Digital platform</Link>
-              </li>
-            </ul>
-          </nav>
+          <ul>
+            <li>
+              <h4>About Us</h4>
+            </li>
+            <li>
+              <a
+                href="https://www.gpmarinelitter.org/who-we-are"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Who we are
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.gpmarinelitter.org/what-we-do"
+                target="_blank"
+                rel="noreferrer"
+              >
+                What we do
+              </a>
+            </li>
+            <li>
+              <Link to="/about-us">About the GPML Digital platform</Link>
+            </li>
+          </ul>
         </div>
         <div className="col">
-          <nav>
-            <ul>
-              <li>
-                <h4>GPML Tools</h4>
-              </li>
-              <li>
-                <Link
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setShowMenu(true);
-                  }}
-                  to="/"
-                >
-                  Show all tools
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <ul>
+            <li>
+              <h4>GPML Tools</h4>
+            </li>
+            <li>
+              <Link
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShowMenu(true);
+                }}
+                to="/"
+              >
+                Show all tools
+              </Link>
+            </li>
+          </ul>
         </div>
         <div className="col">
-          <nav>
-            <ul>
-              <li>
-                <h4>Contact Us</h4>
-              </li>
-              <li>
-                <a href="mailto:unep-gpmarinelitter@un.org">
-                  unep-gpmarinelitter@un.org
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <ul>
+            <li>
+              <h4>Contact Us</h4>
+            </li>
+            <li>
+              <a href="mailto:unep-gpmarinelitter@un.org">
+                unep-gpmarinelitter@un.org
+              </a>
+            </li>
+          </ul>
         </div>
         <div className="col">
-          <nav>
-            <ul>
-              <li>
-                <h4>Follow Us</h4>
-              </li>
-            </ul>
-            <ul className="horizonList socialLink">
-              <li>
-                <a
-                  href="https://www.linkedin.com/company/global-partnership-on-marine-litter/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <LinkedinOutlined />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.youtube.com/channel/UCoWXFwDeoD4c9GoXzFdm9Bg"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <YoutubeOutlined />
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <ul>
+            <li>
+              <h4>Follow Us</h4>
+            </li>
+          </ul>
+          <ul className="horizonList socialLink">
+            <li>
+              <a
+                href="https://www.linkedin.com/company/global-partnership-on-marine-litter/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <LinkedinOutlined />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCoWXFwDeoD4c9GoXzFdm9Bg"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <YoutubeOutlined />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="ui container">
         <div className="subscribe-section">
           <div className="col">
-            <nav>
-              <ul>
-                <li>
-                  <h5>Powered by</h5>
-                </li>
-                <img src={logo} className="logo" alt="GPML" />
-              </ul>
-            </nav>
+            <ul>
+              <li>
+                <h5>Powered by</h5>
+              </li>
+              <img src={logo} className="logo" alt="GPML" />
+            </ul>
           </div>
           <div className="col">
             {!showMessage && (
               <div>
-                <nav>
-                  <ul>
-                    <li>
-                      <h4>Stay tuned with the GPML latest news and events!</h4>
-                    </li>
-                  </ul>
-                </nav>
-                <nav>
-                  <Input.Group compact>
-                    <Input
-                      placeholder="Email"
-                      value={email}
-                      onChange={handleChange}
-                      className={`${error ? "ant-input-status-error" : ""}`}
-                      onPressEnter={() => subscribe()}
-                    />
-                    <Button
-                      onClick={() => subscribe()}
-                      disabled={error || email.length === 0}
-                      loading={loading}
-                    >
-                      Subscribe
-                    </Button>
-                  </Input.Group>
-                </nav>
+                <ul>
+                  <li>
+                    <h4>Stay tuned with the GPML latest news and events!</h4>
+                  </li>
+                </ul>
+                <Input.Group compact>
+                  <Input
+                    placeholder="Email"
+                    value={email}
+                    onChange={handleChange}
+                    className={`${error ? "ant-input-status-error" : ""}`}
+                    onPressEnter={() => subscribe()}
+                  />
+                  <Button
+                    onClick={() => subscribe()}
+                    disabled={error || email.length === 0}
+                    loading={loading}
+                  >
+                    Subscribe
+                  </Button>
+                </Input.Group>
               </div>
             )}
             <CSSTransition
@@ -211,7 +190,7 @@ const Footer = ({ setShowMenu }) => {
       </div>
       <div className="second-footer">
         <div className="ui container unep">
-          <nav>
+          <div>
             <img src={unepLogo} alt="unep" />
             <ul className="horizonList">
               <li className="copyright">
@@ -226,7 +205,7 @@ const Footer = ({ setShowMenu }) => {
                 </a>
               </li>
             </ul>
-          </nav>
+          </div>
           <div className="unepInfo">
             <h2>
               The Digital Platform is a UNEP contribution to the Global
