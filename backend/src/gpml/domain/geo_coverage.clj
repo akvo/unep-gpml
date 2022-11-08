8218d23e0bf551865c783aa7bdde76c6531912a6
Lucas Sousa <lucas.sousa@magnet.coop>
2022-11-08 14:07:20 +0100

    [#1506] Refactor geo coverage domain and add geo coverage processing
    * Processing means taking the values for country groups, countries and
    country states and combining them together as a single
    entity. Also, respecting some geo coverage restrictions (read
    ->geo-coverage docstring for details).

--- backend/src/gpml/domain/geo_coverage.clj
+++ backend/src/gpml/domain/geo_coverage.clj
@@ -1,6 +1,8 @@
 (ns gpml.domain.geo-coverage
   (:require [gpml.util :as util]
-            [malli.core :as m]))
+            [gpml.util.malli :as util.malli]
+            [malli.core :as m]
+            [malli.util :as mu]))
 
 (def CountryState
   (m/schema
@@ -11,7 +13,7 @@
     [:type [:maybe string?]]
     [:country_id pos-int?]]))
 
-(def GeoCoverageCountryCountryGroups
+(def GeoCoverage
   "Geo Coverage schema for countries and country groups. This is a sub
   entity relation stored in the `<entity-name>_geo_coverage` tables."
   (m/schema
@@ -24,32 +26,48 @@
      [:initiative {:optional true} pos-int?]
      [:organisation {:optional true} pos-int?]
      [:country {:optional true} pos-int?]
-     [:country_group {:optional true} pos-int?]]
-    [:fn (fn [{:keys [resource event policy technology organisation country country_group]}]
-           (and (util/xor? country country_group)
-                (util/xor? resource event policy technology organisation)))]]))
+     [:country_group {:optional true} pos-int?]
+     [:country_state {:optional true} pos-int?]]
+    [:fn (fn [{:keys [resource event policy technology organisation]}]
+           (util/xor? resource event policy technology organisation))]]))
 
-(def GeoCoverageCountryStates
-  "Geo Coverage schema for country states. This is a sub entity relation
-  stored in the `<entity-name>_country_state` tables."
-  (m/schema
-   [:and
-    [:map
-     [:resource_id {:optional true} pos-int?]
-     [:event_id {:optional true} pos-int?]
-     [:policy_id {:optional true} pos-int?]
-     [:technology_id {:optional true} pos-int?]
-     [:initiative_id {:optional true} pos-int?]
-     [:organisation_id {:optional true} pos-int?]
-     [:country_state_id {:optional true} pos-int?]]
-    [:fn (fn [{:keys [resource event policy technology organisation initiative]}]
-           (util/xor? resource event policy technology initiative organisation))]]))
+(defn- initial-geo-coverage
+  [entity-key]
+  (let [schema-keys (->> (mu/get GeoCoverage 0)
+                         util.malli/keys
+                         (remove (comp not #{entity-key :country :country_group :country_state})))]
+    (zipmap schema-keys (repeat nil))))
 
-(defn geo-countries-country-groups
-  [entity-id entity-key countries country-groups]
-  (concat (map #(assoc {} entity-key entity-id :country %) countries)
-          (map #(assoc {} entity-key entity-id :country_group %) country-groups)))
+(defn- build-geo-coverage-coll
+  [geo-coverage-coll entity-key entity-id geo-key geo-ids]
+  (map #(assoc geo-coverage-coll
+               entity-key entity-id
+               geo-key %)
+       geo-ids))
 
-(defn geo-country-states
-  [entity-id entity-key country-states]
-  (map #(assoc {} entity-key entity-id :country_state_id %) country-states))
+(defn ->geo-coverage
+  "Given the collections of `countries`, `country-groups` and
+  `country-states` IDs, creates the canonical representation for the
+  `<entity-name>_geo_coverage` tables. There are some restrictions
+  depending on the `geo-coverage-type`:
+
+  1 - If the `geo-coverage-type` is `:sub-national` `countries` and
+  `country-states` must be pairs.
+
+  2 - If the `geo-coverage-type` is `:national` `countries` are solo.
+
+  3 - If the `geo-coverage-type` is `:transnational` `countries` and
+  `country-groups` are not pairs and must be different records."
+  [entity-id entity-key geo-coverage-type countries country-groups country-states]
+  (let [geo-coverage (initial-geo-coverage entity-key)]
+    (cond
+      (= :sub-national geo-coverage-type)
+      (apply merge
+             (build-geo-coverage-coll geo-coverage entity-key entity-id :country countries)
+             (build-geo-coverage-coll geo-coverage entity-key entity-id :country_state country-states))
+      (= :national geo-coverage-type)
+      (build-geo-coverage-coll geo-coverage entity-key entity-id :country countries)
+
+      (= :transnational geo-coverage-type)
+      (concat (build-geo-coverage-coll geo-coverage entity-key entity-id :country countries)
+              (build-geo-coverage-coll geo-coverage entity-key entity-id :country_group country-groups)))))
