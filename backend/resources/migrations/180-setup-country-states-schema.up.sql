f47523ee54262a6ec7762d416b50487d172dae47
Lucas Sousa <lucas.sousa@magnet.coop>
2022-11-08 09:27:54 +0100

    [#1506] Merge country-state geo with geo_coverage tables

--- backend/resources/migrations/180-setup-country-states-schema.up.sql
+++ backend/resources/migrations/180-setup-country-states-schema.up.sql
@@ -8,44 +8,22 @@ CREATE TABLE country_state (
   country_id INTEGER NOT NULL REFERENCES country(id)
 );
 --;;
-CREATE TABLE initiative_country_state (
-  initiative_id INTEGER NOT NULL REFERENCES initiative(id) ON DELETE CASCADE,
-  country_state_id INTEGER NOT NULL REFERENCES country_state(id) ON DELETE CASCADE
-);
+ALTER TABLE initiative_geo_coverage
+ADD COLUMN country_state INTEGER REFERENCES country_state(id) ON DELETE CASCADE;
 --;;
-CREATE TABLE policy_country_state (
- policy_id INTEGER NOT NULL REFERENCES policy(id) ON DELETE CASCADE,
- country_state_id INTEGER NOT NULL REFERENCES country_state(id) ON DELETE CASCADE
-);
+ALTER TABLE policy_geo_coverage
+ADD COLUMN country_state INTEGER REFERENCES country_state(id) ON DELETE CASCADE;
 --;;
-CREATE TABLE event_country_state (
-  event_id INTEGER NOT NULL REFERENCES event(id) ON DELETE CASCADE,
-  country_state_id INTEGER NOT NULL REFERENCES country_state(id) ON DELETE CASCADE
-);
+ALTER TABLE event_geo_coverage
+ADD COLUMN country_state INTEGER REFERENCES country_state(id) ON DELETE CASCADE;
 --;;
-CREATE TABLE technology_country_state (
- technology_id INTEGER NOT NULL REFERENCES technology(id) ON DELETE CASCADE,
- country_state_id INTEGER NOT NULL REFERENCES country_state(id) ON DELETE CASCADE
-);
+ALTER TABLE resource_geo_coverage
+ADD COLUMN country_state INTEGER REFERENCES country_state(id) ON DELETE CASCADE;
 --;;
-CREATE TABLE resource_country_state (
- resource_id INTEGER NOT NULL REFERENCES resource(id) ON DELETE CASCADE,
- country_state_id INTEGER NOT NULL REFERENCES country_state(id) ON DELETE CASCADE
-);
+ALTER TABLE technology_geo_coverage
+ADD COLUMN country_state INTEGER REFERENCES country_state(id) ON DELETE CASCADE;
 --;;
-ALTER TABLE initiative_country_state
-ADD CONSTRAINT initiative_country_state_pkey PRIMARY KEY (initiative_id, country_state_id);
---;;
-ALTER TABLE policy_country_state
-ADD CONSTRAINT policy_country_state_pkey PRIMARY KEY (policy_id, country_state_id);
---;;
-ALTER TABLE event_country_state
-ADD CONSTRAINT event_country_state_pkey PRIMARY KEY (event_id, country_state_id);
---;;
-ALTER TABLE technology_country_state
-ADD CONSTRAINT technology_country_state_pkey PRIMARY KEY (technology_id, country_state_id);
---;;
-ALTER TABLE resource_country_state
-ADD CONSTRAINT resource_country_state_pkey PRIMARY KEY (resource_id, country_state_id);
+ALTER TABLE organisation_geo_coverage
+ADD COLUMN country_state INTEGER REFERENCES country_state(id) ON DELETE CASCADE;
 --;;
 COMMIT;
