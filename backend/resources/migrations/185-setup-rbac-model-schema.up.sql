4916a088408720c2191a53dc2b2cb234a0e92b0e
joseAyudarte91 <jose.ayudarte@gmail.com>
2022-12-01 12:25:24 +0100

    Add rbac model schema for new permissions system
    [Re #1530]
    
    Added rbac model schema, modified to match the specific needs of this
    application, since resource ids and stakeholder (user) ids need to match the
    format of our app DB schema.
    
    We skip down.up.sql-related file since we are not using that strategy at all.

---
+++ backend/resources/migrations/185-setup-rbac-model-schema.up.sql
@@ -0,0 +1,42 @@
+BEGIN;
+--;;
+CREATE TABLE IF NOT EXISTS rbac_context_type (
+    name VARCHAR(127) PRIMARY KEY,
+    description TEXT);
+--;;
+CREATE TABLE IF NOT EXISTS rbac_context (
+    id uuid PRIMARY KEY,
+    context_type_name VARCHAR(127) NOT NULL REFERENCES rbac_context_type(name) ON UPDATE CASCADE,
+    resource_id INTEGER NOT NULL,
+    parent uuid REFERENCES rbac_context(id) ON UPDATE CASCADE,
+    UNIQUE(context_type_name, resource_id));
+--;;
+CREATE TABLE IF NOT EXISTS rbac_role (
+    id uuid PRIMARY KEY,
+    name VARCHAR(127) NOT NULL,
+    description TEXT,
+    UNIQUE (name));
+--;;
+CREATE TABLE IF NOT EXISTS rbac_role_assignment (
+    role_id uuid NOT NULL REFERENCES rbac_role(id) ON UPDATE CASCADE,
+    context_id uuid NOT NULL REFERENCES rbac_context(id) ON UPDATE CASCADE,
+    user_id INTEGER NOT NULL,
+    PRIMARY KEY (role_id, context_id, user_id));
+--;;
+CREATE TABLE IF NOT EXISTS rbac_super_admin (
+    user_id INTEGER PRIMARY KEY);
+--;;
+CREATE TABLE IF NOT EXISTS rbac_permission (
+    id uuid PRIMARY KEY,
+    name VARCHAR(127) NOT NULL,
+    description TEXT,
+    context_type_name VARCHAR(127) NOT NULL REFERENCES rbac_context_type(name) ON UPDATE CASCADE,
+    UNIQUE (name));
+--;;
+CREATE TABLE IF NOT EXISTS rbac_role_permission (
+    role_id uuid REFERENCES rbac_role(id) ON UPDATE CASCADE,
+    permission_id uuid NOT NULL REFERENCES rbac_permission(id) ON UPDATE CASCADE,
+    permission_value SMALLINT NOT NULL,
+    PRIMARY KEY (role_id, permission_id));
+--;;
+COMMIT;
