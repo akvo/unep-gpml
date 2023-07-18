96e34a0b7daa4b1299cc9912501d334338fa7db6
navins94 <nikshingote@gmail.com>
2023-07-18 12:50:24 +0530

    Add meta tags

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Head from "next/head";
+import "../main.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="app">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="GPML Digital Platform" />
+        <title>UNEP GPML Digital Platform</title>
+      </Head>
       <Component {...pageProps} />
     </div>
   );
