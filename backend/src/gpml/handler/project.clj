ee402a560abba3cc1aacdd34d9b7723c65de520f
joseAyudarte91 <jose.ayudarte@gmail.com>
2023-07-18 18:21:33 +0200

    Instrument Project API
    [Re #1530]
    
    We have added the needed permissions check for each operation of the
    Project API endpoint, removing the not needed middlewares.
    
    On the other hand, we have removed the `stakeholder-id` property when
    receiving the data to be updated from a Project entity, since that does
    not make sense, as the owner of the Project is the only one able to
    edit a project and it does not make sense to transfer the ownership of
    it.
    
    Renamed db transaction binding to better identify the variable and avoid
    mistakes.

--- backend/src/gpml/handler/project.clj
+++ backend/src/gpml/handler/project.clj
@@ -6,8 +6,11 @@
             [gpml.domain.project :as dom.prj]
             [gpml.domain.types :as dom.types]
             [gpml.handler.resource.geo-coverage :as handler.geo-coverage]
+            [gpml.handler.resource.permission :as h.r.permission]
             [gpml.handler.responses :as r]
             [gpml.handler.util :as handler.util]
+            [gpml.service.permissions :as srv.permissions]
+            [gpml.util.malli :as util.malli]
             [gpml.util.postgresql :as pg-util]
             [integrant.core :as ig]
             [malli.util :as mu])
@@ -69,12 +72,12 @@
 (defn- create-project
   [{:keys [db logger]} {:keys [parameters user] :as _req}]
   (try
-    (jdbc/with-db-transaction [conn (:spec db)]
+    (jdbc/with-db-transaction [tx (:spec db)]
       (let [body (:body parameters)
             db-project (-> body
                            (assoc :stakeholder_id (:id user))
                            (db.prj/project->db-project))
-            inserted-values (db.prj/create-projects conn
+            inserted-values (db.prj/create-projects tx
                                                     {:insert-cols (map name (keys db-project))
                                                      :insert-values [(vals db-project)]})
             {:keys [geo_coverage_type
@@ -84,16 +87,29 @@
             geo-coverage-type (keyword geo_coverage_type)
             project-id (-> inserted-values first :id)]
         (if (= (count inserted-values) 1)
-          (let [result (handler.geo-coverage/create-resource-geo-coverage
-                        conn
-                        :project
-                        project-id
-                        geo-coverage-type
-                        {:countries geo_coverage_countries
-                         :country-groups geo_coverage_country_groups
-                         :country-states geo_coverage_country_states})]
-            (when-not (:success? result)
-              (throw (ex-info "Failed to create project geo coverage" {:inserted-values inserted-values}))))
+          (do
+            (let [result (handler.geo-coverage/create-resource-geo-coverage
+                          tx
+                          :project
+                          project-id
+                          geo-coverage-type
+                          {:countries geo_coverage_countries
+                           :country-groups geo_coverage_country_groups
+                           :country-states geo_coverage_country_states})]
+              (when-not (:success? result)
+                (throw (ex-info "Failed to create project geo coverage" {:inserted-values inserted-values}))))
+            (srv.permissions/create-resource-context
+             {:conn tx
+              :logger logger}
+             {:context-type :project
+              :resource-id project-id})
+            (srv.permissions/assign-roles-to-users-from-connections
+             {:conn tx
+              :logger logger}
+             {:context-type :project
+              :resource-id project-id
+              :individual-connections [{:role "owner"
+                                        :stakeholder (:id user)}]}))
           (throw (ex-info "Failed to create project" {:inserted-values inserted-values})))
         (r/ok {:success? true :project_id project-id})))
     (catch Exception e
@@ -179,13 +195,21 @@
 (defn- delete-project
   [{:keys [db logger]} {:keys [parameters]}]
   (try
-    (let [opts {:ids [(get-in parameters [:path :id])]}
+    (let [project-id (get-in parameters [:path :id])
+          opts {:ids [project-id]}
           db-opts (db.prj/opts->db-opts opts)
           filters (select-keys db-opts [:ids])
           deleted-values (db.prj/delete-projects (:spec db)
                                                  {:filters filters})]
       (if (= deleted-values 1)
-        (r/ok {:success? true})
+        (let [{:keys [success?] :as delete-ctx-result} (srv.permissions/delete-resource-context
+                                                        {:conn (:spec db)
+                                                         :logger logger}
+                                                        {:resource-id project-id
+                                                         :context-type-name :project})]
+          (if success?
+            (r/ok {:success? true})
+            (r/server-error delete-ctx-result)))
         (r/server-error {:success? false
                          :reason :could-not-delete-project})))
     (catch Exception e
@@ -199,8 +223,15 @@
 
 (defmethod ig/init-key :gpml.handler.project/post
   [_ config]
-  (fn [req]
-    (create-project config req)))
+  (fn [{:keys [user] :as req}]
+    (if (h.r.permission/operation-allowed?
+         config
+         {:user-id (:id user)
+          :entity-type :project
+          :operation-type :create
+          :root-context? true})
+      (create-project config req)
+      (r/forbidden {:message "Unauthorized"}))))
 
 (defmethod ig/init-key :gpml.handler.project/post-params
   [_ _]
@@ -221,11 +252,18 @@
 
 (defmethod ig/init-key :gpml.handler.project/get
   [_ config]
-  (fn [req]
-    (let [result (get-projects config req)]
-      (if (:success? result)
-        (r/ok result)
-        (r/server-error result)))))
+  (fn [{:keys [user] :as req}]
+    (if (h.r.permission/operation-allowed?
+         config
+         {:user-id (:id user)
+          :entity-type :application
+          :custom-permission :read-projects
+          :root-context? true})
+      (let [result (get-projects config req)]
+        (if (:success? result)
+          (r/ok result)
+          (r/server-error result)))
+      (r/forbidden {:message "Unauthorized"}))))
 
 (defmethod ig/init-key :gpml.handler.project/get-params
   [_ _]
@@ -239,13 +277,20 @@
 
 (defmethod ig/init-key :gpml.handler.project/put
   [_ config]
-  (fn [req]
-    (update-project config req)))
+  (fn [{:keys [user parameters] :as req}]
+    (if (h.r.permission/operation-allowed?
+         config
+         {:user-id (:id user)
+          :entity-type :project
+          :entity-id (get-in parameters [:path :id])
+          :operation-type :update})
+      (update-project config req)
+      (r/forbidden {:message "Unauthorized"}))))
 
 (defmethod ig/init-key :gpml.handler.project/put-params
   [_ _]
   {:path (mu/select-keys dom.prj/Project [:id])
-   :body (mu/optional-keys (mu/dissoc dom.prj/Project :id))})
+   :body (mu/optional-keys (util.malli/dissoc dom.prj/Project [:id :stakeholder_id]))})
 
 (defmethod ig/init-key :gpml.handler.project/put-responses
   [_ _]
@@ -254,16 +299,23 @@
 
 (defmethod ig/init-key :gpml.handler.project/get-by-id
   [_ config]
-  (fn [{{:keys [path]} :parameters :as req}]
-    (let [req (assoc-in req [:parameters :query :ids] [(:id path)])
-          result (get-projects config req)]
-      (if (:success? result)
-        (if-let [project (-> result :projects first)]
-          (r/ok {:success? true
-                 :project project})
-          (r/not-found {:success? false
-                        :reason :project-not-found}))
-        (r/server-error result)))))
+  (fn [{{:keys [path]} :parameters user :user :as req}]
+    (if (h.r.permission/operation-allowed?
+         config
+         {:user-id (:id user)
+          :entity-type :project
+          :entity-id (:id path)
+          :operation-type :read})
+      (let [req (assoc-in req [:parameters :query :ids] [(:id path)])
+            result (get-projects config req)]
+        (if (:success? result)
+          (if-let [project (-> result :projects first)]
+            (r/ok {:success? true
+                   :project project})
+            (r/not-found {:success? false
+                          :reason :project-not-found}))
+          (r/server-error result)))
+      (r/forbidden {:message "Unauthorized"}))))
 
 (defmethod ig/init-key :gpml.handler.project/get-by-id-params
   [_ _]
@@ -278,8 +330,15 @@
 
 (defmethod ig/init-key :gpml.handler.project/delete
   [_ config]
-  (fn [req]
-    (delete-project config req)))
+  (fn [{:keys [user parameters] :as req}]
+    (if (h.r.permission/operation-allowed?
+         config
+         {:user-id (:id user)
+          :entity-type :project
+          :entity-id (get-in parameters [:path :id])
+          :operation-type :delete})
+      (delete-project config req)
+      (r/forbidden {:message "Unauthorized"}))))
 
 (defmethod ig/init-key :gpml.handler.project/delete-params
   [_ _]
