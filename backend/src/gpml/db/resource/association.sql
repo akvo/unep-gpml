6bf8e57c39cc912e7a19428085710f7d0b4ff7d3
lucassousaf <lucas.sousa@magnet.coop>
2023-07-19 16:53:29 +0200

    Refactor detail API PUT endpoint to add RBAC instrumentation
    [Re #1530]
    
    * Secure endpoint.
    * Assign and unassign roles based on individual connections.
    * Organisation ownership will change as per the previous commit.

--- backend/src/gpml/db/resource/association.sql
+++ backend/src/gpml/db/resource/association.sql
@@ -1,6 +1,6 @@
 -- :name get-resource-associations :query :many
 -- :doc Get the organisation association details for a specific resource.
-SELECT *
+SELECT *, association as role
 FROM :i:table
 WHERE :i:resource-col = :filters.resource-id
 --~ (when (get-in params [:filters :entity-id]) " AND :i:entity-col = :filters.entity-id")
