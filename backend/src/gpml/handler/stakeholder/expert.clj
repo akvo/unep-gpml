50c77a233aa9a4dad9d65e66a72f1f7ce5ee71fc
joseAyudarte91 <jose.ayudarte@gmail.com>
2023-07-26 17:55:22 +0200

    Instrument Expert Suggest API endpoint with RBAC
    [Re #1530]
    
    Added RBAC permissions check, replacing the middleware that was used for
    that purpose.

--- backend/src/gpml/handler/stakeholder/expert.clj
+++ backend/src/gpml/handler/stakeholder/expert.clj
@@ -283,8 +283,15 @@ User %s is suggesting an expert with the following information:
 
 (defmethod ig/init-key :gpml.handler.stakeholder.expert/post-suggest
   [_ config]
-  (fn [req]
-    (suggest-expert config req)))
+  (fn [{:keys [user] :as req}]
+    (if (h.r.permission/operation-allowed?
+         config
+         {:user-id (:id user)
+          :entity-type :application
+          :custom-permission :suggest-experts
+          :root-context? true})
+      (suggest-expert config req)
+      (r/forbidden {:message "Unauthorized"}))))
 
 (defmethod ig/init-key :gpml.handler.stakeholder.expert/get-params
   [_ _]
