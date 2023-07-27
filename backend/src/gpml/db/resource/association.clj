45e5bb3654a71956f6d6efb5a70e8da723070f28
Lucas Sousa <lucas.sousa@magnet.coop>
2023-07-27 11:20:41 +0200

    Impl generic association and org permission inheritance
    [Re #1530]
    
    * Permission inheritance is directly associated with
    `associations`. We had a very dispersed management of both
    associations and the permissions derived from it. This implementation
    covers both the associations management and the calls to RBAC to
    manage the derived role/permissions assignments.

--- backend/src/gpml/db/resource/association.clj
+++ backend/src/gpml/db/resource/association.clj
@@ -2,6 +2,12 @@
   {:ns-tracker/resource-deps ["resource/association.sql"]}
   (:require [hugsql.core :as hugsql]))
 
-(declare get-resource-associations)
+(declare get-resource-associations
+         create-stakeholder-association
+         create-organisation-association
+         update-stakeholder-association
+         update-organisation-association
+         delete-stakeholder-associations
+         delete-organisation-associations)
 
 (hugsql/def-db-fns "gpml/db/resource/association.sql" {:quoting :ansi})
--- backend/src/gpml/db/resource/association.sql
+++ backend/src/gpml/db/resource/association.sql
@@ -5,3 +5,44 @@ FROM :i:table
 WHERE :i:resource-col = :filters.resource-id
 --~ (when (get-in params [:filters :entity-id]) " AND :i:entity-col = :filters.entity-id")
 --~ (when (get-in params [:filters :associations]) " AND association = ANY(CAST(ARRAY[:v*:filters.associations] AS :i:resource-assoc-type[])) ")
+;
+
+-- :name delete-stakeholder-associations :execute :affected
+DELETE FROM
+--~ (format "stakeholder_%s" (:table-suffix params))
+WHERE id IN (:v*:ids);
+
+-- :name delete-organisation-associations :execute :affected
+DELETE FROM
+--~ (format "organisation_%s" (:table-suffix params))
+WHERE id IN (:v*:ids);
+
+-- :name update-stakeholder-association :execute :affected
+-- :require [gpml.util.sql]
+UPDATE
+--~ (format "stakeholder_%s" (:table-suffix params))
+SET modified=now(),
+--~ (#'gpml.util.sql/generate-update-stakeholder-association params)
+WHERE id = :id;
+
+-- :name update-organisation-association :execute :affected
+-- :require [gpml.util.sql]
+UPDATE
+--~ (format "organisation_%s" (:table-suffix params))
+SET modified=now(),
+--~ (#'gpml.util.sql/generate-update-stakeholder-association params)
+WHERE id = :id;
+
+-- :name create-stakeholder-association :execute :affected
+INSERT INTO
+--~ (format "stakeholder_%s(stakeholder, :i:resource-col, association, remarks)" (:table-suffix params))
+VALUES
+--~ (format "(:stakeholder, :resource-id, :association::%s_association, :remarks)" (:resource-col params))
+;
+
+-- :name create-organisation-association :execute :affected
+INSERT INTO
+--~ (format "organisation_%s(organisation, :i:resource-col, association, remarks)" (:table-suffix params))
+VALUES
+--~ (format "(:organisation, :resource-id, :association::%s_association, :remarks)" (:resource-col params))
+;
