32b9717bc7e96d429d833013975289c869363450
Lucas Sousa <lucas.sousa@magnet.coop>
2023-08-01 16:08:14 +0200

    Fix auth_test.clj to use auth-middleware instead of admin one
    [Re #1530]
    
    * admin-required-middleware was replace with RBAC instrumentation at
    the endpoint level.

--- backend/test/gpml/auth_test.clj
+++ backend/test/gpml/auth_test.clj
@@ -47,39 +47,6 @@
     (db.stakeholder/update-stakeholder-role db (assoc sth :role role))
     (db.stakeholder/stakeholder-by-id db sth)))
 
-(defn post-approved-user-request
-  [user]
-  (->
-   (mock/request :post "/" {})
-   (assoc :approved? true
-          :user user
-          :jwt-claims {:email (:email user) :email_verified true})))
-
-(deftest test-admin-middleware
-  (let [system (-> fixtures/*system*
-                   (ig/init [::auth/admin-required-middleware]))
-        db (-> system :duct.database.sql/hikaricp :spec)
-        middleware (::auth/admin-required-middleware system)
-        admin (new-stakeholder db 1 "admin@un.org" "ADMIN" "APPROVED")
-        user (new-stakeholder db 2 "user@un.org" "USER" "APPROVED")
-        test-fn (fn [{{admin-id :id} :admin}]
-                  (testing "> admin id added correctly to request"
-                    (is (= (:id admin) admin-id)))
-                  {:status 200 :body "OK"})
-        handler (middleware test-fn)]
-
-    (testing "An admin is authorized to perform the request"
-      (is (= 200 (-> admin
-                     post-approved-user-request
-                     handler
-                     :status))))
-
-    (testing "A normal user is **not** authorized to perform the request"
-      (is (= 403 (-> user
-                     post-approved-user-request
-                     handler
-                     :status))))))
-
 (deftest test-check-authentication
   (testing "Testing check-authentication logic"
     (are [expected auth-header result] (= expected (select-keys (auth/check-authentication {:headers {"authorization" auth-header}}
@@ -94,7 +61,7 @@
 (deftest test-check-approved
   (testing "Testing check-approved logic"
     (let [system (-> fixtures/*system*
-                     (ig/init [::auth/admin-required-middleware]))
+                     (ig/init [::auth/auth-middleware]))
           db (-> system :duct.database.sql/hikaricp :spec)
 
           ;; Create approved user
@@ -104,7 +71,7 @@
           unapproved (new-stakeholder db 2 "foo@bar.org" "USER" "SUBMITTED")]
 
       (are [expected auth-header]
-           (= expected (auth/check-approved db auth-header))
+          (= expected (auth/check-approved db auth-header))
 
         ;; Anonymous user
         {:approved? false :user nil} {:authenticated? false}
