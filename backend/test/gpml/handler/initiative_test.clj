ec178d76c5f4d9aabed830f8409fe9bbd4723557
Lucas Sousa <lucas.sousa@magnet.coop>
2023-08-01 17:40:48 +0200

    Update initiative API tests to consider RBAC
    [Re #1530]
    
    * Added additional test to check if user has the necessary permissions
    to create a initiative.

--- backend/test/gpml/handler/initiative_test.clj
+++ backend/test/gpml/handler/initiative_test.clj
@@ -2,46 +2,61 @@
   (:require [clojure.java.io :as io]
             [clojure.test :refer [deftest is testing use-fixtures]]
             [gpml.db.initiative :as db.initiative]
-            [gpml.db.stakeholder :as db.stakeholder]
             [gpml.domain.types :as dom.types]
             [gpml.fixtures :as fixtures]
             [gpml.handler.initiative :as initiative]
             [gpml.handler.profile-test :as profile-test]
             [gpml.seeder.main :as seeder]
+            [gpml.test-util :as test-util]
             [integrant.core :as ig]
             [ring.mock.request :as mock]))
 
 (use-fixtures :each fixtures/with-test-system)
 
 (deftest handler-post-test
-  (testing "New initiative is created"
-    (let [system (ig/init fixtures/*system* [::initiative/post])
-          handler (::initiative/post system)
-          db (-> system :duct.database.sql/hikaricp :spec)
-          ;; create new country [IDN SPA]
-          ;; create new country group [Africa Asia Europe]
-          ;; create new organisation [Akvo]
-          ;; create new general 3 tags
-          _ (profile-test/seed-important-database db)
-          ;; create new user name John
-          user (db.stakeholder/new-stakeholder db (profile-test/new-profile 1))
-          _ (db.stakeholder/update-stakeholder-status db (assoc user :review_status "APPROVED"))
-          ;; John create new initiative with new organisation
-          submission (seeder/parse-data
-                      (slurp (io/resource "examples/submission-initiative.json"))
-                      {:keywords? true
-                       :add-default-lang? true})
-          resp (handler (-> (mock/request :post "/")
-                            (assoc :jwt-claims {:email "john@org"})
-                            (assoc :body-params (assoc submission :version 1))
-                            (assoc :parameters {:body {:source dom.types/default-resource-source}})))
-          data (db.initiative/initiative-by-id db (:body resp))]
-      (is (= 201 (:status resp)))
-      (is (= 1 (-> data :version)))
-      (is (= 10001 (-> data :created_by)))
-      (is (= 10001 (-> data :id)))
-      (is (= "SUBMITTED" (-> data :review_status)))
-      (is (= (:url data) (:url submission)))
-      (doseq [[k v] submission]
-        (testing (str "testing-" k)
-          (is (= v (get data k))))))))
+  (let [system (ig/init fixtures/*system* [::initiative/post])
+        config (get system [:duct/const :gpml.config/common])
+        conn (get-in config [:db :spec])
+        handler (::initiative/post system)
+        _ (profile-test/seed-important-database conn)]
+    (testing "New initiative is created"
+      (let [;; create new user name John
+            sth-id (test-util/create-test-stakeholder config
+                                                      "john.doe@mail.invalid"
+                                                      "APPROVED"
+                                                      "USER")
+            ;; John create new initiative with new organisation
+            submission (seeder/parse-data
+                        (slurp (io/resource "examples/submission-initiative.json"))
+                        {:keywords? true
+                         :add-default-lang? true})
+            resp (handler (-> (mock/request :post "/")
+                              (assoc :user {:id sth-id})
+                              (assoc :body-params (assoc submission :version 1))
+                              (assoc :parameters {:body {:source dom.types/default-resource-source}})))
+            data (db.initiative/initiative-by-id conn (:body resp))]
+        (is (= 201 (:status resp)))
+        (is (= 1 (-> data :version)))
+        (is (= 10001 (-> data :created_by)))
+        (is (= 10001 (-> data :id)))
+        (is (= "SUBMITTED" (-> data :review_status)))
+        (is (= (:url data) (:url submission)))
+        (doseq [[k v] submission]
+          (testing (str "testing-" k)
+            (is (= v (get data k)))))))
+    (testing "Unapproved users can't create a resource"
+      (let [;; create new user name John
+            sth-id (test-util/create-test-stakeholder config
+                                                      "john.doe2@mail.invalid"
+                                                      "SUBMITTED"
+                                                      "USER")
+            ;; John create new initiative with new organisation
+            submission (seeder/parse-data
+                        (slurp (io/resource "examples/submission-initiative.json"))
+                        {:keywords? true
+                         :add-default-lang? true})
+            resp (handler (-> (mock/request :post "/")
+                              (assoc :user {:id sth-id})
+                              (assoc :body-params (assoc submission :version 1))
+                              (assoc :parameters {:body {:source dom.types/default-resource-source}})))]
+        (is (= 403 (:status resp)))))))
