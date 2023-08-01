4e1c9248f679c6cfcc2bbdc826b0fd4453061293
Lucas Sousa <lucas.sousa@magnet.coop>
2023-08-01 18:06:17 +0200

    Update policy API tests to consider RBAC
    [Re #1530]
    
    * Added an additional test to check permissions when creating a new policy.

--- backend/test/gpml/handler/policy_test.clj
+++ backend/test/gpml/handler/policy_test.clj
@@ -2,12 +2,12 @@
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [gpml.db.language :as db.language]
             [gpml.db.policy :as db.policy]
-            [gpml.db.stakeholder :as db.stakeholder]
             [gpml.db.tag :as db.tag]
             [gpml.domain.types :as dom.types]
             [gpml.fixtures :as fixtures]
             [gpml.handler.policy :as policy]
             [gpml.handler.profile-test :as profile-test]
+            [gpml.test-util :as test-util]
             [integrant.core :as ig]
             [ring.mock.request :as mock]))
 
@@ -35,50 +35,58 @@
    :language "id"})
 
 (deftest handler-post-test
-  (testing "New policy is created"
-    (let [system (ig/init fixtures/*system* [::policy/post])
-          handler (::policy/post system)
-          db (-> system :duct.database.sql/hikaricp :spec)
-          ;; create new country [IDN SPA]
-          ;; create new country group [Africa Asia Europe]
-          ;; create new organisation [Akvo]
-          ;; create new general 3 tags
-          data (profile-test/seed-important-database db)
-          ;; create new policy category tag
-          _ (do (db.tag/new-tag-category db {:category "financing mechanism"})
-                (db.tag/new-tag db {:tag "RT 1" :tag_category 2})
-                (db.tag/new-tag db {:tag "RT 2" :tag_category 2}))
-          ;; create new language
-          _ (db.language/new-language db {:iso_code "id"
+  (let [system (ig/init fixtures/*system* [::policy/post])
+        config (get system [:duct/const :gpml.config/common])
+        conn (get-in config [:db :spec])
+        handler (::policy/post system)
+        data (profile-test/seed-important-database conn)
+        _ (do (db.tag/new-tag-category conn {:category "financing mechanism"})
+              (db.tag/new-tag conn {:tag "RT 1" :tag_category 2})
+              (db.tag/new-tag conn {:tag "RT 2" :tag_category 2}))
+        ;; create new language
+        _ (db.language/new-language conn {:iso_code "id"
                                           :english_name "Indonesian"
-                                          :native_name "Bahasa Indonesia"})
-          ;; create new user name John
-          user (db.stakeholder/new-stakeholder db (profile-test/new-profile 1))
-          _ (db.stakeholder/update-stakeholder-status db (assoc user :review_status "APPROVED"))
-          ;; create John create new policy with available organisation
-          resp-one (handler (-> (mock/request :post "/")
-                                (assoc :jwt-claims {:email "john@org"})
-                                (assoc :body-params (new-policy data))
-                                (assoc :parameters {:body {:source dom.types/default-resource-source}})))
-          ;; create John create new policy with new organisation
-          resp-two (handler (-> (mock/request :post "/")
-                                (assoc :jwt-claims {:email "john@org"})
-                                (assoc :body-params (assoc (new-policy data) :org
-                                                           {:id -1
-                                                            :name "New Era"
-                                                            :geo_coverage_type "global"
-                                                            :country (-> (:countries data) second :id)}))
-                                (assoc :parameters {:body {:source dom.types/default-resource-source}})))
-          policy-one (db.policy/policy-by-id db (:body resp-one))
-          policy-two (db.policy/policy-by-id db (:body resp-two))]
-      (is (= 201 (:status resp-one)))
-      (is (= (assoc (new-policy data)
-                    :id 10001
-                    :image nil
-                    :tags (map :id (:tags data))
-                    :created_by 10001) policy-one))
-      (is (= (assoc (new-policy data)
-                    :id 10002
-                    :image nil
-                    :tags (map :id (:tags data))
-                    :created_by 10001) policy-two)))))
+                                          :native_name "Bahasa Indonesia"})]
+    (testing "New policy is created"
+      (let [;; create new user name John
+            sth-id (test-util/create-test-stakeholder config
+                                                      "john.doe@mail.invalid"
+                                                      "APPROVED"
+                                                      "USER")
+            ;; create John create new policy with available organisation
+            resp-one (handler (-> (mock/request :post "/")
+                                  (assoc :user {:id sth-id}
+                                         :body-params (new-policy data)
+                                         :parameters {:body {:source dom.types/default-resource-source}})))
+            ;; create John create new policy with new organisation
+            resp-two (handler (-> (mock/request :post "/")
+                                  (assoc :user {:id sth-id}
+                                         :body-params (assoc (new-policy data) :org
+                                                             {:id -1
+                                                              :name "New Era"
+                                                              :geo_coverage_type "global"
+                                                              :country (-> (:countries data) second :id)})
+                                         :parameters {:body {:source dom.types/default-resource-source}})))
+            policy-one (db.policy/policy-by-id conn (:body resp-one))
+            policy-two (db.policy/policy-by-id conn (:body resp-two))]
+        (is (= 201 (:status resp-one)))
+        (is (= (assoc (new-policy data)
+                      :id 10001
+                      :image nil
+                      :tags (map :id (:tags data))
+                      :created_by 10001) policy-one))
+        (is (= (assoc (new-policy data)
+                      :id 10002
+                      :image nil
+                      :tags (map :id (:tags data))
+                      :created_by 10001) policy-two))))
+    (testing "Unapproved user shouldn't be able to create policy"
+      (let [sth-id (test-util/create-test-stakeholder config
+                                                      "john.doe2@mail.invalid"
+                                                      "SUBMITTED"
+                                                      "USER")
+            resp (handler (-> (mock/request :post "/")
+                              (assoc :user {:id sth-id}
+                                     :body-params (new-policy data)
+                                     :parameters {:body {:source dom.types/default-resource-source}})))]
+        (is (= 403 (:status resp)))))))
