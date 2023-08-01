d22bc738458876160c06a95a49d8cb7f738daa02
Lucas Sousa <lucas.sousa@magnet.coop>
2023-08-01 16:11:38 +0200

    Update comment tests to consider RBAC system
    [Re #1530]
    
    * Added a additional tests to check permissions in the different API endpoints.

--- backend/test/gpml/handler/comment_test.clj
+++ backend/test/gpml/handler/comment_test.clj
@@ -22,40 +22,37 @@
    :title "Dummy title"
    :content "Dummy content"})
 
-(defn- make-profile [first-name last-name email]
-  {:picture nil
-   :cv nil
-   :title "mr."
-   :first_name first-name
-   :last_name last-name
-   :affiliation nil
-   :email email
-   :linked_in nil
-   :twitter nil
-   :url nil
-   :country nil
-   :representation "test"
-   :about "Lorem Ipsum"
-   :geo_coverage_type "global"
-   :role "USER"
-   :idp_usernames ["auth0|123"]})
-
 (deftest create-comment-test
-  (let [db (test-util/db-test-conn)
-        system (-> fixtures/*system*
+  (let [system (-> fixtures/*system*
                    (ig/init [::sut/post]))
+        config (get system [:duct/const :gpml.config/common])
+        conn (get-in config [:db :spec])
         handler (::sut/post system)
-        profile (make-profile "John" "Doe" "mail@org.com")
-        _ (seeder/seed db {:country? true :event? true})
-        sth-id (:id (db.stakeholder/new-stakeholder db profile))]
-    (testing "Happy path"
+        _ (seeder/seed conn {:country? true :event? true})
+        sth-id (test-util/create-test-stakeholder config
+                                                  "john.doe@mail.invalid"
+                                                  "APPROVED"
+                                                  "USER")]
+    (testing "Create a comment successfully"
       (let [comment-payload (random-comment-data sth-id 1 "event")
             request (-> (mock/request :post "/")
-                        (assoc :parameters {:body comment-payload}))
+                        (assoc :parameters {:body comment-payload}
+                               :user {:id sth-id}))
             {:keys [status body]} (handler request)]
         (is (= 200 status))
         (is (seq (:comment body)))
         (is (= (get-in body [:comment :author_id]) sth-id))))
+    (testing "User don't have the necessary permissions to comment"
+      (let [sth-id (test-util/create-test-stakeholder config
+                                                      "john.doe2@mail.invalid"
+                                                      "SUBMITTED"
+                                                      "USER")
+            comment-payload (random-comment-data sth-id 1 "event")
+            request (-> (mock/request :post "/")
+                        (assoc :parameters {:body comment-payload}
+                               :user {:id sth-id}))
+            {:keys [status body]} (handler request)]
+        (is (= 403 status))))
     (testing "Fails validation due to missing required parameters"
       (let [comment-payload (dissoc (random-comment-data sth-id 1 "event") :author_id)]
         (is (not (m/validate sut/create-comment-params comment-payload)))
@@ -65,17 +62,20 @@
         (is (not (m/validate sut/create-comment-params comment-payload)))))))
 
 (deftest get-comment-test
-  (let [db (test-util/db-test-conn)
-        system (-> fixtures/*system*
+  (let [system (-> fixtures/*system*
                    (ig/init [::sut/get]))
+        config (get system [:duct/const :gpml.config/common])
+        conn (get-in config [:db :spec])
         handler (::sut/get system)
-        profile (make-profile "John" "Doe" "mail@org.com")
-        _ (seeder/seed db {:country? true :event? true})
-        sth-id (:id (db.stakeholder/new-stakeholder db profile))
-        comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
-                                                         :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
-                                                  (util/replace-in-keys #"_" "-")))]
-    (testing "Happy path"
+        _ (seeder/seed conn {:country? true :event? true})
+        sth-id (test-util/create-test-stakeholder config
+                                                  "john.doe@mail.invalid"
+                                                  "APPROVED"
+                                                  "USER")
+        comment (db.comment/create-comment conn (-> (assoc (random-comment-data sth-id 1 "event")
+                                                           :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                    (util/replace-in-keys #"_" "-")))]
+    (testing "Get comments successfully"
       (let [request (-> (mock/request :get "/")
                         (assoc :parameters {:query {:resource_id "1" :resource_type "event"}}))
             {:keys [status body]} (handler request)]
@@ -89,10 +89,10 @@
       (let [parameters {:resource_type "event" :resource_id :a}]
         (is (not (m/validate sut/get-resource-comments-params parameters)))))
     (testing "Returns a tree like datastructure when there are comment replies"
-      (let [reply-comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
-                                                                   :parent-id (:id comment)
-                                                                   :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
-                                                            (util/replace-in-keys #"_" "-")))
+      (let [reply-comment (db.comment/create-comment conn (-> (assoc (random-comment-data sth-id 1 "event")
+                                                                     :parent-id (:id comment)
+                                                                     :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                              (util/replace-in-keys #"_" "-")))
             request (-> (mock/request :get "/")
                         (assoc :parameters {:query {:resource_id "1" :resource_type "event"}}))
             {:keys [status body]} (handler request)]
@@ -100,27 +100,43 @@
         (is (= (:id reply-comment) (-> body :comments first :children first :id)))))))
 
 (deftest update-comment-test
-  (let [db (test-util/db-test-conn)
-        system (-> fixtures/*system*
+  (let [system (-> fixtures/*system*
                    (ig/init [::sut/put]))
+        config (get system [:duct/const :gpml.config/common])
+        conn (get-in config [:db :spec])
         handler (::sut/put system)
-        profile (make-profile "John" "Doe" "mail@org.com")
-        _ (seeder/seed db {:country? true :event? true})
-        sth-id (:id (db.stakeholder/new-stakeholder db profile))
-        comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
-                                                         :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
-                                                  (util/replace-in-keys #"_" "-")))]
-    (testing "Happy path"
+        _ (seeder/seed conn {:country? true :event? true})
+        sth-id (test-util/create-test-stakeholder config
+                                                  "john.doe@mail.invalid"
+                                                  "APPROVED"
+                                                  "USER")
+        comment (db.comment/create-comment conn (-> (assoc (random-comment-data sth-id 1 "event")
+                                                           :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                    (util/replace-in-keys #"_" "-")))]
+    (testing "Update a comment successfully"
       (let [request (-> (mock/request :put "/")
                         (assoc :parameters {:body {:id (:id comment)
                                                    :title "test comment"
-                                                   :content "test content"}}))
+                                                   :content "test content"}}
+                               :user {:id sth-id}))
             {:keys [status body]} (handler request)
-            updated-comment (first (db.comment/get-resource-comments db {:id (:id comment)}))]
+            updated-comment (first (db.comment/get-resource-comments conn {:id (:id comment)}))]
         (is (= 200 status))
         (is (= 1 (:updated-comments body)))
         (is (= "test comment" (:title updated-comment)))
         (is (= "test content" (:content updated-comment)))))
+    (testing "Update a comment of another user should output unauthorized"
+      (let [sth-id (test-util/create-test-stakeholder config
+                                                      "john.doe2@mail.invalid"
+                                                      "APPROVED"
+                                                      "USER")
+            request (-> (mock/request :put "/")
+                        (assoc :parameters {:body {:id (:id comment)
+                                                   :title "test comment"
+                                                   :content "test content"}}
+                               :user {:id sth-id}))
+            {:keys [status]} (handler request)]
+        (is (= 403 status))))
     (testing "Fails validation due to missing required parameters"
       (let [parameters {}]
         (is (not (m/validate sut/update-comment-params parameters)))
@@ -133,38 +149,69 @@
         (is (= 3 (count (:errors (m/explain sut/update-comment-params parameters)))))))))
 
 (deftest delete-comment-test
-  (let [db (test-util/db-test-conn)
-        system (-> fixtures/*system*
+  (let [system (-> fixtures/*system*
                    (ig/init [::sut/delete]))
+        config (get system [:duct/const :gpml.config/common])
+        conn (get-in config [:db :spec])
         handler (::sut/delete system)
-        profile (make-profile "John" "Doe" "mail@org.com")
-        _ (seeder/seed db {:country? true :event? true})
-        sth-id (:id (db.stakeholder/new-stakeholder db profile))]
-    (testing "Happy path"
-      (let [comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
-                                                             :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
-                                                      (util/replace-in-keys #"_" "-")))
+        _ (seeder/seed conn {:country? true :event? true})
+        sth-id (test-util/create-test-stakeholder config
+                                                  "john.doe@mail.invalid"
+                                                  "APPROVED"
+                                                  "USER")]
+    (testing "Delete a comment successfully"
+      (let [comment (db.comment/create-comment conn (-> (assoc (random-comment-data sth-id 1 "event")
+                                                               :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                        (util/replace-in-keys #"_" "-")))
             request (-> (mock/request :delete "/")
-                        (assoc :parameters {:path {:id (:id comment)}}))
+                        (assoc :parameters {:path {:id (:id comment)}}
+                               :user {:id sth-id}))
             {:keys [status body]} (handler request)]
         (is (= 200 status))
         (is (= 1 (:deleted-comments body)))))
     (testing "Deleting a root comment also delete its replies"
-      (let [comment (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
-                                                             :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
-                                                      (util/replace-in-keys #"_" "-")))
-            _ (db.comment/create-comment db (-> (assoc (random-comment-data sth-id 1 "event")
-                                                       :parent-id (:id comment)
-                                                       :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
-                                                (util/replace-in-keys #"_" "-")))
+      (let [comment (db.comment/create-comment conn (-> (assoc (random-comment-data sth-id 1 "event")
+                                                               :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                        (util/replace-in-keys #"_" "-")))
+            _ (db.comment/create-comment conn (-> (assoc (random-comment-data sth-id 1 "event")
+                                                         :parent-id (:id comment)
+                                                         :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                  (util/replace-in-keys #"_" "-")))
             request (-> (mock/request :delete "/")
-                        (assoc :parameters {:path {:id (:id comment)}}))
+                        (assoc :parameters {:path {:id (:id comment)}}
+                               :user {:id sth-id}))
             {:keys [status body]} (handler request)
-            comments (db.comment/get-resource-comments db {})]
+            comments (db.comment/get-resource-comments conn {})]
         (is (= 200 status))
         ;; NOTE: since the parent-id foreign key has the ON DELETE
         ;; CASCADE the deletion of child comments doesn't account to
         ;; number of rows affected. Therefore we just get the number
         ;; of affected rows by the DELETE statement.
         (is (= 1 (:deleted-comments body)))
-        (is (not (seq comments)))))))
+        (is (not (seq comments)))))
+    (testing "Trying to delete another user's comment will outputs unauthorized"
+      (let [sth-id-2 (test-util/create-test-stakeholder config
+                                                        "john.doe2@mail.invalid"
+                                                        "APPROVED"
+                                                        "USER")
+            comment (db.comment/create-comment conn (-> (assoc (random-comment-data sth-id 1 "event")
+                                                               :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                        (util/replace-in-keys #"_" "-")))
+            request (-> (mock/request :delete "/")
+                        (assoc :parameters {:path {:id (:id comment)}}
+                               :user {:id sth-id-2}))
+            {:keys [status]} (handler request)]
+        (is (= 403 status))))
+    (testing "Admins should be able to delete comments"
+      (let [sth-admin-id (test-util/create-test-stakeholder config
+                                                            "john.doe3@mail.invalid"
+                                                            "APPROVED"
+                                                            "ADMIN")
+            comment (db.comment/create-comment conn (-> (assoc (random-comment-data sth-id 1 "event")
+                                                               :updated-at (time-pre-j8/sql-timestamp (time/instant) "UTC"))
+                                                        (util/replace-in-keys #"_" "-")))
+            request (-> (mock/request :delete "/")
+                        (assoc :parameters {:path {:id (:id comment)}}
+                               :user {:id sth-admin-id}))
+            {:keys [status]} (handler request)]
+        (is (= 200 status))))))
