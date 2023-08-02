63e051ac4e3b71b6df0cf78b5e1f308613c2517b
Lucas Sousa <lucas.sousa@magnet.coop>
2023-08-02 13:33:37 +0200

    Update submission API tests to consider RBAC
    [Re #1530]

--- backend/test/gpml/handler/submission_test.clj
+++ backend/test/gpml/handler/submission_test.clj
@@ -1,10 +1,11 @@
 (ns gpml.handler.submission-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [gpml.db.event :as db.event]
-            [gpml.db.stakeholder :as db.stakeholder]
             [gpml.fixtures :as fixtures]
             [gpml.handler.profile-test :as profile-test]
             [gpml.handler.submission :as submission]
+            [gpml.service.permissions :as srv.permissions]
+            [gpml.test-util :as test-util]
             [integrant.core :as ig]
             [ring.mock.request :as mock]))
 
@@ -13,56 +14,59 @@
 (deftest handler-get-test
   (testing "Get pending submission list"
     (let [system (ig/init fixtures/*system* [::submission/get])
+          config (get system [:duct/const :gpml.config/common])
+          conn (get-in config [:db :spec])
+          logger (get config :logger)
           handler (::submission/get system)
-          db (-> system :duct.database.sql/hikaricp :spec)
-          _ (profile-test/seed-important-database db)
-          ;; create new admin name Jane
-          admin (profile-test/new-profile 1)
-          admin (db.stakeholder/new-stakeholder db  (assoc admin
-                                                           :email "jane@org"
-                                                           :first_name "Jane"))
-          _ (db.stakeholder/update-stakeholder-role db (assoc admin
-                                                              :role "ADMIN"
-                                                              :review_status "APPROVED"))
-          ;; User Default
-          user (profile-test/new-profile 1)
-          ;; create new user name John
-          _ (db.stakeholder/new-stakeholder db  (profile-test/new-profile 1))
 
+          _ (profile-test/seed-important-database conn)
+          ;; create new admin name Jane
+          admin-id (test-util/create-test-stakeholder config
+                                                      "jane@org.com"
+                                                      "APPROVED"
+                                                      "ADMIN")
+          ;; User Default
+          _ (test-util/create-test-stakeholder config
+                                               "john@org.com"
+                                               "SUBMITTED"
+                                               "ADMIN")
           ;; create new user name Bob
-          _ (db.stakeholder/new-stakeholder db  (assoc user
-                                                       :email "bob@org"
-                                                       :first_name "Bob"
-                                                       :last_name "Doe"))
+          _ (test-util/create-test-stakeholder config
+                                               "bob@org.com"
+                                               "SUBMITTED"
+                                               "ADMIN")
 
           ;; create new user name Justin
-          user_justin (db.stakeholder/new-stakeholder db  (assoc user
-                                                                 :email "justin@org"
-                                                                 :first_name "Justin"
-                                                                 :last_name "Doe"))
-
-          ;; Jane approve justin
-          _ (db.stakeholder/update-stakeholder-status db (assoc user_justin
-                                                                :review_status "APPROVED"
-                                                                :reviewed_by (:id admin)))
+          user-justin-id (test-util/create-test-stakeholder config
+                                                            "justin@org.com"
+                                                            "APPROVED"
+                                                            "ADMIN")
 
           ;; Justin create an event
-          _ (db.event/new-event db {:title "Title"
-                                    :start_date "2021-02-03T09:00:00Z"
-                                    :end_date "2021-02-03T10:00:00Z"
-                                    :description "desc"
-                                    :remarks "some remarks"
-                                    :country nil
-                                    :city nil
-                                    :image nil
-                                    :geo_coverage_type "global"
-                                    :created_by (:id user_justin)
-                                    :language "en"})
-
+          event (db.event/new-event conn {:title "Title"
+                                          :start_date "2021-02-03T09:00:00Z"
+                                          :end_date "2021-02-03T10:00:00Z"
+                                          :description "desc"
+                                          :remarks "some remarks"
+                                          :country nil
+                                          :city nil
+                                          :image nil
+                                          :geo_coverage_type "global"
+                                          :created_by user-justin-id
+                                          :language "en"})
+          _ (srv.permissions/create-resource-context {:conn conn
+                                                      :logger logger}
+                                                     {:context-type :event
+                                                      :resource-id (:id event)})
+          _ (srv.permissions/assign-roles-to-users {:conn conn
+                                                    :logger (:logger config)}
+                                                   [{:role-name :resource-owner
+                                                     :context-type :project
+                                                     :resource-id (:id event)
+                                                     :user-id user-justin-id}])
           ;; Jane trying to see the list of submission user in this case Nick
           resp (handler (-> (mock/request :get "/")
-                            (assoc :jwt-claims {:email "jane@org"}
-                                   :admin admin
+                            (assoc :user {:id admin-id}
                                    :parameters {:query {:page 1
                                                         :review_status "SUBMITTED"
                                                         :limit 10}})))]
@@ -73,39 +77,47 @@
       (is (= 3 (count (-> resp :body :data))))
       (is (= "stakeholder" (-> resp :body :data first :type)))
       (is (= "/submission/stakeholder/10002" (-> resp :body :data first :preview)))
-      (is (= "Mr. Doe Bob" (-> resp :body :data second :title)))
       (is (= "event" (some #{"event"} (map :type (-> resp :body :data)))))
       (is (= "event" (-> resp :body :data last :type)))
-      (is (= "justin@org" (-> (filter #(not= "tag" (:type %)) (-> resp :body :data)) last :created_by))))))
+      (is (= "justin@org.com" (-> (filter #(not= "tag" (:type %)) (-> resp :body :data)) last :created_by))))))
 
 (deftest handler-get-detail-test
-  (testing "Get pending submission detail"
-    (let [system (ig/init fixtures/*system* [::submission/get-detail])
-          handler (::submission/get-detail system)
-          db (-> system :duct.database.sql/hikaricp :spec)
-          _ (profile-test/seed-important-database db)
-          ;; create new admin name Jane
-          admin (profile-test/new-profile 1)
-          admin (db.stakeholder/new-stakeholder db  (assoc admin
-                                                           :email "jane@org"
-                                                           :first_name "Jane"))
-          _ (db.stakeholder/update-stakeholder-role db (assoc admin
-                                                              :role "ADMIN"
-                                                              :review_status "APPROVED"))
-          ;; create new user name Justin
-          user (profile-test/new-profile 1)
-          justin (db.stakeholder/new-stakeholder db (assoc user
-                                                           :email "justin@org"
-                                                           :first_name "Justin"
-                                                           :last_name "Doe"))
-          resp (handler (-> (mock/request :get "/")
-                            (assoc :jwt-claims {:email "jane@org"}
-                                   :admin admin
-                                   :parameters
-                                   {:path (assoc justin :submission "stakeholder")})))
-          body (:body resp)]
-      (is (not (:public_email justin)))
-      (is (= (:email body) "justin@org")))))
+  (let [system (ig/init fixtures/*system* [::submission/get-detail])
+        config (get system [:duct/const :gpml.config/common])
+        conn (get-in config [:db :spec])
+        handler (::submission/get-detail system)
+        _ (profile-test/seed-important-database conn)
+        ;; create new admin name Jane
+        admin-id (test-util/create-test-stakeholder config
+                                                    "jane@org.com"
+                                                    "APPROVED"
+                                                    "ADMIN")
+        ;; create new user name Justin
+        user-justin-id (test-util/create-test-stakeholder config
+                                                          "justin@org.com"
+                                                          "SUBMITTED"
+                                                          "ADMIN")]
+    (testing "Get pending submission detail"
+      (let [resp (handler (-> (mock/request :get "/")
+                              (assoc :user {:id admin-id}
+                                     :parameters
+                                     {:path {:id user-justin-id
+                                             :submission "stakeholder"}})))
+            body (:body resp)]
+        (is (= (:id body) user-justin-id))
+        (is (= (:email body) "justin@org.com"))))
+    (testing "If user is not admin or reviewer outputs unathorized"
+      (let [;; create new admin name Jane
+            sth-id (test-util/create-test-stakeholder config
+                                                      "john.doe@org.com"
+                                                      "APPROVED"
+                                                      "USER")
+            resp (handler (-> (mock/request :get "/")
+                              (assoc :user {:id sth-id}
+                                     :parameters
+                                     {:path {:id user-justin-id
+                                             :submission "stakeholder"}})))]
+        (is (= 403 (:status resp)))))))
 
 (deftest remap-initative-test
   (testing "Get remap initiative code"
