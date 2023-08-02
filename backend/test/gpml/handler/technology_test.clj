e4a22ac7d7a895259dadc9219b1d5e2f3d3e27f6
Lucas Sousa <lucas.sousa@magnet.coop>
2023-08-02 15:20:01 +0200

    Update technology API tests to consider RBAC
    [Re #1530]
    
    * Added a additional check for the POST API endpoint when called by an
    unapproved user.

--- backend/test/gpml/handler/technology_test.clj
+++ backend/test/gpml/handler/technology_test.clj
@@ -1,13 +1,14 @@
 (ns gpml.handler.technology-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [gpml.db.language :as db.language]
-            [gpml.db.stakeholder :as db.stakeholder]
+            [gpml.db.rbac-util :as db.rbac-util]
             [gpml.db.tag :as db.tag]
             [gpml.db.technology :as db.technology]
             [gpml.domain.types :as dom.types]
             [gpml.fixtures :as fixtures]
             [gpml.handler.profile-test :as profile-test]
             [gpml.handler.technology :as technology]
+            [gpml.test-util :as test-util]
             [integrant.core :as ig]
             [ring.mock.request :as mock]))
 
@@ -33,66 +34,83 @@
    :language "en"})
 
 (deftest handler-post-test
-  (testing "New technology is created"
-    (let [system (ig/init fixtures/*system* [::technology/post])
-          handler (::technology/post system)
-          db (-> system :duct.database.sql/hikaricp :spec)
-          ;; create new country [IDN SPA]
-          ;; create new country group [Africa Asia Europe]
-          ;; create new organisation [Akvo]
-          ;; create new general 3 tags
-          data (profile-test/seed-important-database db)
-          ;; create new technology category tag
-          _ (do (db.tag/new-tag-category db {:category "technology"})
-                (db.tag/new-tag db {:tag "RT 1" :tag_category 2})
-                (db.tag/new-tag db {:tag "RT 2" :tag_category 2}))
-          ;; create new language
-          _ (db.language/new-language db {:iso_code "id"
-                                          :english_name "Indonesian"
-                                          :native_name "Bahasa Indonesia"})
-          ;; create new user name John
-          user (db.stakeholder/new-stakeholder db (profile-test/new-profile 1))
-          _ (db.stakeholder/update-stakeholder-status db (assoc user :review_status "APPROVED"))
-          ;; create John create new technology with available organisation
-          resp-one (handler (-> (mock/request :post "/")
-                                (assoc :jwt-claims {:email "john@org"})
-                                (assoc :body-params (new-technology data))
-                                (assoc :parameters {:body {:source dom.types/default-resource-source}})))
-          ;; create John create new technology with new organisation
-          resp-two (handler (-> (mock/request :post "/")
-                                (assoc :jwt-claims {:email "john@org"})
-                                (assoc :body-params (assoc (new-technology data)
-                                                           :owners [(:id user)]
-                                                           :org
-                                                           {:id -1
-                                                            :name "New Era"
-                                                            :geo_coverage_type "global"
-                                                            :country (-> (:countries data) second :id)}))
-                                (assoc :parameters {:body {:source dom.types/default-resource-source}})))
-          technology-one (db.technology/technology-by-id db (:body resp-one))
-          technology-two (db.technology/technology-by-id db (:body resp-two))]
-      (is (= 201 (:status resp-one)))
-      (is (= (assoc (new-technology data)
-                    :id 10001
-                    :image nil
-                    :logo nil
-                    :headquarter nil
-                    :info_docs nil
-                    :sub_content_type nil
-                    :subnational_city nil
-                    :tags (map :id (:tags data))
-                    :owners [(:id user)]
-                    :created_by 10001)
-             technology-one))
-      (is (= (assoc (new-technology data)
-                    :id 10002
-                    :image nil
-                    :logo nil
-                    :headquarter nil
-                    :info_docs nil
-                    :sub_content_type nil
-                    :subnational_city nil
-                    :tags (map :id (:tags data))
-                    :owners [(:id user)]
-                    :created_by 10001)
-             technology-two)))))
+  (let [system (ig/init fixtures/*system* [::technology/post])
+        config (get system [:duct/const :gpml.config/common])
+        conn (get-in config [:db :spec])
+        handler (::technology/post system)
+        data (profile-test/seed-important-database conn)]
+    (testing "New technology is created"
+      (let [;; create new technology category tag
+            _ (do (db.tag/new-tag-category conn {:category "technology"})
+                  (db.tag/new-tag conn {:tag "RT 1" :tag_category 2})
+                  (db.tag/new-tag conn {:tag "RT 2" :tag_category 2}))
+            ;; create new language
+            _ (db.language/new-language conn {:iso_code "id"
+                                              :english_name "Indonesian"
+                                              :native_name "Bahasa Indonesia"})
+            ;; create new user name John
+            sth-id (test-util/create-test-stakeholder config
+                                                      "john.doe@mail.invalid"
+                                                      "APPROVED"
+                                                      "USER")
+            ;; create John create new technology with available organisation
+            resp-one (handler (-> (mock/request :post "/")
+                                  (assoc :user {:id sth-id}
+                                         :body-params (new-technology data)
+                                         :parameters {:body {:source dom.types/default-resource-source}})))
+            ;; create John create new technology with new organisation
+            resp-two (handler (-> (mock/request :post "/")
+                                  (assoc :user {:id sth-id}
+                                         :body-params (assoc (new-technology data)
+                                                             :owners [sth-id]
+                                                             :org
+                                                             {:id -1
+                                                              :name "New Era"
+                                                              :geo_coverage_type "global"
+                                                              :country (-> (:countries data) second :id)})
+                                         :parameters {:body {:source dom.types/default-resource-source}})))
+            technology-one (db.technology/technology-by-id conn (:body resp-one))
+            technology-two (db.technology/technology-by-id conn (:body resp-two))
+            owners-tech-one (db.rbac-util/get-users-with-granted-permission-on-resource conn {:context-type-name "technology"
+                                                                                              :resource-id (get-in resp-one [:body :id])
+                                                                                              :permission-name "technology/delete"})
+            owners-tech-two (db.rbac-util/get-users-with-granted-permission-on-resource conn {:context-type-name "technology"
+                                                                                              :resource-id (get-in resp-two [:body :id])
+                                                                                              :permission-name "technology/delete"})]
+        (is (= 201 (:status resp-one)))
+        (is (get (set (map :user_id owners-tech-one)) sth-id))
+        (is (get (set (map :user_id owners-tech-two)) sth-id))
+        (is (= (assoc (new-technology data)
+                      :id 10001
+                      :image nil
+                      :logo nil
+                      :headquarter nil
+                      :info_docs nil
+                      :sub_content_type nil
+                      :subnational_city nil
+                      :tags (map :id (:tags data))
+                      :owners [sth-id]
+                      :created_by 10001)
+               technology-one))
+        (is (= (assoc (new-technology data)
+                      :id 10002
+                      :image nil
+                      :logo nil
+                      :headquarter nil
+                      :info_docs nil
+                      :sub_content_type nil
+                      :subnational_city nil
+                      :tags (map :id (:tags data))
+                      :owners [sth-id]
+                      :created_by 10001)
+               technology-two))))
+    (testing "Unapproved users doesn't have enough permissions to create a new technology"
+      (let [sth-id (test-util/create-test-stakeholder config
+                                                      "john.doe2@mail.invalid"
+                                                      "SUBMITTED"
+                                                      "USER")
+            resp (handler (-> (mock/request :post "/")
+                              (assoc :user {:id sth-id}
+                                     :body-params (new-technology data)
+                                     :parameters {:body {:source dom.types/default-resource-source}})))]
+        (is (= 403 (:status resp)))))))
