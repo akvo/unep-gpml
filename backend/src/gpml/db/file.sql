20bb02c4dc6c326f4f1075e45f7f7de0f3e4a855
Lucas Sousa <lucas.sousa@magnet.coop>
2023-08-09 11:41:22 +0200

    Impl. file persistence layer
    [Re #1543]

---
+++ backend/src/gpml/db/file.sql
@@ -0,0 +1,16 @@
+-- :name create-file* :execute :affected
+INSERT INTO file(id, object_key, name, alt_desc, type, extension, visibility)
+VALUES (:id, :object_key, :name, :alt_desc, :type, :extension, :visibility::FILE_VISIBILITY);
+
+-- :name delete-file* :execute :affected
+DELETE FROM file
+WHERE id = :id;
+
+-- :name get-files* :query :many
+-- :doc asasa
+SELECT *
+FROM file
+WHERE 1=1
+--~ (when (get-in params [:filters :id]) " AND id = :filters.id")
+--~ (when (get-in params [:filters :ids]) " AND id IN (:v*:filters.ids)")
+--~ (when (get-in params [:filters :visibilities]) " AND visibility = ANY(CAST(ARRAY[:v*:filters.visibilities] AS FILE_VISIBILITY[]))")
