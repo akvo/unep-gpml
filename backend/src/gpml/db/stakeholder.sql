9f284298e822ac4873cea91e60690faefb2a2e43
Lucas Sousa <lucas.sousa@magnet.coop>
2023-08-11 14:22:57 +0200

    Add delete-stakeholder* hugsql function
    [Re #1543]
    
    * We'll need this to rollback stakeholder creation in case of an error.

--- backend/src/gpml/db/stakeholder.sql
+++ backend/src/gpml/db/stakeholder.sql
@@ -311,3 +311,7 @@ WHERE 1=1
 --~(when (seq (get-in params [:filters :roles])) " AND s.role = ANY(ARRAY[:v*:filters.roles]::stakeholder_role[])")
 --~(when (seq (get-in params [:filters :search-text])) " AND (LOWER(s.first_name) ILIKE '%' || :filters.search-text || '%' OR LOWER(s.last_name) ILIKE '%' || :filters.search-text || '%' OR LOWER(s.email) ILIKE '%' || :filters.search-text || '%')")
 GROUP BY s.id
+
+-- :name delete-stakeholder* :execute :affected
+DELETE FROM stakeholder
+WHERE id = :id;
