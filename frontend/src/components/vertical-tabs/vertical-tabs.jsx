24dee0247650b0e06dcee8315fd40042a21189cc
ifirmawan <firmawaneiwan@gmail.com>
2023-08-18 17:19:56 +0700

    [#1547] Create tabs functionality

--- frontend/src/components/vertical-tabs/vertical-tabs.jsx
+++ frontend/src/components/vertical-tabs/vertical-tabs.jsx
@@ -1,26 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import styles from "./style.module.scss";
 
-const VerticalTabs = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+const VerticalTabs = ({ children, defaultKey = 1 }) => {
+  const [activeKey, setActiveKey] = useState(defaultKey);
+
+  const tabsChildren = React.Children.map(children, (child, index) => {
+    if (React.isValidElement(child)) {
+      if (child.type.name === "Items") {
+        return React.cloneElement(child, {
+          activeKey,
+          setActiveKey,
+        });
+      }
+      if (child.type.name === "Content") {
+        const keyProp = child?.props?.key || index;
+        const visible = activeKey === keyProp;
+        return React.cloneElement(child, {
+          indexKey: index,
+          visible,
+        });
+      }
+    }
+    return child;
+  });
+  return <div className={styles.container}>{tabsChildren}</div>;
 };
 
-const Items = ({ children, className = null }) => {
-  return <ul className={classNames(styles.items, className)}>{children}</ul>;
-};
-
-const Item = ({ children, key, className = null }) => {
+const Items = ({ children, activeKey, setActiveKey, className = null }) => {
+  const itemsChildren = React.Children.map(children, (child, index) => {
+    if (React.isValidElement(child)) {
+      if (child.type.name === "Item") {
+        const indexKey = index + 1;
+        return React.cloneElement(child, {
+          activeKey,
+          indexKey,
+          onClick: (param) => setActiveKey(param || index),
+        });
+      }
+    }
+    return child;
+  });
   return (
-    <li className={classNames(styles.item, className)} key={key}>
+    <ul className={classNames(styles.items, className)}>{itemsChildren}</ul>
+  );
+};
+
+const Item = ({
+  children,
+  activeKey,
+  indexKey,
+  key,
+  onClick,
+  className = null,
+}) => {
+  const keyValue = key || indexKey;
+  const activeClass = activeKey === keyValue ? styles.activeItem : null;
+  return (
+    <li
+      className={classNames(styles.item, activeClass, className)}
+      onClick={() => onClick(keyValue)}
+      key={keyValue}
+      aria-label="vtabs-item"
+    >
       {children}
     </li>
   );
 };
 
-const Content = ({ children, className = null }) => {
+const Content = ({
+  children,
+  indexKey,
+  key,
+  className = null,
+  visible = true,
+}) => {
+  const isVisible = visible ? styles.show : styles.hidden;
+  const keyValue = key || indexKey;
   return (
-    <div className={classNames(styles.content, className)}>{children}</div>
+    <div
+      className={classNames(styles.content, isVisible, className)}
+      key={keyValue}
+    >
+      {children}
+    </div>
   );
 };
 
