f8fd4746a042136d17bc0241494e88f0d5849d1b
ifirmawan <firmawaneiwan@gmail.com>
2023-08-18 17:22:57 +0700

    [#1547] Create ui-text for what we are section

---
+++ frontend/src/ui-text/what-we-are.js
@@ -0,0 +1,54 @@
+export const whatWeAre = {
+  navs: [
+    {
+      text: "What we are?",
+    },
+    {
+      text: "What we do?",
+    },
+    {
+      text: "What is the connection between this platform and GPML?",
+    },
+    {
+      text: "Why to join the partnership?",
+    },
+  ],
+  contents: [
+    {
+      caption: "What we are?",
+      heading: {
+        strong: "The #1 global platform",
+        text: "on plastic pollution decisions.",
+      },
+      text:
+        "The plastic action platform brings decision-making power to countries and active organisations by integrating data, crowdsourcing knowledge, and fostering collaborations to co-create and advance solutions to end plastic pollution.",
+    },
+    {
+      caption: "What we do?",
+      heading: {
+        strong: "The #1 global platform",
+        text: "on plastic pollution decisions.",
+      },
+      text:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas alias, corporis amet officiis, ratione consectetur, quia hic repudiandae temporibus repellendus similique at eveniet qui obcaecati iusto quam facilis ut fuga.",
+    },
+    {
+      caption: "What is the connection between this platform and GPML?",
+      heading: {
+        strong: "The #1 global platform",
+        text: "on plastic pollution decisions.",
+      },
+      text:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas alias, corporis amet officiis, ratione consectetur, quia hic repudiandae temporibus repellendus similique at eveniet qui obcaecati iusto quam facilis ut fuga.",
+    },
+    {
+      caption: "What is the connection between this platform and GPML?",
+      heading: {
+        strong: "The #1 global platform",
+        text: "on plastic pollution decisions.",
+      },
+      text:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas alias, corporis amet officiis, ratione consectetur, quia hic repudiandae temporibus repellendus similique at eveniet qui obcaecati iusto quam facilis ut fuga.",
+    },
+  ],
+};
