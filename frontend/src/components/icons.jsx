914489586a9bfcd3fb4b7f92321b5bd463bc438f
ifirmawan <firmawaneiwan@gmail.com>
2023-08-24 12:06:47 +0700

    [#1547] Revert to redesign branch

--- frontend/src/components/icons.jsx
+++ frontend/src/components/icons.jsx
@@ -1,11 +1,10 @@
-export const CirclePointer = ({ rotate = 0 }) => (
+export const CirclePointer = () => (
   <svg
     width="32"
     height="32"
     viewBox="0 0 32 32"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    transform={`rotate(${rotate})`}
   >
     <circle
       cx={16}
