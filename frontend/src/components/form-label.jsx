479cb6380e9fcedb5e5bae32e518fc88e812e14e
ifirmawan <firmawaneiwan@gmail.com>
2023-09-05 17:02:49 +0700

    [#1584] Refactoring FormLabel component to add optional text

--- frontend/src/components/form-label.jsx
+++ frontend/src/components/form-label.jsx
@@ -1,50 +1,61 @@
 import React from 'react'
-import classNames from 'classnames'
+import { Form } from 'antd'
 import { FieldErrorIcon, FieldSuccessIcon } from './icons'
 
-const ErrorSign = () => {
+const Feedback = ({ status }) => {
+  const items = {
+    success: {
+      text: 'Success',
+      icon: <FieldSuccessIcon />,
+    },
+    error: {
+      text: 'Error',
+      icon: <FieldErrorIcon />,
+    },
+  }
+  const statusItem = items?.[status] || items.success
   return (
     <span className="feedback-wrapper">
-      <span className="feedback-text error">Error</span>
-      <span>
-        <FieldErrorIcon />
-      </span>
-    </span>
-  )
-}
-
-const SuccessSign = () => {
-  return (
-    <span className="feedback-wrapper">
-      <span className="feedback-text success">Success</span>
-      <span>
-        <FieldSuccessIcon />
-      </span>
+      <span className={`feedback-text ${status}`}>{statusItem.text}</span>
+      <span>{statusItem.icon}</span>
     </span>
   )
 }
 
 const FormLabel = ({
   children,
-  className = null,
-  status = null,
   meta,
+  label,
+  validateStatus = null,
+  className = null,
+  isOptional = false,
   ...props
 }) => {
   const statusText =
-    meta.touched && meta.error
+    meta?.touched && meta?.error
       ? 'error'
-      : meta.touched && !meta.error
+      : meta?.touched && !meta?.error && !isOptional
       ? 'success'
-      : status
+      : null
+  const status = validateStatus || statusText
   return (
-    <div class={classNames('ant-col ant-form-item-label', className)}>
-      <label {...props}>{children}</label>
-      <span>
-        {statusText === 'error' && <ErrorSign />}
-        {statusText === 'success' && <SuccessSign />}
-      </span>
-    </div>
+    <Form.Item
+      className={className}
+      label={
+        <span className="hasFeedback">
+          <span>{label}</span>
+          {isOptional && !meta.error && (
+            <span className="feedback-wrapper">
+              <span className="optional">(Optional)</span>
+            </span>
+          )}
+          {status && <Feedback status={status} />}
+        </span>
+      }
+      {...props}
+    >
+      {children}
+    </Form.Item>
   )
 }
 
