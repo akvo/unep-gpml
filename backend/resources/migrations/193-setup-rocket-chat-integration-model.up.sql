d97ac544f124ad572e420064d61e075c0fed5c01
Lucas Sousa <lucas.sousa@magnet.coop>
2023-09-05 11:17:39 +0200

    Migration: setup rocket chat integration model
    [Re #1590]
    
    * We'll only store the chat user ID and account status in the case of
    users. The rest of the operation will be done against RocketChat API.
    * We'll store a list of channels selected by administrators, which is
    like a list of curated channels to showcase.

---
+++ backend/resources/migrations/193-setup-rocket-chat-integration-model.up.sql
@@ -0,0 +1,17 @@
+BEGIN;
+--;;
+CREATE TYPE USER_CHAT_ACCOUNT_STATUS AS ENUM (
+    'pending-activation',
+    'active',
+    'inactive'
+);
+--;;
+ALTER TABLE stakeholder
+    ADD COLUMN chat_account_id TEXT UNIQUE,
+    ADD COLUMN chat_account_status USER_CHAT_ACCOUNT_STATUS DEFAULT 'pending-activation';
+--;;
+CREATE TABLE chat_curated_channel (
+    id TEXT PRIMARY KEY
+);
+--;;
+COMMIT;
