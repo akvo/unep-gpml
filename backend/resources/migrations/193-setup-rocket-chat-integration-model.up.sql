9cd198cc21e85f60f5b12b815f9fd59bef0c7f9a
Lucas Sousa <lucas.sousa@magnet.coop>
2023-09-14 17:08:43 +0200

    Remove default setting on chat_account_status column
    [Re #1590]
    
    * If users don't opt-in to the chat functionality their
    chat_account_status will be NULL. Otherwise it will be on
    "pending-activation" status.

--- backend/resources/migrations/193-setup-rocket-chat-integration-model.up.sql
+++ backend/resources/migrations/193-setup-rocket-chat-integration-model.up.sql
@@ -8,7 +8,7 @@ CREATE TYPE USER_CHAT_ACCOUNT_STATUS AS ENUM (
 --;;
 ALTER TABLE stakeholder
     ADD COLUMN chat_account_id TEXT UNIQUE,
-    ADD COLUMN chat_account_status USER_CHAT_ACCOUNT_STATUS DEFAULT 'pending-activation';
+    ADD COLUMN chat_account_status USER_CHAT_ACCOUNT_STATUS;
 --;;
 CREATE TABLE chat_curated_channel (
     id TEXT PRIMARY KEY
