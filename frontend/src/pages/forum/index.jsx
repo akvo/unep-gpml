abe6f4f9ad40e5473e903b47ce733a500a11b609
ifirmawan <firmawaneiwan@gmail.com>
2023-09-26 16:31:55 +0700

    [#1590] Export My forums section as a component

--- frontend/src/pages/forum/index.jsx
+++ frontend/src/pages/forum/index.jsx
@@ -1,14 +1,13 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import styles from './index.module.scss'
+import React, { useState } from 'react'
+import dynamic from 'next/dynamic'
 import { Card, List } from 'antd'
-import moment from 'moment'
+import styles from './index.module.scss'
 import Button from '../../components/button'
-import api from '../../utils/api'
+import { UIStore } from '../../store'
 
 const Forum = () => {
-  const [myForums, setMyForums] = useState([])
-
-  const data = [
+  // TODO
+  const allDummies = [
     {
       title: 'Issue Briefs',
       isPrivate: false,
@@ -39,98 +38,19 @@ const Forum = () => {
       isPrivate: true,
     },
   ]
+  const [allForums, setAllForums] = useState(allDummies)
+  const profile = UIStore.useState((s) => s.profile)
 
-  const getMyForums = useCallback(async () => {
-    // const res = await api.get('/chat/user/channel')
-    // console.log('responsee', res)
-    const dummy = [
-      {
-        title: 'Issue Briefs',
-        isPrivate: false,
-        newMessages: 11,
-        lastModifiedAt: '2023-09-25T14:48:00',
-      },
-      {
-        title: 'AFRIPAC',
-        isPrivate: true,
-        newMessages: 0,
-        lastModifiedAt: '2023-09-11T09:48:00',
-      },
-      {
-        title: 'Plastic Strategy South Africa',
-        isPrivate: true,
-        newMessages: 5,
-        lastModifiedAt: '2023-09-26T09:00:00',
-      },
-    ]
-    setMyForums(dummy)
-  }, [])
+  const DynamicMyForums = dynamic(() => import('./my-forums'), {
+    ssr: false,
+  })
 
-  useEffect(() => {
-    getMyForums()
-  }, [getMyForums])
   return (
     <div className="container">
       <div className={styles.forumHome}>
-        <div className="header my-forums">
-          <div>
-            <span className="h-xs">Forums</span>
-          </div>
-          <div className="jumbotron">
-            <h2>My Forums</h2>
-            <p className="h-xs">
-              Engage in forums across a wide variety of subjects and sectors
-              currently ongoing. Join the public channels or request to join the
-              private channels.
-            </p>
-          </div>
-        </div>
-        <section className="my-forums">
-          <List
-            grid={{ column: 3 }}
-            dataSource={myForums}
-            renderItem={(item) => (
-              <List.Item>
-                <Card>
-                  <div className="flex my-forums">
-                    <div className="channel my-forums">
-                      <span className="h-xs">
-                        {item.isPrivate ? 'private' : 'public'}
-                      </span>
-                      <h5>{item.title}</h5>
-                    </div>
-                    <div className="new-messages">
-                      {item.newMessages ? (
-                        <>
-                          <small className="label">New Messages</small>
-                          <span className="h-m value">{item.newMessages}</span>
-                        </>
-                      ) : (
-                        <small className="label empty">No New messages</small>
-                      )}
-                    </div>
-                  </div>
-                  <div className="flex">
-                    <div className="last-message">
-                      <span className="label">Last message</span>
-                      <p className="p-m value" suppressHydrationWarning>
-                        {moment(
-                          item.lastModifiedAt,
-                          'YYYY-MM-DD HH:mm:dd'
-                        ).fromNow()}
-                      </p>
-                    </div>
-                    <div>
-                      <Button size="small" withArrow="link" ghost>
-                        Chat
-                      </Button>
-                    </div>
-                  </div>
-                </Card>
-              </List.Item>
-            )}
-          />
-        </section>
+        <span className="h-xs title">Forums</span>
+        {profile?.id && <DynamicMyForums />}
+
         <div className="header">
           <div className="jumbotron">
             <h2>All Forums</h2>
@@ -144,7 +64,7 @@ const Forum = () => {
         <section>
           <List
             grid={{ column: 3 }}
-            dataSource={data}
+            dataSource={allForums}
             renderItem={(item) => (
               <List.Item>
                 <Card>
