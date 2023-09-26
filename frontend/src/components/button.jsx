935bfdd4b56bc203c7eac8368e7868f9ce392dff
navins94 <nikshingote@gmail.com>
2023-09-26 10:21:53 +0530

    Update onboarding page style

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -1,16 +1,26 @@
 import { Button as AntButton } from 'antd'
-import { CirclePointer, ArrowRight, LongArrowRight } from './icons'
+import { CirclePointer, ArrowRight } from './icons'
 import classNames from 'classnames'
 
-const Button = ({ withArrow, children, className, ...props }) => {
+const Button = ({ withArrow, back, children, className, ...props }) => {
   let arrowIcon = null
-  if (withArrow) {
+  let backIcon = null
+  if (withArrow && !back) {
     if (props.type === 'primary') arrowIcon = <CirclePointer />
     else if (props.type === 'link') arrowIcon = <ArrowRight />
-    else arrowIcon = <LongArrowRight />
+    else arrowIcon = withArrow
+  }
+  if (back) {
+    if (props.type === 'primary') backIcon = <CirclePointer />
+    else if (props.type === 'link') backIcon = <ArrowRight />
+    else backIcon = withArrow
   }
   return (
-    <AntButton {...props} className={classNames(className, { withArrow })}>
+    <AntButton
+      {...props}
+      className={classNames(className, { withArrow, back })}
+    >
+      {backIcon}
       {children}
       {withArrow && ' '}
       {arrowIcon}
