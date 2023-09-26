7e1f15e40bb780370452221ec2e2bccd038bd00b
navins94 <nikshingote@gmail.com>
2023-09-26 11:19:44 +0530

    Update entity form

--- frontend/src/utils/forms/form-select.js
+++ frontend/src/utils/forms/form-select.js
@@ -1,47 +1,48 @@
 /* eslint-disable no-else-return */
-import React, { useState } from "react";
-import { utils } from "@rjsf/core";
-import { Select, Divider, Input, List } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+import React, { useState } from 'react'
+import { utils } from '@rjsf/core'
+import { Select, Divider, Input, List } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
 
-import ModalAddEntity from "../../modules/flexible-forms/entity-modal/add-entity-modal";
+import ModalAddEntity from '../../modules/flexible-forms/entity-modal/add-entity-modal'
+import { SearchIcon } from '../../components/icons'
 
-const { asNumber, guessType } = utils;
+const { asNumber, guessType } = utils
 
 const SELECT_STYLE = {
-  width: "100%",
-};
+  width: '100%',
+}
 
-const nums = new Set(["number", "integer"]);
+const nums = new Set(['number', 'integer'])
 
 const processValue = (schema, value) => {
-  const { type, items } = schema;
+  const { type, items } = schema
 
-  if (value === "") {
-    return undefined;
-  } else if (type === "array" && items && nums.has(items.type)) {
-    return value.map(asNumber);
-  } else if (type === "boolean") {
-    return value === "true";
-  } else if (type === "number") {
-    return asNumber(value);
+  if (value === '') {
+    return undefined
+  } else if (type === 'array' && items && nums.has(items.type)) {
+    return value.map(asNumber)
+  } else if (type === 'boolean') {
+    return value === 'true'
+  } else if (type === 'number') {
+    return asNumber(value)
   }
   // If type is undefined, but an enum is present, try and infer the type from
   // the enum values
   if (schema.enum) {
-    if (schema.enum.every((x) => guessType(x) === "number")) {
-      return asNumber(value);
-    } else if (schema.enum.every((x) => guessType(x) === "boolean")) {
-      return value === "true";
+    if (schema.enum.every((x) => guessType(x) === 'number')) {
+      return asNumber(value)
+    } else if (schema.enum.every((x) => guessType(x) === 'boolean')) {
+      return value === 'true'
     }
   }
-  return value;
-};
+  return value
+}
 
 function ciEquals(a, b) {
-  return typeof a === "string" && typeof b === "string"
-    ? a.localeCompare(b, undefined, { sensitivity: "accent" }) === 0
-    : a === b;
+  return typeof a === 'string' && typeof b === 'string'
+    ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0
+    : a === b
 }
 
 const SelectWidget = ({
@@ -62,35 +63,38 @@ const SelectWidget = ({
   uiSchema,
   value,
 }) => {
-  const { readonlyAsDisabled = true } = formContext;
-  const { enumOptions, enumDisabled } = options;
-  const handleChange = (nextValue) => onChange(processValue(schema, nextValue));
-  const handleBlur = () => onBlur(id, processValue(schema, value));
-  const handleFocus = () => onFocus(id, processValue(schema, value));
-  const getPopupContainer = (node) => node.parentNode;
+  const { readonlyAsDisabled = true } = formContext
+  const { enumOptions, enumDisabled } = options
+  const handleChange = (nextValue) => onChange(processValue(schema, nextValue))
+  const handleBlur = () => onBlur(id, processValue(schema, value))
+  const handleFocus = () => onFocus(id, processValue(schema, value))
+  const getPopupContainer = (node) => node.parentNode
   const stringify = (currentValue) =>
-    Array.isArray(currentValue) ? value.map(String) : String(value);
+    Array.isArray(currentValue) ? value.map(String) : String(value)
 
-  const showEntity = uiSchema?.["ui:options"]?.["showEntity"];
-  const showTag = uiSchema?.["ui:options"]?.["ui:showTag"];
+  const showEntity = uiSchema?.['ui:options']?.['showEntity']
+  const showTag = uiSchema?.['ui:options']?.['ui:showTag']
+  const size = uiSchema?.['ui:options']?.['size']
+  const icon = uiSchema?.['ui:options']?.['icon']
 
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false)
   const [tagMode, setTagMode] = useState(
-    uiSchema?.["ui:mode"] !== undefined ? uiSchema["ui:mode"] : ""
-  );
+    uiSchema?.['ui:mode'] !== undefined ? uiSchema['ui:mode'] : ''
+  )
 
   const setOrg = (res) => {
-    if (res) handleChange(res.id);
-  };
+    if (res) handleChange(res.id)
+  }
 
   return (
     <>
       <Select
-        allowClear={uiSchema?.["ui:allowClear"] ? true : false}
-        showSearch={uiSchema?.["ui:showSearch"] ? true : false}
+        size={size ? 'small' : ''}
+        allowClear={uiSchema?.['ui:allowClear'] ? true : false}
+        showSearch={uiSchema?.['ui:showSearch'] ? true : false}
         filterOption={(input, option) =>
           option?.children?.toLowerCase().indexOf(input.toLowerCase()) === 0 ||
-          option.value === "-1"
+          option.value === '-1'
         }
         autoFocus={autofocus}
         disabled={disabled || (readonlyAsDisabled && readonly)}
@@ -101,33 +105,35 @@ const SelectWidget = ({
         onBlur={!readonly ? handleBlur : undefined}
         onChange={!readonly ? handleChange : undefined}
         onFocus={!readonly ? handleFocus : undefined}
+        showArrow={icon ? true : false}
+        suffixIcon={<SearchIcon />}
         onSearch={(v) => {
-          if (uiSchema?.["ui:options"]?.["type"] === "single") {
-            return false;
+          if (uiSchema?.['ui:options']?.['type'] === 'single') {
+            return false
           }
           const find = enumOptions.find(
             (tag) => v.toLowerCase() == tag.label.toLowerCase()
-          );
+          )
           if (find) {
-            setTagMode("multiple");
-          } else if (uiSchema?.["ui:options"]?.["type"] === "connection") {
-            setTagMode("multiple");
+            setTagMode('multiple')
+          } else if (uiSchema?.['ui:options']?.['type'] === 'connection') {
+            setTagMode('multiple')
           } else {
-            setTagMode("tags");
+            setTagMode('tags')
           }
         }}
         placeholder={placeholder}
         style={SELECT_STYLE}
-        value={typeof value !== "undefined" ? stringify(value) : undefined}
+        value={typeof value !== 'undefined' ? stringify(value) : undefined}
         virtual={false}
         dropdownRender={(menu) => (
           <div>
             {menu}
             {showEntity && (
               <>
-                <Divider style={{ margin: "4px 0" }} />
+                <Divider style={{ margin: '4px 0' }} />
                 <div
-                  style={{ display: "flex", flexWrap: "nowrap", padding: 8 }}
+                  style={{ display: 'flex', flexWrap: 'nowrap', padding: 8 }}
                 >
                   <a onClick={() => setShowModal(!showModal)}>
                     <PlusOutlined /> Add new entity
@@ -146,7 +152,7 @@ const SelectWidget = ({
               }
               key={String(optionValue) + i.toString(36)}
               value={
-                schema?.title === "Tags"
+                schema?.title === 'Tags'
                   ? String(optionLabel)
                   : String(optionValue)
               }
@@ -184,11 +190,11 @@ const SelectWidget = ({
         setEntity={setOrg}
       />
     </>
-  );
-};
+  )
+}
 
 SelectWidget.defaultProps = {
   formContext: {},
-};
+}
 
-export default SelectWidget;
+export default SelectWidget
