612d7d76c43597bc18b665c0b5ce486d479610b1
Lucas Sousa <lucas.sousa@magnet.coop>
2023-10-02 10:59:44 +0200

    Add GPML users info when getting all channels
    [Re #1590]
    
    * The org expansion is also needed for this so I added a
    related-entities key configuration option to get-stakeholders. It's a
    set of keywords containing for entities that should be included in the
    query's result.

--- backend/src/gpml/db/stakeholder.sql
+++ backend/src/gpml/db/stakeholder.sql
@@ -317,16 +317,21 @@ VALUES :t*:values RETURNING id, email;
 -- :name get-stakeholders :query :many
 -- :doc Get stakeholders with filters
 SELECT s.*, json_agg(json_build_object('id', t.id, 'tag', t.tag, 'tag_relation_category', st.tag_relation_category, 'tag_category', tg.category)) FILTER (WHERE t.id IS NOT NULL) AS tags
+--~(when (get (:related-entities params) :organisation) ", row_to_json(o.*) AS org")
 FROM stakeholder s
 LEFT JOIN stakeholder_tag st ON st.stakeholder = s.id
 LEFT JOIN tag t ON st.tag = t.id
 LEFT JOIN tag_category tg ON t.tag_category = tg.id
+--~(when (get (:related-entities params) :organisation) " LEFT JOIN organisation o ON s.affiliation = o.id")
 WHERE 1=1
 --~(when (seq (get-in params [:filters :review-statuses])) " AND s.review_status = ANY(ARRAY[:v*:filters.review-statuses]::review_status[])")
 --~(when (seq (get-in params [:filters :ids])) " AND s.id IN (:v*:filters.ids)")
 --~(when (seq (get-in params [:filters :roles])) " AND s.role = ANY(ARRAY[:v*:filters.roles]::stakeholder_role[])")
 --~(when (seq (get-in params [:filters :search-text])) " AND (LOWER(s.first_name) ILIKE '%' || :filters.search-text || '%' OR LOWER(s.last_name) ILIKE '%' || :filters.search-text || '%' OR LOWER(s.email) ILIKE '%' || :filters.search-text || '%')")
+--~(when (seq (get-in params [:filters :chat-accounts-ids])) " AND s.chat_account_id IN (:v*:filters.chat-accounts-ids)")
 GROUP BY s.id
+--~(when (get (:related-entities params) :organisation) ", o.id")
+;
 
 -- :name delete-stakeholder* :execute :affected
 DELETE FROM stakeholder
