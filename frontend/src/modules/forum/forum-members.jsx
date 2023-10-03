48e316f806d25b2cafa940070741cd3e9222f4b5
ifirmawan <firmawaneiwan@gmail.com>
2023-10-03 11:25:17 +0700

    [#1590] FE: Replace members w/ users & remove async function

--- frontend/src/modules/forum/forum-members.jsx
+++ frontend/src/modules/forum/forum-members.jsx
@@ -1,55 +1,18 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { Avatar, Spin } from 'antd'
+import React from 'react'
+import { Avatar } from 'antd'
 
-const ForumMembers = ({ allForums, setAllForums, initName, forum }) => {
-  const [preload, setPreload] = useState(true)
-  const [members, setMembers] = useState([])
-  const { usersCount, avatarUrl, id: forumId } = forum || {}
-
-  const getAllMembers = useCallback(async () => {
-    const _members = await new Promise((resolve, _) => {
-      setTimeout(() => {
-        const _fakeMembers = Array.from({ length: usersCount }).map(
-          (_, ix) => ({
-            id: ix,
-            name: `Marie-Celine Marechal ${ix + 1}`,
-            image: ix % 2 === 0 ? null : avatarUrl,
-            title: 'UNEP France',
-          })
-        )
-        resolve(_fakeMembers)
-      }, 2000)
-    })
-    setMembers(_members)
-    setPreload(false)
-  }, [usersCount, avatarUrl])
-
-  useEffect(() => {
-    const findForum = allForums.find((a) => a.id === forumId)
-    if (!findForum?.members && members.length) {
-      const _all = allForums.map((a) =>
-        a?.id === forumId ? { ...findForum, members } : a
-      )
-      setAllForums(_all)
-    }
-  }, [allForums, members, forumId])
-
-  useEffect(() => {
-    getAllMembers()
-  }, [getAllMembers])
-
+const ForumMembers = ({ initName, forum }) => {
+  const { usersCount, users } = forum || {}
   return (
     <div className="participants">
-      {preload ? (
-        <Spin />
-      ) : (
-        <Avatar.Group>
-          {members.slice(0, 5).map((m, mx) => (
-            <Avatar src={m.image}>{!m.image && initName(m.name)}</Avatar>
-          ))}
-          {usersCount - 5 > 0 && <Avatar>{`+${usersCount - 5}`}</Avatar>}
-        </Avatar.Group>
-      )}
+      <Avatar.Group>
+        {users.slice(0, 5).map((u, index) => (
+          <Avatar src={u.image} key={index} aria-label={u.name}>
+            {!u.image && initName(u.name)}
+          </Avatar>
+        ))}
+        {usersCount - 5 > 0 && <Avatar>{`+${usersCount - 5}`}</Avatar>}
+      </Avatar.Group>
     </div>
   )
 }
