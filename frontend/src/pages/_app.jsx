2455634d5d86242726d2751691d7b28f89742c1d
navins94 <nikshingote@gmail.com>
2023-10-03 20:52:44 +0530

    Update layout logic

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -267,20 +267,8 @@ function MyApp({ Component, pageProps }) {
   )
 
   const Layout = useMemo(() => {
-    if (!newRoutes.some((route) => router.pathname.startsWith(route))) {
-      return withLayout(Component)
-    } else {
-      return withNewLayout(Component)
-    }
-  }, [router.pathname, Component])
-
-  const RenderedLayout = useMemo(() => {
-    if (!newRoutes.some((route) => router.pathname.startsWith(route))) {
-      return <Layout {...pageProps} {...componentProps} />
-    } else {
-      return <Layout {...pageProps} {...componentProps} />
-    }
-  }, [router.pathname, Layout, Component, pageProps, componentProps])
+    return withNewLayout(Component)
+  }, [Component])
 
   return (
     <div id="root">
@@ -299,7 +287,7 @@ function MyApp({ Component, pageProps }) {
           typeof window !== 'undefined' ? window.location.origin : ''
         }
       >
-        {RenderedLayout}
+        <Layout {...pageProps} {...componentProps} />
       </Auth0Provider>
     </div>
   )
