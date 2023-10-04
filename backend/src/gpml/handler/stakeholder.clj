a0e7ae8adb521eb4b047a015c25ee4a7323acc02
Lucas Sousa <lucas.sousa@magnet.coop>
2023-10-04 16:22:10 +0200

    Add missing chat account properties to user profile response

--- backend/src/gpml/handler/stakeholder.clj
+++ backend/src/gpml/handler/stakeholder.clj
@@ -86,7 +86,8 @@
            affiliation job-title
            country geo-coverage-type
            reviewed-at reviewed-by review-status
-           organisation-role public-email tags org]}]
+           organisation-role public-email
+           tags org chat-account-id chat-account-status]}]
   (let [{:keys [seeking offering expertise]} (->> tags
                                                   (group-by :tag_relation_category)
                                                   (reduce-kv (fn [m k v] (assoc m (keyword k) (map :tag v))) {}))]
@@ -115,7 +116,9 @@
      :review_status review-status
      :public_email public-email
      :cv cv
-     :picture picture}))
+     :picture picture
+     :chat_account_id chat-account-id
+     :chat_account_status chat-account-status}))
 
 (defn- get-stakeholder-profile
   [config stakeholder-id]
