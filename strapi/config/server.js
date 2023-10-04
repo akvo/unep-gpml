461194ee9f3c646b0d297606d6832c009c7706f7
Anjar Fiandrianto <anjar@akvo.org>
2023-10-04 20:58:52 +0700

    [#1623] Fix Strapi URL variable

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL')
+  url: env('URL'),
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
