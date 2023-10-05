0ac749b157b71e4cac27778e185ac15a1a94d2c8
Anjar Fiandrianto <anjar@akvo.org>
2023-10-05 13:53:19 +0700

    [#1623] Add Strapi TinyMCE Plugin + Fix Public URL

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://192.168.55.136/strapi',
+  url: 'http://localhost/strapi',
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
