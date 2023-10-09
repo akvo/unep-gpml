1ca9a3ad40a1c1d52b97bc0bcd13ddeca9e4672a
ifirmawan <firmawaneiwan@gmail.com>
2023-10-09 20:33:37 +0700

    PS: Add isoA2 config

---
+++ frontend/src/modules/workspace/ps/config.js
@@ -0,0 +1,12 @@
+export const isoA2 = {
+  mauritius: 'MU',
+  peru: 'PE',
+  cambodia: 'KH',
+  'solomon-islands': 'SB',
+  ecuador: 'EC',
+  senegal: 'SN',
+  'south-africa': 'ZA',
+  'cote-d-ivoire': 'CI',
+}
+
+export const PREFIX_SLUG = 'plastic-strategy'
