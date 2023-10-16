87b6bda7d4a5cc38749f47bafbb7931b90995846
Martin Christov <martin.christov@gmail.com>
2023-10-16 17:55:21 +0300

    add a sample slides iframe

--- frontend/src/pages/workspace/[slug]/1-project-team/index.js
+++ frontend/src/pages/workspace/[slug]/1-project-team/index.js
@@ -6,6 +6,16 @@ const View = () => (
       National steering committee & Project Team
     </h4>
     <h2 className="h-xxl w-bold">Introduction</h2>
+    <iframe
+      src="https://docs.google.com/presentation/d/e/2PACX-1vQNMz6tpUDzSsoriaREwket_z3Ti27CVMXy3ewl7sNziRm51AtnwF0n-3isgH6AiB4yIiX2s4JCy-hS/embed?start=false&loop=false&delayms=60000"
+      frameborder="0"
+      width="900"
+      height="542"
+      allowfullscreen="true"
+      mozallowfullscreen="true"
+      webkitallowfullscreen="true"
+      style={{ marginTop: 30 }}
+    ></iframe>
   </>
 )
 
