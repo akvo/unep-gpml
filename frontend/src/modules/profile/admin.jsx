07590f62efbaddf354cfe7b85bc348d294083ac6
navins94 <nikshingote@gmail.com>
2023-10-26 20:20:50 +0530

    Change from type to topic

--- frontend/src/modules/profile/admin.jsx
+++ frontend/src/modules/profile/admin.jsx
@@ -1018,7 +1018,7 @@ const AdminSection = ({
                                 find ? false : true,
                                 listOpts,
                                 setListOpts,
-                                item.type
+                                item.topic
                               )
                             }
                           >
