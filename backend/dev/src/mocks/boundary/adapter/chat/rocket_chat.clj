d08ffeb521254ca790c979233b14f531642d968e
Lucas Sousa <lucas.sousa@magnet.coop>
2023-11-13 18:11:17 +0100

    Add mock RocketChat adapter for unit tests

---
+++ backend/dev/src/mocks/boundary/adapter/chat/rocket_chat.clj
@@ -0,0 +1,19 @@
+(ns mocks.boundary.adapter.chat.rocket-chat
+  (:require [gpml.boundary.port.chat :as port]
+            [gpml.util :as util]
+            [integrant.core :as ig]))
+
+(defn- create-user-account
+  []
+  {:success? true
+   :user {:id (str (util/uuid))
+          :active true}})
+
+(defrecord MockRocketChat []
+  port/Chat
+  (create-user-account [_ _]
+    (create-user-account)))
+
+(defmethod ig/init-key :mocks.boundary.adapter.chat/rocket-chat
+  [_ _]
+  (->MockRocketChat))
