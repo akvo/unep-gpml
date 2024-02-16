272debc1ab51d6035cf47444a925c55f0521a888
vemv <vemv@users.noreply.github.com>
2024-02-16 09:40:14 +0100

    Don't use Makefile in CI for now

--- backend/release.sh
+++ backend/release.sh
@@ -14,7 +14,15 @@ if [[ -s /tmp/duplicated  ]]; then
   exit 1
 fi
 
-make lint test uberjar
+# `make` isn't available in the Docker images atm - disable for now:
+# make lint test uberjar
+
+lein with-profile -dev,+test,+seeder,+clj-kondo clj-kondo --copy-configs --dependencies --parallel --lint '$classpath'
+lein with-profile -dev,+test,+seeder,+clj-kondo clj-kondo
+lein with-profile -user,-dev,+test,+seeder,+eastwood eastwood
+lein with-profile -user,-dev,+test,+seeder,+eftest eftest
+lein clean
+lein with-profile uberjar uberjar
 
 jar tf target/uberjar/app.jar | grep --silent duct_hierarchy.edn || exit 1
 jar tf target/uberjar/app.jar | grep --silent migrations/203-add-missing-on-delete-cascade-constraints.up.sql || exit 1
