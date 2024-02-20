395876d01bf9e8c7045a021955b79a1b4131cb8b
vemv <vemv@users.noreply.github.com>
2024-02-20 11:34:01 +0100

    Use clearer `--no-TTY` option

--- db/script/dump-db.sh
+++ db/script/dump-db.sh
@@ -3,4 +3,4 @@
 
 set -eu
 
-docker-compose exec -T db bash -c 'pg_dump --user unep --clean --create --format plain gpml > /docker-entrypoint-initdb.d/001-init.sql; echo "Export done"'
+docker-compose exec --no-TTY db bash -c 'pg_dump --user unep --clean --create --format plain gpml > /docker-entrypoint-initdb.d/001-init.sql; echo "Export done"'
