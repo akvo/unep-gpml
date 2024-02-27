144e0f25b04ee31f2c456b1924d92fa7758468e6
vemv <vemv@users.noreply.github.com>
2024-02-27 13:43:12 +0100

    Use `-T` again

--- ci/build.sh
+++ ci/build.sh
@@ -107,7 +107,7 @@ frontend_build_staging
 strapi_build
 nginx_build
 
-if ! dci run --no-TTY ci ./basic.sh; then
+if ! dci run -T ci ./basic.sh; then
   echo "Build failed when running basic.sh. Logs:"
   dci logs
   exit 1
