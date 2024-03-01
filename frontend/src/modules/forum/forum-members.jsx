7c7ae317bc81c041cc4c93a1ea04c2df4646516a
ifirmawan <hellofirmawan@gmail.com>
2024-03-01 21:51:32 +0700

    [#1780] Fix forum members when migrating to DSC

--- frontend/src/modules/forum/forum-members.jsx
+++ frontend/src/modules/forum/forum-members.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Avatar } from 'antd'
 import styles from './forum-members.module.scss'
+import api from '../../utils/api'
+import { ChatStore, UIStore } from '../../store'
 
 const ForumMembers = ({ forum }) => {
-  const { usersCount, users } = forum || {}
+  const [users, setUsers] = useState(forum?.users || [])
+  const [preload, setPreload] = useState(true)
+  const usersCount = forum?.usersCount || users?.length
+  const profile = UIStore.useState((s) => s.profile)
 
-  const avatarUrl = `${process.env.NEXT_PUBLIC_CHAT_API_DOMAIN_URL}/avatar/`
+  const fetchData = useCallback(async () => {
+    try {
+      if (preload && profile?.id && !forum?.users && forum?.id) {
+        setPreload(false)
+        const { data: apiData } = await api.get(
+          `/chat/channel/details/${forum.id}`
+        )
+        const { channel } = apiData || {}
+        setUsers(channel?.users)
+        ChatStore.update((s) => {
+          s.allForums = s.allForums.map((a) =>
+            a?.id === forum.id ? { ...a, users: channel?.users } : a
+          )
+        })
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }, [preload, profile, forum?.users])
+
+  /**
+   * Disabled atm, due performance issue
+   */
+  // useEffect(() => {
+  //   fetchData()
+  // }, [fetchData])
+
   return (
     <div className={styles.membersView}>
       <Avatar.Group>
-        {users.slice(0, 5).map((u, index) => {
-          const userImage = u?.avatarETag
-            ? `${avatarUrl}${u?.username}?etag=${u.avatarETag}`
-            : null
-          const userName = u?.name || u?.username || ''
-          const [fistName, lastName] = userName.split(/[ ,]+/)
+        {users?.slice(0, 5)?.map((u, index) => {
+          const fullName = `${u?.firstName} ${u?.lastName || ''}`
           return (
-            <Avatar src={userImage} key={index} aria-label={u.name}>
-              {`${fistName[0]}${lastName?.[0] || ''}`}
+            <Avatar src={u?.picture} key={index} aria-label={fullName}>
+              {fullName}
             </Avatar>
           )
         })}
