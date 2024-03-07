7ebaf710347141b35a5d69fdbed8646faae98e52
vemv <vemv@users.noreply.github.com>
2024-03-07 13:13:49 +0100

    `db/execute!` add logging, Result interface

--- backend/src/gpml/db.clj
+++ backend/src/gpml/db.clj
@@ -1,6 +1,7 @@
 (ns gpml.db
   (:require
    [clojure.java.jdbc :as jdbc]
+   [gpml.util.malli :refer [check! failure-with success-with]]
    [honey.sql]
    [integrant.core :as ig]
    [next.jdbc]
@@ -17,18 +18,44 @@
   (result-set-read-column [pgobj _metadata _i]
     (vec (.getArray pgobj))))
 
+(def Result
+  [:or
+   (success-with :result any?)
+   (failure-with :error-details [:map [:ex-class :string]])])
+
+;; XXX logger arg
 (defn execute! [hikari honey-data]
-  (let [formatted (honey.sql/format honey-data)]
-    (when *assert*
-      (timbre/info formatted))
-    (next.jdbc/execute! (-> hikari :spec :datasource)
-                        formatted
-                        {:builder-fn next.jdbc.result-set/as-unqualified-kebab-maps})))
+  {:post [(check! Result %)]}
+  (try
+    (let [formatted (honey.sql/format honey-data)
+          v (next.jdbc/execute! (-> hikari :spec :datasource)
+                                formatted
+                                {:builder-fn next.jdbc.result-set/as-unqualified-kebab-maps})]
+      (timbre/with-context+ {:sql-result v}
+        (when *assert*
+          (timbre/info formatted)))
+      {:success? true
+       :result v})
+    (catch Exception e
+      (timbre/error e)
+      {:success? false
+       :reason :exception
+       :error-details {:ex-class (-> e class str)}})))
 
 (defn execute-one! [hikari honey-data]
-  (let [formatted (honey.sql/format honey-data)]
-    (when *assert*
-      (timbre/info formatted))
-    (next.jdbc/execute-one! (-> hikari :spec :datasource)
-                            formatted
-                            {:builder-fn next.jdbc.result-set/as-unqualified-kebab-maps})))
+  {:post [(check! Result %)]}
+  (try
+    (let [formatted (honey.sql/format honey-data)
+          v (next.jdbc/execute-one! (-> hikari :spec :datasource)
+                                    formatted
+                                    {:builder-fn next.jdbc.result-set/as-unqualified-kebab-maps})]
+      (when *assert*
+        (timbre/with-context+ {:sql-result v}
+          (timbre/info formatted)))
+      {:success? true
+       :result v})
+    (catch Exception e
+      (timbre/error e)
+      {:success? false
+       :reason :exception
+       :error-details {:ex-class (-> e class str)}})))
