f15db9b2eafb8ca7071ebc19b51d793554b4492d
vemv <vemv@users.noreply.github.com>
2024-03-09 20:44:47 +0100

    Introduce and satisfy nvd-clojure
    Closes https://github.com/akvo/unep-gpml/issues/1777

---
+++ backend/.nvd/project.clj
@@ -0,0 +1,5 @@
+(defproject nvd-helper "local"
+  :description "nvd-clojure helper project"
+  :dependencies [[nvd-clojure "4.0.0"]
+                 [org.clojure/clojure "1.11.1"]]
+  :jvm-opts ["-Dclojure.main.report=stderr"])
