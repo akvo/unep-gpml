c6f645974adf27c0a390dd1b259685e414c51ad8
vemv <vemv@users.noreply.github.com>
2024-03-09 21:02:44 +0100

    Don't run nvd-clojure in CI for now

--- backend/release.sh
+++ backend/release.sh
@@ -17,10 +17,16 @@ fi
 # `make` isn't available in the Docker images atm - disable for now:
 # make lint test uberjar
 
-classpath=$(lein with-profile -user,-dev classpath)
-cd .nvd
-lein with-profile -user,-dev run -m nvd.task.check "nvd.edn" $classpath
-cd -
+# Security check using nvd-clojure.
+# Currently cannot be run in Semaphore since it hits:
+#     Error retrieving https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json
+# ...which probably means a problem on Semaphore's end.
+
+# classpath=$(lein with-profile -user,-dev classpath)
+# cd .nvd
+# lein with-profile -user,-dev run -m nvd.task.check "nvd.edn" $classpath
+# cd -
+
 lein with-profile -dev,+test,+seeder,+clj-kondo clj-kondo
 lein with-profile -user,-dev,+test,+seeder,+eastwood eastwood
 lein with-profile -user,-dev,+test,+seeder,+eftest eftest
