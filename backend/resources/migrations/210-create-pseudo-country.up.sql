abab31c83b9f2064db6e96b2b9cffa223bb46503
vemv <vemv@users.noreply.github.com>
2024-03-21 14:57:44 +0100

    Adapt test suite to pseudo country

--- backend/resources/migrations/210-create-pseudo-country.up.sql
+++ backend/resources/migrations/210-create-pseudo-country.up.sql
@@ -1,2 +1,2 @@
-INSERT INTO country (name, iso_code_a3, territory, description, headquarter, iso_code_a2)
-VALUES ('Country A', '00A', '00A', 'Country A', 'Country A', '0A');
+INSERT INTO country (id, name, iso_code_a3, territory, description, headquarter, iso_code_a2)
+VALUES (9999, 'Country A', '00A', '00A', 'Country A', 'Country A', '0A');
