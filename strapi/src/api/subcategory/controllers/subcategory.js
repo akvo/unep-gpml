bf5ddeef0e51acef5df045f4c148e57a3f3a2222
DHI\jlaz <l.jasmiina@gmail.com>
2024-03-26 11:35:08 +0100

    Add models for the Datahub

---
+++ strapi/src/api/subcategory/controllers/subcategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * subcategory controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::subcategory.subcategory');
