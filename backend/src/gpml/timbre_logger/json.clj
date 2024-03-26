ab2bb2bc165e4f0ca76cb0d0590e3feb47b43c3c
vemv <vemv@users.noreply.github.com>
2024-03-26 21:39:39 +0100

    Structured logging: configure a `:json-error-fn`

--- backend/src/gpml/timbre_logger/json.clj
+++ backend/src/gpml/timbre_logger/json.clj
@@ -1,6 +1,7 @@
 (ns gpml.timbre-logger.json
   (:require
    [integrant.core :as ig]
+   [taoensso.timbre :as timbre]
    [timbre-json-appender.core :as timbre-json-appender]))
 
 (def key-banlist
@@ -11,6 +12,11 @@
                                        :pretty pretty
                                        :level-key           :level
                                        :msg-key             :msg
+                                       :json-error-fn (fn [^Throwable e]
+                                                        (try
+                                                          (timbre/error :json-logging-failed e)
+                                                          (catch Exception _ ;; possibly, prevent recursion
+                                                            nil)))
                                        :should-log-field-fn (fn [field-name _data]
                                                               (not (contains? key-banlist field-name))) ;; by default :file/:line are omitted (except for exceptions) - avoid that, while gaining some security
                                        :ex-data-field-fn    timbre-json-appender/default-ex-data-field-fn
