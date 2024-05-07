3f040d2d57b26ddee965dfff433eee7f55e08501
Martin Christov <martin.christov@gmail.com>
2024-05-07 17:36:11 +0300

    add modal

--- frontend/src/pages/workspace/plastic-strategies/index.js
+++ frontend/src/pages/workspace/plastic-strategies/index.js
@@ -6,10 +6,13 @@ import SkeletonItems from '../../../modules/workspace/ps/skeleton-items'
 import { PSCard } from '../../../modules/workspace/view'
 import { UIStore } from '../../../store'
 import { loadCatalog } from '../../../translations/utils'
+import Button from '../../../components/button'
+import { Input, Modal, Select } from 'antd'
 
 const View = () => {
   const [psAll, setPSAll] = useState([])
   const [loading, setLoading] = useState(true)
+  const [showAddModal, setShowAddModal] = useState(false)
   const profile = UIStore.useState((s) => s.profile)
   const isAdmin = profile?.role === 'ADMIN'
 
@@ -37,6 +40,9 @@ const View = () => {
           <h2 className="h-xxl w-bold">
             <Trans>National Source Inventories</Trans>
           </h2>
+          <Button type="ghost" onClick={() => setShowAddModal(true)}>
+            + Add New
+          </Button>
         </div>
         <SkeletonItems loading={loading} />
         <ul className="plastic-strategies-items">
@@ -45,7 +51,43 @@ const View = () => {
           ))}
         </ul>
       </div>
+      <AddModal {...{ showAddModal, setShowAddModal }} />
     </div>
+  )
+}
+
+const AddModal = ({ showAddModal, setShowAddModal }) => {
+  const { countries } = UIStore.currentState
+  const [value, setValue] = useState(null)
+  return (
+    <Modal
+      visible={showAddModal}
+      title="Add A National Source Inventory"
+      onCancel={() => setShowAddModal(false)}
+      className={styles.psModal}
+    >
+      <Select
+        value={value}
+        size="small"
+        onChange={(val) => setValue(val)}
+        filterOption={(input, option) =>
+          option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }
+        placeholder="Countries"
+        allowClear
+        showSearch
+      >
+        {countries
+          .filter(
+            (country) => country.description.toLowerCase() === 'member state'
+          )
+          .map((it) => (
+            <Option key={it.id} value={it.id}>
+              {it.name}
+            </Option>
+          ))}
+      </Select>
+    </Modal>
   )
 }
 
