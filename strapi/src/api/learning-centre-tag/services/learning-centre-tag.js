60262d33ff31c87bf08205595c8a0614bb7cc1df
navins94 <nikshingote@gmail.com>
2024-06-21 01:01:52 +0530

    Add strapi for learning center

---
+++ strapi/src/api/learning-centre-tag/services/learning-centre-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * learning-centre-tag service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::learning-centre-tag.learning-centre-tag');
