58b8aaec3dd70c776f25bd6f324e4a096b1a3ff2
navins94 <nikshingote@gmail.com>
2024-07-01 16:02:55 +0530

    Add console for debug

--- frontend/src/pages/knowledge/learning-centre/index.jsx
+++ frontend/src/pages/knowledge/learning-centre/index.jsx
@@ -172,6 +172,7 @@ const LearningCentreCard = ({ data, loading }) => {
 
 export async function getStaticProps(ctx) {
   const strapiURL = getStrapiUrl()
+  console.log(strapiURL,"strapiURL")
 
   const fetchLearningCentres = async () => {
     const response = await axios.get(
