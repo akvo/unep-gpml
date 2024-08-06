4fb27de57263cb883db19c01cd0c528bc46de8e8
Iván Perdomo <ivan@perdomo.me>
2024-08-06 06:51:55 +0200

    Feat: Change logging level
    In production the minimum logging level is `warn`

--- backend/src/gpml/handler/search.clj
+++ backend/src/gpml/handler/search.clj
@@ -56,7 +56,7 @@
         (throw (Exception. "Internal server error")))
       (let [resp (open-api-request logger openapi-api-key (:q query))
             sql (-> resp :body :choices first :message :content str/trim)]
-        (log logger :report :openai-generated-query {:q (:q query) :sql sql :response (:body resp)})
+        (log logger :warn :openai-generated-query {:q (:q query) :sql sql :response (:body resp)})
         (when (not (sql-allowed? sql))
           (log logger :error :invalid-query sql)
           (throw (Exception. "Invalid query")))
