e281631b5dde593f7f445298031a84465a272f39
DHI\jlaz <l.jasmiina@gmail.com>
2024-10-22 12:58:11 +0200

    Sider fix

--- frontend/src/components/map-and-layers/sidebar/partials/categoriesNested.jsx
+++ frontend/src/components/map-and-layers/sidebar/partials/categoriesNested.jsx
@@ -42,7 +42,12 @@ const CategoriesNested = ({ categories }) => {
   }
 
   return (
-    <Sider breakpoint="lg" collapsedWidth="0" width={360}>
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      style={{ overflow: 'auto' }}
+      width={360}
+    >
       <div className="caps-heading-s">Topics</div>
       <Menu defaultSelectedKeys={['1']} style={{ maxHeight: '100%' }}>
         {categories.map((category) => (
