64e82369fc19e9615577334560c414dd66e5e36a
DHI\jlaz <l.jasmiina@gmail.com>
2024-11-13 13:45:57 +0100

    Clean up

--- frontend/src/modules/country-dashboard/index.jsx
+++ frontend/src/modules/country-dashboard/index.jsx
@@ -1,25 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import CountryLib from '../../modules/country-dashboard/view'
-import { UIStore } from '../../store'
-import api from '../../utils/api'
-import { loadCatalog } from '../../translations/utils'
+
 import Head from 'next/head'
 function CountryView({ isAuthenticated }) {
-  const { landing } = UIStore.useState((s) => ({
-    landing: s.landing,
-  }))
-  const fetchMapData = () => {
-    api.get(`/landing?entityGroup=topic`).then((resp) => {
-      UIStore.update((e) => {
-        e.landing = resp.data
-      })
-    })
-  }
-  useEffect(() => {
-    if (typeof window !== 'undefined' && Object.keys(landing).length === 0) {
-      fetchMapData()
-    }
-  }, [])
   return (
     <>
       <Head>
@@ -29,11 +12,5 @@ function CountryView({ isAuthenticated }) {
     </>
   )
 }
-export const getStaticProps = async (ctx) => {
-  return {
-    props: {
-      i18n: await loadCatalog(ctx.locale),
-    },
-  }
-}
+
 export default React.memo(CountryView)
