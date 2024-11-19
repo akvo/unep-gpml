1b15c8fd3b16164d049d34a0e266364bc0fb7610
DHI\jlaz <l.jasmiina@gmail.com>
2024-11-19 12:58:15 +0100

    Refactoring

--- frontend/src/modules/country-dashboard/charts/MSWGeneration.tsx
+++ frontend/src/modules/country-dashboard/charts/MSWGeneration.tsx
@@ -2,24 +2,21 @@ import React, { useEffect, useState } from 'react'
 import ReactEcharts from 'echarts-for-react'
 import { useRouter } from 'next/router'
 import useLayerInfo from '../../../hooks/useLayerInfo'
-import useRegions from '../../../hooks/useRegions'
 import { getBaseUrl } from '../../../utils/misc'
 
-const MSWGenerationChart = () => {
+const MSWGenerationChart = (regionMswValue) => {
   const router = useRouter()
   const baseURL = getBaseUrl()
   const { country } = router.query
   const { layers, loading: layerLoading } = useLayerInfo()
-  const { countriesWithRegions, loading: regionLoading } = useRegions()
 
   const [nationalEstimate, setNationalEstimate] = useState(0)
   const [cityEstimates, setCityEstimates] = useState([])
   const [cities, setCities] = useState([])
-  const [regionMswValue, setRegionMswValue] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
-      if (layerLoading || regionLoading || !country || !layers.length) return
+      if (layerLoading || !country || !layers.length) return
 
       const layerMapping = {
         national: 'Municipal_solid_waste_generated_daily_per_capita_V3_WFL1',
@@ -48,17 +45,10 @@ const MSWGenerationChart = () => {
           ? cityData.map((item, index) => item.City || `City ${index + 1}`)
           : []
       )
-
-      const selectedCountry = countriesWithRegions.find(
-        (c) => c.CountryName === country
-      )
-      if (selectedCountry) {
-        setRegionMswValue(selectedCountry.regionMswValue)
-      }
     }
 
     fetchData()
-  }, [country, layers, layerLoading, countriesWithRegions, regionLoading])
+  }, [country, layers, layerLoading])
 
   const getOption = () => {
     const categories = ['National estimate', ...cities]
@@ -92,7 +82,7 @@ const MSWGenerationChart = () => {
             } kg/person/day<br/>`
           })
           if (regionMswValue) {
-            content += `<br/><span style="color: #020A5B; font-weight: bold;">Estimated Regional Average:</span> ${regionMswValue} kg/person/day`
+            content += `<br/><span style="color: #020A5B; font-weight: bold;">Estimated Regional Average:</span> ${regionMswValue.regionMswValue} kg/person/day`
           }
           return content
         },
@@ -150,10 +140,12 @@ const MSWGenerationChart = () => {
           markLine: {
             data: [
               {
-                yAxis: regionMswValue || 0.78,
+                yAxis: regionMswValue.regionMswValue || 0.78,
                 label: {
                   formatter: () =>
-                    `Regional Average (${regionMswValue || 0.78}) kg`,
+                    `Regional Average (${
+                      regionMswValue.regionMswValue || 0.78
+                    }) kg`,
                   position: 'middle',
                   color: '#020A5B',
                   fontSize: 12,
--- strapi/src/api/category/routes/category-replace.js
+++ strapi/src/api/category/routes/category-replace.js
@@ -2,7 +2,7 @@ module.exports = {
     routes: [
         {
             method: "GET",
-            path: "/category/category-replace/:country/:categoryId",
+            path: "/category/category-replace/:country/:categoryId/:regionMswValue",
             handler: "category-replace.replacePlaceholders",
             config: { auth: false },
         },
