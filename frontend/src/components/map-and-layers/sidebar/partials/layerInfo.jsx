755c817e9e07723fdc4c5041fa72e8a53794fb53
DHI\jlaz <l.jasmiina@gmail.com>
2025-01-29 10:08:40 +0100

    Add translations for layer info

--- frontend/src/components/map-and-layers/sidebar/partials/layerInfo.jsx
+++ frontend/src/components/map-and-layers/sidebar/partials/layerInfo.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Button, Card, Typography } from 'antd'
 import * as XLSX from 'xlsx'
 import { saveAs } from 'file-saver'
+import { Trans, t } from '@lingui/macro'
+import { loadCatalog } from '../../../../translations/utils'
 
 const { Title: AntTitle, Paragraph: AntParagraph } = Typography
 
@@ -12,7 +14,7 @@ const LayerInfo = ({ layer }) => {
 
   const handleDownloadData = () => {
     if (!layer?.attributes.ValuePerCountry?.length) {
-      console.error('No data available to download.')
+      console.error(t`No data available to download.`)
       return
     }
 
@@ -24,7 +26,7 @@ const LayerInfo = ({ layer }) => {
 
     const worksheet = XLSX.utils.json_to_sheet(data)
     const workbook = XLSX.utils.book_new()
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Layer data')
+    XLSX.utils.book_append_sheet(workbook, worksheet, t`Layer data`)
 
     const excelBuffer = XLSX.write(workbook, {
       bookType: 'xlsx',
@@ -32,7 +34,7 @@ const LayerInfo = ({ layer }) => {
     })
     const blob = new Blob([excelBuffer], { type: 'application/octet-stream' })
 
-    saveAs(blob, `${layer?.attributes.title || 'LayerData'}.xlsx`)
+    saveAs(blob, `${layer?.attributes.title || t`LayerData`}.xlsx`) 
   }
 
   const hasDataToDownload =
@@ -49,19 +51,19 @@ const LayerInfo = ({ layer }) => {
           onClick={handleReadMoreClick}
           style={{ border: '1px solid #09334B', padding: '0 8px' }}
         >
-          Read More
+          {t`Read More`}
         </Button>
       </Card>
 
       <Card>
         <AntParagraph>
-          <strong>Time Period:</strong> {layer?.attributes.timePeriod}
+          <strong>{t`Time Period`}:</strong> {layer?.attributes.timePeriod}
         </AntParagraph>
         <AntParagraph>
-          <strong>Data Source:</strong> {layer?.attributes.dataSource}
+          <strong>{t`Data Source`}:</strong> {layer?.attributes.dataSource}
         </AntParagraph>
         <AntParagraph>
-          <strong>URL:</strong>{' '}
+          <strong>{t`URL`}:</strong>{' '}
           <a
             href={layer?.attributes.url}
             target="_blank"
@@ -77,12 +79,20 @@ const LayerInfo = ({ layer }) => {
             onClick={handleDownloadData}
             style={{ marginTop: '10px' }}
           >
-            Download Data
+            {t`Download Data`}
           </Button>
         )}
       </Card>
     </div>
   )
+}
+
+export const getStaticProps = async (ctx) => {
+  return {
+    props: {
+      i18n: await loadCatalog(ctx.locale),
+    },
+  }
 }
 
 export default LayerInfo
