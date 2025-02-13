1e8d128e1c6337c9fd200dfbb706a87245282d20
DHI\jlaz <l.jasmiina@gmail.com>
2025-02-13 10:41:15 +0100

    Run build

--- frontend/src/pages/countryOverview/index.jsx
+++ frontend/src/pages/countryOverview/index.jsx
@@ -41,6 +41,8 @@ const splitTextByMarker = (text, marker) => {
 const addTooltipsToPlaceholders = (htmlString, placeholders, tooltips) => {
   if (!placeholders || Object.keys(placeholders).length === 0) return htmlString
 
+  console.log('run build')
+
   const options = {
     replace: (node) => {
       if (node.name === 'placeholder' && node.attribs?.key) {
