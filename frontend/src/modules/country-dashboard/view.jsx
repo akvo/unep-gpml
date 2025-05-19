f1343ae8d188c938252cee023b25022204542e36
rullymartanto <rullymartanto@gmail.com>
2025-05-19 14:43:00 +0700

    update chart and mobile version for dashboard

--- frontend/src/modules/country-dashboard/view.jsx
+++ frontend/src/modules/country-dashboard/view.jsx
@@ -5,9 +5,7 @@ import styles from './style.module.scss'
 function CountryLibrary({}) {
   return (
     <div className={styles.knowledgeLib} style={{ backgroundColor: '#F5F7FF', paddingBottom:'10px' }}>
-      <Fragment>
         <ResourceView />
-      </Fragment>
     </div>
   )
 }
