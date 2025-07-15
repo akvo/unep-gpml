79f74e22c993403ae24e60f2670a76bbfb041cff
navins94 <nikshingote@gmail.com>
2025-07-15 15:21:02 +0530

    add case study schema cobsea

---
+++ strapi/src/api/cobsea-case-study/controllers/cobsea-case-study.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cobsea-case-study controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cobsea-case-study.cobsea-case-study');
