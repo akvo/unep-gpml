07ae14c9aacf6f12be676bb1047a879289fbb37a
navins94 <nikshingote@gmail.com>
2025-07-15 15:23:52 +0530

    add latest news

---
+++ strapi/src/api/cobsea-latest-news-item/controllers/cobsea-latest-news-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cobsea-latest-news-item controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cobsea-latest-news-item.cobsea-latest-news-item');
---
+++ strapi/src/api/cobsea-latest-news-item/routes/cobsea-latest-news-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cobsea-latest-news-item router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cobsea-latest-news-item.cobsea-latest-news-item');
