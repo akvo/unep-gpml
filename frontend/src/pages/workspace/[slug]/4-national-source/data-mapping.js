f8eebaf76323ab04b2556a7cfa35a591c80dd853
Martin Christov <martin.christov@gmail.com>
2025-08-28 11:07:38 +0300

    rendering strapi pages html directly

--- frontend/src/pages/workspace/[slug]/4-national-source/data-mapping.js
+++ frontend/src/pages/workspace/[slug]/4-national-source/data-mapping.js
@@ -8,14 +8,16 @@ import { useRouter } from 'next/router'
 const View = () => {
   const router = useRouter()
   const { data } = useStepInstructions('data-mapping', router.locale)
-
   return (
     <>
       <h4 className="caps-heading-m">
         <Trans>Data Mapping</Trans>
       </h4>
       <h2 className="h-xxl w-bold">{data?.title}</h2>
-      <MarkdownRenderer content={data?.content} allowSlides={true} />
+      <div
+        className="strapi-workflow-page"
+        dangerouslySetInnerHTML={{ __html: data?.content }}
+      ></div>
     </>
   )
 }
