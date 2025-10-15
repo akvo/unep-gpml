b29d72dc618e8020045d5750de12ac860476c2c7
navins94 <nikshingote@gmail.com>
2025-10-15 11:08:11 +0530

    fix sentry reply

--- frontend/sentry.client.config.js
+++ frontend/sentry.client.config.js
@@ -1,30 +1,21 @@
-// This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://42e4971ab9224be0a5f4babba207e6f4@o65834.ingest.sentry.io/5732892",
+  dsn:
+    'https://42e4971ab9224be0a5f4babba207e6f4@o65834.ingest.sentry.io/5732892',
 
-  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
   replaysOnErrorSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
 
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
-    new Sentry.Replay({
-      // Additional Replay configuration goes in here, for example:
+    Sentry.replayIntegration({
       maskAllText: true,
       blockAllMedia: true,
     }),
   ],
-});
+})
